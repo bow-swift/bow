f18c34c5daa877ac5c8f5d991998967d04bfc4fb
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-28 15:28:14 +0200

    Make functions public

--- CategoryCore/Predef.swift
+++ CategoryCore/Predef.swift
@@ -8,27 +8,27 @@
 
 import Foundation
 
-func id<A>(_ a : A) -> A {
+public func id<A>(_ a : A) -> A {
     return a
 }
 
-func compose<A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
+public func compose<A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
     return { x in g(f(x)) }
 }
 
 infix operator >> : AdditionPrecedence
 infix operator << : AdditionPrecedence
 
-func >><A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
+public func >><A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
     return compose(f, g)
 }
 
-func <<<A, B, C>(_ g : @escaping (B) -> C, _ f : @escaping (A) -> B) -> (A) -> C {
+public func <<<A, B, C>(_ g : @escaping (B) -> C, _ f : @escaping (A) -> B) -> (A) -> C {
     return f >> g
 }
 
 infix operator |> : AdditionPrecedence
 
-func |><A, B>(_ a : A, _ f : (A) -> B) -> B {
+public func |><A, B>(_ a : A, _ f : (A) -> B) -> B {
     return f(a)
 }
