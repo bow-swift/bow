60629252a1b4da5b6adcb154d33b56cb965e936f
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-28 13:12:36 +0200

    Initial Commit

---
+++ CategoryCore/CategoryCore.h
@@ -0,0 +1,19 @@
+//
+//  CategoryCore.h
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 28/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CategoryCore.
+FOUNDATION_EXPORT double CategoryCoreVersionNumber;
+
+//! Project version string for CategoryCore.
+FOUNDATION_EXPORT const unsigned char CategoryCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CategoryCore/PublicHeader.h>
+
+
---
+++ CategoryCoreTests/CategoryCoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryCoreTests.swift
+//  CategoryCoreTests
+//
+//  Created by Tomás Ruiz López on 28/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryCore
+
+class CategoryCoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
