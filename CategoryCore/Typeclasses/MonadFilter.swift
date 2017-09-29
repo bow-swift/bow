11d5804193fe80e6dac4e202a586f587974b9d45
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-29 11:44:27 +0200

    Create MonadFilter Typeclass

---
+++ CategoryCore/Typeclasses/MonadFilter.swift
@@ -0,0 +1,25 @@
+//
+//  MonadFilter.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 29/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public protocol MonadFilter : Monad, FunctorFilter {
+    func empty<A>() -> HK<F, A>
+}
+
+public extension MonadFilter {
+    public func mapFilter<A, B>(_ fa: HK<F, A>, _ f: (A) -> B?) -> HK<F, B> {
+        return flatMap(fa, { a in
+            if let b = f(a) {
+                return pure(b)
+            } else {
+                return empty()
+            }
+        })
+    }
+}
