d7b8f9a59b95e9912bd6c0662b4d469436cf7fd2
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-29 12:00:12 +0200

    Create MonadState Typeclass

---
+++ CategoryCore/Typeclasses/MonadState.swift
@@ -0,0 +1,33 @@
+//
+//  MonadState.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 29/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public protocol MonadState : Monad {
+    associatedtype S
+    
+    func get() -> HK<F, S>
+    func set(_ s : S) -> HK<F, ()>
+}
+
+public extension MonadState {
+    public func state<A>(_ f : (S) -> (S, A)) -> HK<F, A> {
+        return self.flatMap(self.get(), { s in
+            let result = f(s)
+            return self.map(self.set(result.0), { _ in result.1 })
+        })
+    }
+    
+    public func modify(_ f : (S) -> S) -> HK<F, ()> {
+        return self.flatMap(self.get(), { s in self.set(f(s))})
+    }
+    
+    public func inspect<A>(_ f : (S) -> A) -> HK<F, A> {
+        return self.map(self.get(), f)
+    }
+}
