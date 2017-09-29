e3fd906e6edd8245bbc575ec5bad2f9b9464dab1
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-29 11:24:49 +0200

    Create ApplicativeError Typeclass

---
+++ CategoryCore/Typeclasses/ApplicativeError.swift
@@ -0,0 +1,30 @@
+//
+//  ApplicativeError.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 29/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public protocol ApplicativeError : Applicative {
+    associatedtype E
+    
+    func raiseError<A>(_ e : E) -> HK<F, A>
+    func handleErrorWith<A>(_ fa : HK<F, A>, _ f : (E) -> HK<F, A>) -> HK<F, A>
+}
+
+public extension ApplicativeError {
+    public func handleError<A>(_ fa : HK<F, A>, _ f : (E) -> A) -> HK<F, A> {
+        return handleErrorWith(fa, { a in pure(f(a)) })
+    }
+    
+    public func catchError<A>(_ f : () throws -> A, recover : (Error) -> E) -> HK<F, A> {
+        do {
+            return pure(try f())
+        } catch {
+            return raiseError(recover(error))
+        }
+    }
+}
