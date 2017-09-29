6755e6acfe85d2a1531d9ddc33126945b3f9380a
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-09-29 12:36:43 +0200

    Create Comonad Typeclass

---
+++ CategoryCore/Typeclasses/Comonad.swift
@@ -0,0 +1,20 @@
+//
+//  Comonad.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 29/9/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public protocol Comonad : Functor {
+    func coflatMap<A, B>(_ fa : HK<F, A>, _ f : (HK<F, A>) -> B) -> HK<F, B>
+    func extract<A>(_ fa : HK<F, A>) -> A
+}
+
+public extension Comonad {
+    public func duplicate<A>(_ fa : HK<F, A>) -> HK<F, HK<F, A>> {
+        return coflatMap(fa, id)
+    }
+}
