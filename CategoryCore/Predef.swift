509ad18dcf2fb4c484b6d7f019b3e8983b7545e6
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-02 14:59:13 +0200

    Add special cases for function composition

--- CategoryCore/Predef.swift
+++ CategoryCore/Predef.swift
@@ -12,6 +12,10 @@ public func id<A>(_ a : A) -> A {
     return a
 }
 
+public func compose<A, B>(_ f : @escaping () -> A, _ g : @escaping (A) -> B) -> () -> B {
+    return { g(f()) }
+}
+
 public func compose<A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
     return { x in g(f(x)) }
 }
@@ -19,8 +23,16 @@ public func compose<A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C)
 infix operator >> : AdditionPrecedence
 infix operator << : AdditionPrecedence
 
+public func >><A, B>(_ f : @escaping () -> A, _ g : @escaping (A) -> B) -> () -> B {
+    return compose(f, g)
+}
+
 public func >><A, B, C>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> C) -> (A) -> C {
     return compose(f, g)
+}
+
+public func <<<A, B>(_ g : @escaping (A) -> B, _ f : @escaping () -> A) -> () -> B {
+    return f >> g
 }
 
 public func <<<A, B, C>(_ g : @escaping (B) -> C, _ f : @escaping (A) -> B) -> (A) -> C {
