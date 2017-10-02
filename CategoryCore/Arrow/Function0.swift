30e617b8696a28af4540f690ddd861c415e8ff11
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-02 15:00:20 +0200

    Create class for Function0

---
+++ CategoryCore/Arrow/Function0.swift
@@ -0,0 +1,77 @@
+//
+//  Function0.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 2/10/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public class Function0F {}
+
+public class Function0<A> : HK<Function0F, A> {
+    fileprivate let f : () -> A
+    
+    init(_ f : @escaping () -> A) {
+        self.f = f
+    }
+    
+    public func invoke() -> A {
+        return f()
+    }
+    
+    public func map<B>(_ f : @escaping (A) -> B) -> Function0<B> {
+        return Function0Instances().map(self, f) as! Function0<B>
+    }
+    
+    public func flatMap<B>(_ f : @escaping (A) -> Function0<B>) -> Function0<B> {
+        return Function0Instances().flatMap(self, f) as! Function0<B>
+    }
+    
+    public func coflatMap<B>(_ f : @escaping (Function0<A>) -> B) -> Function0<B> {
+        return Function0Instances().coflatMap(self, f as! (HK<Function0F, A>) -> B) as! Function0<B>
+    }
+    
+    public func ap<B>(_ ff : Function0<(A) -> B>) -> Function0<B> {
+        return Function0Instances().ap(self, ff) as! Function0<B>
+    }
+    
+    public func extract() -> A {
+        return Function0Instances().extract(self)
+    }
+    
+    public static func pure(_ a : A) -> Function0<A> {
+        return Function0Instances().pure(a) as! Function0<A>
+    }
+}
+
+public class Function0Instances : Functor, Applicative, Monad, Comonad, Bimonad {
+    public typealias F = Function0F
+    
+    public func map<A, B>(_ fa: HK<Function0F, A>, _ f: @escaping (A) -> B) -> HK<Function0F, B> {
+        let funA = fa as! Function0<A>
+        return Function0(funA.f >> f)
+    }
+    
+    public func lift<A, B>(_ f: @escaping (A) -> B) -> (HK<Function0F, A>) -> HK<Function0F, B> {
+        return { fa in self.map(fa, f) }
+    }
+    
+    public func pure<A>(_ a: A) -> HK<Function0Instances.F, A> {
+        return Function0({ a })
+    }
+    
+    public func flatMap<A, B>(_ fa: HK<Function0Instances.F, A>, _ f: @escaping (A) -> HK<Function0Instances.F, B>) -> HK<Function0Instances.F, B> {
+        let funA = fa as! Function0<A>
+        return f(funA.invoke())
+    }
+    
+    public func coflatMap<A, B>(_ fa: HK<Function0Instances.F, A>, _ f: @escaping (HK<Function0Instances.F, A>) -> B) -> HK<Function0Instances.F, B> {
+        return Function0({ f(fa) })
+    }
+    
+    public func extract<A>(_ fa: HK<Function0Instances.F, A>) -> A {
+        return (fa as! Function0<A>).invoke()
+    }
+}
