3ec3d307dcbb1ac4db6b8e2b9bc4d03c4d449d8d
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-03 16:48:05 +0200

    Create class for Either

---
+++ CategoryCore/Data/Either.swift
@@ -0,0 +1,117 @@
+//
+//  Either.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 3/10/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public class EitherF {}
+
+public class Either<A, B> : HK2<EitherF, A, B> {
+    public static func left(_ a : A) -> Either<A, B> {
+        return Left<A, B>(a)
+    }
+    
+    public static func right(_ b : B) -> Either<A, B> {
+        return Right<A, B>(b)
+    }
+    
+    public func fold<C>(_ fa : (A) -> C, _ fb : (B) -> C) -> C {
+        switch self {
+            case is Left<A, B>:
+                return (self as! Left<A, B>).a |> fa
+            case is Right<A, B>:
+                return (self as! Right<A, B>).b |> fb
+            default:
+                fatalError("Either must only have left and right cases")
+        }
+    }
+    
+    public var isLeft : Bool {
+        return fold(constF(true), constF(false))
+    }
+    
+    public var isRight : Bool {
+        return !isLeft
+    }
+    
+    public func foldL<C>(_ c : C, _ f : (C, B) -> C) -> C {
+        return fold(constF(c), { b in f(c, b) })
+    }
+    
+    public func swap() -> Either<B, A> {
+        return fold(Either<B, A>.right, Either<B, A>.left)
+    }
+    
+    public func map<C>(_ f : (B) -> C) -> Either<A, C> {
+        return fold(Either<A, C>.left,
+                    { b in Either<A, C>.right(f(b)) })
+    }
+    
+    public func bimap<C, D>(_ fa : (A) -> C, _ fb : (B) -> D) -> Either<C, D> {
+        return fold({ a in Either<C, D>.left(fa(a)) },
+                    { b in Either<C, D>.right(fb(b)) })
+    }
+    
+    public func ap<C>(_ ff : Either<A, (B) -> C>) -> Either<A, C> {
+        return ff.flatMap{ f in self.map(f) }
+    }
+    
+    public func flatMap<C>(_ f : (B) -> Either<A, C>) -> Either<A, C> {
+        return fold(Either<A, C>.left, f)
+    }
+    
+    public func exists(_ predicate : (B) -> Bool) -> Bool {
+        return fold(constF(false), predicate)
+    }
+    
+    public func toMaybe() -> Maybe<B> {
+        return fold(constF(Maybe<B>.none()), Maybe<B>.some)
+    }
+    
+    public func getOrElse(_ defaultValue : B) -> B {
+        return fold(constF(defaultValue), id)
+    }
+    
+    public func filterOrElse(_ predicate : @escaping (B) -> Bool, _ defaultValue : A) -> Either<A, B> {
+        return fold(Either<A, B>.left,
+                    { b in predicate(b) ?
+                        Either<A, B>.right(b) :
+                        Either<A, B>.left(defaultValue) })
+    }
+
+    public func traverse<G, C, Appl>(_ f : (B) -> HK<G, C>, _ applicative : Appl) -> HK<G, Either<A, C>> where Appl : Applicative, Appl.F == G {
+        return fold({ a in applicative.pure(Either<A, C>.left(a)) },
+                    { b in applicative.map(f(b), { c in Either<A, C>.right(c) }) })
+    }
+    
+    public func combineK(_ y : Either<A, B>) -> Either<A, B> {
+        return fold(constF(y), Either<A, B>.right)
+    }
+}
+
+class Left<A, B> : Either<A, B> {
+    fileprivate let a : A
+    
+    init(_ a : A) {
+        self.a = a
+    }
+}
+
+class Right<A, B> : Either<A, B> {
+    fileprivate let b : B
+    
+    init(_ b : B) {
+        self.b = b
+    }
+}
+
+extension Either : CustomStringConvertible {
+    public var description : String {
+        return fold({ a in "Left(\(a))"},
+                    { b in "Right(\(b))"})
+    }
+}
--- CategoryCore/Predef.swift
+++ CategoryCore/Predef.swift
@@ -12,6 +12,26 @@ public func id<A>(_ a : A) -> A {
     return a
 }
 
+func constF<A>(_ a : A) -> () -> A {
+    return { a }
+}
+
+func constF<A, B>(_ a : A) -> (B) -> A {
+    return { _ in a }
+}
+
+func constF<A, B, C>(_ a : A) -> (B, C) -> A {
+    return { _, _ in a }
+}
+
+func constF<A, B, C, D>(_ a : A) -> (B, C, D) -> A {
+    return { _, _, _ in a }
+}
+
+func constF<A, B, C, D, E>(_ a : A) -> (B, C, D, E) -> A {
+    return { _, _, _, _ in a }
+}
+
 public func compose<A, B>(_ f : @escaping () -> A, _ g : @escaping (A) -> B) -> () -> B {
     return { g(f()) }
 }
