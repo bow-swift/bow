ce0faeabd9faa4551922edf504cc7fbadfc623db
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-04 13:21:01 +0200

    Add product and tupled methods

--- CategoryCore/Typeclasses/Applicative.swift
+++ CategoryCore/Typeclasses/Applicative.swift
@@ -18,8 +18,104 @@ public extension Applicative {
         return self.ap(fb, self.map(fa, { (a : A) in { (b : B) in (a, b) }}))
     }
     
+    public func product<A, B, Z>(_ fa : HK<F, (A, B)>, _ fz : HK<F, Z>) -> HK<F, (A, B, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, Z>(_ fa : HK<F, (A, B, C)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, D, Z>(_ fa : HK<F, (A, B, C, D)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, D, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, D, E, Z>(_ fa : HK<F, (A, B, C, D, E)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, D, E, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, D, E, G, Z>(_ fa : HK<F, (A, B, C, D, E, G)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, D, E, G, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, D, E, G, H, Z>(_ fa : HK<F, (A, B, C, D, E, G, H)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, D, E, G, H, Z)> {
+        return self.product(fa, fz)
+    }
+    
+    public func product<A, B, C, D, E, G, H, I, Z>(_ fa : HK<F, (A, B, C, D, E, G, H, I)>, _ fz : HK<F, Z>) -> HK<F, (A, B, C, D, E, G, H, I, Z)> {
+        return self.product(fa, fz)
+    }
+    
     public func map2<A, B, Z>(_ fa : HK<F, A>, _ fb : HK<F, B>, _ f : @escaping (A, B) -> Z) -> HK<F, Z> {
         return map(product(fa, fb), f)
+    }
+    
+    public func tupled<A, B>(_ a : HK<F, A>,
+                             _ b : HK<F, B>) -> HK<F, (A, B)> {
+        return product(a, b)
+    }
+    
+    public func tupled<A, B, C>(_ a : HK<F, A>,
+                                _ b : HK<F, B>,
+                                _ c : HK<F, C>) -> HK<F, (A, B, C)> {
+        return product(product(a, b), c)
+    }
+    
+    public func tupled<A, B, C, D>(_ a : HK<F, A>,
+                                   _ b : HK<F, B>,
+                                   _ c : HK<F, C>,
+                                   _ d : HK<F, D>) -> HK<F, (A, B, C, D)> {
+        return product(product(product(a, b), c), d)
+    }
+    
+    public func tupled<A, B, C, D, E>(_ a : HK<F, A>,
+                                      _ b : HK<F, B>,
+                                      _ c : HK<F, C>,
+                                      _ d : HK<F, D>,
+                                      _ e : HK<F, E>) -> HK<F, (A, B, C, D, E)> {
+        return product(product(product(product(a, b), c), d), e)
+    }
+    
+    public func tupled<A, B, C, D, E, G>(_ a : HK<F, A>,
+                                         _ b : HK<F, B>,
+                                         _ c : HK<F, C>,
+                                         _ d : HK<F, D>,
+                                         _ e : HK<F, E>,
+                                         _ g : HK<F, G>) -> HK<F, (A, B, C, D, E, G)> {
+        return product(product(product(product(product(a, b), c), d), e), g)
+    }
+    
+    public func tupled<A, B, C, D, E, G, H>(_ a : HK<F, A>,
+                                            _ b : HK<F, B>,
+                                            _ c : HK<F, C>,
+                                            _ d : HK<F, D>,
+                                            _ e : HK<F, E>,
+                                            _ g : HK<F, G>,
+                                            _ h : HK<F, H>) -> HK<F, (A, B, C, D, E, G, H)> {
+        return product(product(product(product(product(product(a, b), c), d), e), g), h)
+    }
+    
+    public func tupled<A, B, C, D, E, G, H, I>(_ a : HK<F, A>,
+                                               _ b : HK<F, B>,
+                                               _ c : HK<F, C>,
+                                               _ d : HK<F, D>,
+                                               _ e : HK<F, E>,
+                                               _ g : HK<F, G>,
+                                               _ h : HK<F, H>,
+                                               _ i : HK<F, I>) -> HK<F, (A, B, C, D, E, G, H, I)> {
+        return product(product(product(product(product(product(product(a, b), c), d), e), g), h), i)
+    }
+    
+    public func tupled<A, B, C, D, E, G, H, I, J>(_ a : HK<F, A>,
+                                                  _ b : HK<F, B>,
+                                                  _ c : HK<F, C>,
+                                                  _ d : HK<F, D>,
+                                                  _ e : HK<F, E>,
+                                                  _ g : HK<F, G>,
+                                                  _ h : HK<F, H>,
+                                                  _ i : HK<F, I>,
+                                                  _ j : HK<F, J>) -> HK<F, (A, B, C, D, E, G, H, I, J)> {
+        return product(product(product(product(product(product(product(product(a, b), c), d), e), g), h), i), j)
     }
 }
 
