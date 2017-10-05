4571e166bd35c06a2d32d38987c22a30fb3126d7
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-05 18:00:04 +0200

    Make public typealias for Higher Kinds

--- CategoryCore/HigherKinds.swift
+++ CategoryCore/HigherKinds.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public class HK<F, A> {}
-typealias HK2<F, A, B> = HK<HK<F, A>, B>
-typealias HK3<F, A, B, C> = HK<HK2<F, A, B>, C>
-typealias HK4<F, A, B, C, D> = HK<HK3<F, A, B, C>, D>
-typealias HK5<F, A, B, C, D, E> = HK<HK4<F, A, B, C, D>, E>
+public typealias HK2<F, A, B> = HK<HK<F, A>, B>
+public typealias HK3<F, A, B, C> = HK<HK2<F, A, B>, C>
+public typealias HK4<F, A, B, C, D> = HK<HK3<F, A, B, C>, D>
+public typealias HK5<F, A, B, C, D, E> = HK<HK4<F, A, B, C, D>, E>
