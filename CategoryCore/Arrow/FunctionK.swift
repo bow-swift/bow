6fffe671e9999aec24cbd0a70b4c3e9b77f7473f
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-12 13:28:29 +0200

    Add FunctionK identity function

--- CategoryCore/Arrow/FunctionK.swift
+++ CategoryCore/Arrow/FunctionK.swift
@@ -15,3 +15,15 @@ public protocol FunctionK {
     func invoke<A>(_ fa : HK<F, A>) -> HK<G, A>
 }
 
+public class IdFunctionK<M> : FunctionK {
+    public typealias F = M
+    public typealias G = M
+    
+    public static var id : IdFunctionK<M> {
+        return IdFunctionK<M>()
+    }
+    
+    public func invoke<A>(_ fa: HK<M, A>) -> HK<M, A> {
+        return fa
+    }
+}
