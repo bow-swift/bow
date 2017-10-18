e8f202dc23150ce521dbe68360faaf066a961df7
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-10-18 10:44:29 +0200

    Create instances Foldable and Traverse for Id

--- CategoryCore/Data/Id.swift
+++ CategoryCore/Data/Id.swift
@@ -47,7 +47,7 @@ public class Id<A> : HK<IdF, A> {
         return f(value, b)
     }
     
-    public func traverse<G, B, Appl>(_ f : (A) -> HK<G, B>, _ applicative : Appl) -> HK<G, Id<B>> where Appl : Applicative, Appl.F == G {
+    public func traverse<G, B, Appl>(_ f : (A) -> HK<G, B>, _ applicative : Appl) -> HK<G, HK<IdF, B>> where Appl : Applicative, Appl.F == G {
         return applicative.map(f(self.value), Id<B>.init)
     }
     
@@ -57,6 +57,12 @@ public class Id<A> : HK<IdF, A> {
     
     public func extract() -> A {
         return value
+    }
+}
+
+public extension HK where F == IdF {
+    public func ev() -> Id<A> {
+        return self as! Id<A>
     }
 }
 
@@ -86,13 +92,21 @@ extension Id {
     public static func bimonad() -> IdBimonad {
         return IdBimonad()
     }
+    
+    public static func foldable() -> IdFoldable {
+        return IdFoldable()
+    }
+    
+    public static func traverse() -> IdTraverse {
+        return IdTraverse()
+    }
 }
 
 public class IdFunctor : Functor {
     public typealias F = IdF
     
     public func map<A, B>(_ fa: HK<IdF, A>, _ f: @escaping (A) -> B) -> HK<IdF, B> {
-        return (fa as! Id<A>).map(f)
+        return fa.ev().map(f)
     }
 }
 
@@ -102,13 +116,13 @@ public class IdApplicative : IdFunctor, Applicative {
     }
     
     public func ap<A, B>(_ fa: HK<IdF, A>, _ ff: HK<IdF, (A) -> B>) -> HK<IdF, B> {
-        return (fa as! Id<A>).ap(ff as! Id<(A) -> B>)
+        return fa.ev().ap(ff as! Id<(A) -> B>)
     }
 }
 
 public class IdMonad : IdApplicative, Monad {
     public func flatMap<A, B>(_ fa: HK<IdF, A>, _ f: @escaping (A) -> HK<IdF, B>) -> HK<IdF, B> {
-        return (fa as! Id<A>).flatMap(f as! (A) -> Id<B>)
+        return fa.ev().flatMap(f as! (A) -> Id<B>)
     }
     
     public func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> HK<IdF, Either<A, B>>) -> HK<IdF, B> {
@@ -118,10 +132,28 @@ public class IdMonad : IdApplicative, Monad {
 
 public class IdBimonad : IdMonad, Bimonad {
     public func coflatMap<A, B>(_ fa: HK<IdF, A>, _ f: @escaping (HK<IdF, A>) -> B) -> HK<IdF, B> {
-        return (fa as! Id<A>).coflatMap(f as (Id<A>) -> B)
+        return fa.ev().coflatMap(f as (Id<A>) -> B)
     }
     
     public func extract<A>(_ fa: HK<IdF, A>) -> A {
-        return (fa as! Id<A>).extract()
+        return fa.ev().extract()
+    }
+}
+
+public class IdFoldable : Foldable {
+    public typealias F = IdF
+    
+    public func foldL<A, B>(_ fa: HK<IdF, A>, _ b: B, _ f: (B, A) -> B) -> B {
+        return fa.ev().foldL(b, f)
+    }
+    
+    public func foldR<A, B>(_ fa: HK<IdF, A>, _ b: Eval<B>, _ f: (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+        return fa.ev().foldR(b, f)
+    }
+}
+
+public class IdTraverse : IdFoldable, Traverse {
+    public func traverse<G, A, B, Appl>(_ fa: HK<IdF, A>, _ f: (A) -> HK<G, B>, _ applicative: Appl) -> HK<G, HK<IdF, B>> where G == Appl.F, Appl : Applicative {
+        return fa.ev().traverse(f, applicative)
     }
 }
