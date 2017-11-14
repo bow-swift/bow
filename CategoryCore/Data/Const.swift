98fafd3b9a63f5787b9cebf4c714932fbc02600b
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-14 15:56:46 +0100

    Provide instances for Const

--- CategoryCore/Data/Const.swift
+++ CategoryCore/Data/Const.swift
@@ -12,7 +12,7 @@ public class ConstF {}
 public typealias ConstPartial<A> = HK<ConstF, A>
 
 public class Const<A, T> : HK2<ConstF, A, T> {
-    private let value : A
+    public let value : A
     
     public static func pure(_ a : A) -> Const<A, T> {
         return Const<A, T>(a)
@@ -26,11 +26,11 @@ public class Const<A, T> : HK2<ConstF, A, T> {
         return Const<A, U>(value)
     }
     
-    public func traverse<F, U, Appl>(_ f : (T) -> HK<F, U>, _ applicative : Appl) -> HK<F, Const<A, U>> where Appl : Applicative, Appl.F == F {
+    public func traverse<F, U, Appl>(_ f : (T) -> HK<F, U>, _ applicative : Appl) -> HK<F, HK2<ConstF, A, U>> where Appl : Applicative, Appl.F == F {
         return applicative.pure(retag())
     }
     
-    public func traverseFilter<F, U, Appl>(_ f : (T) -> HK<F, Maybe<U>>, _ applicative : Appl) -> HK<F, Const<A, U>> where Appl : Applicative, Appl.F == F {
+    public func traverseFilter<F, U, Appl>(_ f : (T) -> HK<F, Maybe<U>>, _ applicative : Appl) -> HK<F, HK2<ConstF, A, U>> where Appl : Applicative, Appl.F == F {
         return applicative.pure(retag())
     }
     
@@ -46,5 +46,126 @@ public class Const<A, T> : HK2<ConstF, A, T> {
 extension Const : CustomStringConvertible {
     public var description : String {
         return "Const(\(value))"
+    }
+}
+
+public extension Const {
+    public static func functor() -> ConstFunctor<A> {
+        return ConstFunctor<A>()
+    }
+    
+    public static func applicative<Mono>(_ monoid : Mono) -> ConstApplicative<A, Mono> {
+        return ConstApplicative<A, Mono>(monoid)
+    }
+    
+    public static func semigroup<SemiG>(_ semigroup : SemiG) -> ConstSemigroup<A, T, SemiG> {
+        return ConstSemigroup<A, T, SemiG>(semigroup)
+    }
+    
+    public static func monoid<Mono>(_ monoid : Mono) -> ConstMonoid<A, T, Mono> {
+        return ConstMonoid<A, T, Mono>(monoid)
+    }
+    
+    public static func foldable() -> ConstFoldable<A> {
+        return ConstFoldable<A>()
+    }
+    
+    public static func traverse() -> ConstTraverse<A> {
+        return ConstTraverse<A>()
+    }
+    
+    public static func traverseFilter() -> ConstTraverseFilter<A> {
+        return ConstTraverseFilter<A>()
+    }
+    
+    public static func eq<EqA>(_ eqa : EqA) -> ConstEq<A, T, EqA> {
+        return ConstEq<A, T, EqA>(eqa)
+    }
+}
+
+public class ConstFunctor<R> : Functor {
+    public typealias F = ConstPartial<R>
+    
+    public func map<A, B>(_ fa: HK<HK<ConstF, R>, A>, _ f: @escaping (A) -> B) -> HK<HK<ConstF, R>, B> {
+        return (fa as! Const<R, A>).retag()
+    }
+}
+
+public class ConstApplicative<R, Mono> : ConstFunctor<R>, Applicative where Mono : Monoid, Mono.A == R {
+    private let monoid : Mono
+    
+    public init(_ monoid : Mono) {
+        self.monoid = monoid
+    }
+    
+    public func pure<A>(_ a: A) -> HK<HK<ConstF, R>, A> {
+        return ConstMonoid(self.monoid).empty
+    }
+    
+    public func ap<A, B>(_ fa: HK<HK<ConstF, R>, A>, _ ff: HK<HK<ConstF, R>, (A) -> B>) -> HK<HK<ConstF, R>, B> {
+        return (fa as! Const<R, A>).ap(ff as! Const<R, (A) -> B>, monoid)
+    }
+}
+
+public class ConstSemigroup<R, S, SemiG> : Semigroup where SemiG : Semigroup, SemiG.A == R {
+    public typealias A = Const<R, S>
+    private let semigroup : SemiG
+    
+    public init(_ semigroup : SemiG) {
+        self.semigroup = semigroup
+    }
+    
+    public func combine(_ a: Const<R, S>, _ b: Const<R, S>) -> Const<R, S> {
+        return a.combine(b, semigroup)
+    }
+}
+
+public class ConstMonoid<R, S, Mono> : ConstSemigroup<R, S, Mono>, Monoid where Mono : Monoid, Mono.A == R {
+    private let monoid : Mono
+    
+    override public init(_ monoid : Mono) {
+        self.monoid = monoid
+        super.init(monoid)
+    }
+    
+    public var empty: Const<R, S> {
+        return Const(monoid.empty)
+    }
+}
+
+public class ConstFoldable<R> : Foldable {
+    public typealias F = ConstPartial<R>
+    
+    public func foldL<A, B>(_ fa: HK<HK<ConstF, R>, A>, _ b: B, _ f: (B, A) -> B) -> B {
+        return b
+    }
+    
+    public func foldR<A, B>(_ fa: HK<HK<ConstF, R>, A>, _ b: Eval<B>, _ f: (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+        return b
+    }
+}
+
+public class ConstTraverse<R> : ConstFoldable<R>, Traverse {
+    public func traverse<G, A, B, Appl>(_ fa: HK<HK<ConstF, R>, A>, _ f: (A) -> HK<G, B>, _ applicative: Appl) -> HK<G, HK<HK<ConstF, R>, B>> where G == Appl.F, Appl : Applicative {
+        return (fa as! Const<R, A>).traverse(f, applicative)
+    }
+}
+
+public class ConstTraverseFilter<R> : ConstTraverse<R>, TraverseFilter {
+    public func traverseFilter<A, B, G, Appl>(_ fa: HK<HK<ConstF, R>, A>, _ f: (A) -> HK<G, Maybe<B>>, _ applicative: Appl) -> HK<G, HK<HK<ConstF, R>, B>> where G == Appl.F, Appl : Applicative {
+        return (fa as! Const<R, A>).traverseFilter(f, applicative)
+    }
+}
+
+public class ConstEq<R, S, EqR> : Eq where EqR : Eq, EqR.A == R {
+    public typealias A = Const<R, S>
+    private let eqr : EqR
+    
+    public init(_ eqr : EqR) {
+        self.eqr = eqr
+    }
+    
+    public func eqv(_ a: Const<R, S>, _ b: Const<R, S>) -> Bool {
+        return eqr.eqv(a.value, b.value)
     }
 }
