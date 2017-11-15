96421bae53d48f90392b6efac04578039a7d8dd9
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-15 15:28:10 +0100

    Create protocol for Composed Foldable

---
+++ CategoryCore/Typeclasses/Composed/ComposedFoldable.swift
@@ -0,0 +1,64 @@
+//
+//  ComposedFoldable.swift
+//  CategoryCore
+//
+//  Created by Tomás Ruiz López on 15/11/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import Foundation
+
+public protocol ComposedFoldable : Foldable where F == Nested<G, H>, FoldG : Foldable, FoldG.F == G, FoldH : Foldable, FoldH.F == H {
+    associatedtype G
+    associatedtype H
+    associatedtype FoldG
+    associatedtype FoldH
+    
+    var foldableG : FoldG { get }
+    var foldableH : FoldH { get }
+}
+
+public extension ComposedFoldable {
+    public static func instance(_ foldableG : FoldG, _ foldableH : FoldH) -> BaseComposedFoldable<G, H, FoldG, FoldH> {
+        return BaseComposedFoldable<G, H, FoldG, FoldH>(foldableG, foldableH)
+    }
+    
+    public func foldL<A, B>(_ fa: HK<Nested<G, H>, A>, _ b: B, _ f: (B, A) -> B) -> B {
+        return foldableG.foldL(unnest(fa), b, { bb, aa in self.foldableH.foldL(aa, bb, f) })
+    }
+    
+    public func foldR<A, B>(_ fa: HK<Nested<G, H>, A>, _ b: Eval<B>, _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+        return foldableG.foldR(unnest(fa), b, { aa, bb in self.foldableH.foldR(aa, bb, f) })
+    }
+    
+    public func foldLC<A, B>(_ fa : HK<G, HK<H, A>>, _ b : B, _ f : (B, A) -> B) -> B {
+        return foldL(nest(fa), b, f)
+    }
+    
+    public func foldRC<A, B>(_ fa : HK<G, HK<H, A>>, _ b : Eval<B>, _ f : @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+        return foldR(nest(fa), b, f)
+    }
+}
+
+public class BaseComposedFoldable<A, B, FoldA, FoldB> : ComposedFoldable where FoldA : Foldable, FoldA.F == A, FoldB : Foldable, FoldB.F == B{
+    
+    public typealias G = A
+    public typealias H = B
+    public typealias FoldG = FoldA
+    public typealias FoldH = FoldB
+    
+    public var foldableG: FoldA
+    public var foldableH: FoldB
+    
+    public init(_ foldableA : FoldA, _ foldableB : FoldB) {
+        self.foldableG = foldableA
+        self.foldableH = foldableB
+    }
+}
+
+public extension Foldable {
+    
+    public func compose<G, FoldG>(_ otherFoldable : FoldG) -> BaseComposedFoldable<F, G, Self, FoldG> where FoldG : Foldable{
+        return BaseComposedFoldable.instance(self, otherFoldable)
+    }
+}
