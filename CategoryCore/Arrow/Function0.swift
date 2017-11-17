5a5c383d6190e054f9463b658db3a547b7b4aebb
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-17 12:03:37 +0100

    Provide instances for Function0 according to the rest of the library

--- CategoryCore/Arrow/Function0.swift
+++ CategoryCore/Arrow/Function0.swift
@@ -13,6 +13,10 @@ public class Function0F {}
 public class Function0<A> : HK<Function0F, A> {
     fileprivate let f : () -> A
     
+    public static func pure(_ a : A) -> Function0<A> {
+        return Function0.applicative().pure(a) as! Function0<A>
+    }
+    
     public static func loop<B>(_ a : A, _ f : (A) -> HK<Function0F, Either<A, B>>) -> B {
         let result = (f(a) as! Function0<Either<A, B>>).extract()
         return result.fold({ a in loop(a, f) }, id)
@@ -35,27 +39,23 @@ public class Function0<A> : HK<Function0F, A> {
     }
     
     public func map<B>(_ f : @escaping (A) -> B) -> Function0<B> {
-        return Function0Instances().map(self, f) as! Function0<B>
+        return Function0.functor().map(self, f).ev()
     }
     
     public func flatMap<B>(_ f : @escaping (A) -> Function0<B>) -> Function0<B> {
-        return Function0Instances().flatMap(self, f) as! Function0<B>
+        return Function0.monad().flatMap(self, f).ev()
     }
     
     public func coflatMap<B>(_ f : @escaping (Function0<A>) -> B) -> Function0<B> {
-        return Function0Instances().coflatMap(self, f as! (HK<Function0F, A>) -> B) as! Function0<B>
+        return Function0.comonad().coflatMap(self, f as! (HK<Function0F, A>) -> B).ev()
     }
     
     public func ap<B>(_ ff : Function0<(A) -> B>) -> Function0<B> {
-        return Function0Instances().ap(self, ff) as! Function0<B>
+        return Function0.applicative().ap(self, ff).ev()
     }
     
     public func extract() -> A {
-        return Function0Instances().extract(self)
-    }
-    
-    public static func pure(_ a : A) -> Function0<A> {
-        return Function0Instances().pure(a) as! Function0<A>
+        return Function0.comonad().extract(self)
     }
 }
 
@@ -65,32 +65,58 @@ public extension HK where F == Function0F {
     }
 }
 
-public class Function0Instances : Functor, Applicative, Monad, Comonad, Bimonad {
+public extension Function0 {
+    public static func functor() -> Function0Functor {
+        return Function0Functor()
+    }
+    
+    public static func applicative() -> Function0Monad {
+        return Function0Monad()
+    }
+    
+    public static func monad() -> Function0Monad {
+        return Function0Monad()
+    }
+    
+    public static func comonad() -> Function0Bimonad {
+        return Function0Bimonad()
+    }
+    
+    public static func bimonad() -> Function0Bimonad {
+        return Function0Bimonad()
+    }
+}
+
+public class Function0Functor : Functor {
     public typealias F = Function0F
     
     public func map<A, B>(_ fa: HK<Function0F, A>, _ f: @escaping (A) -> B) -> HK<Function0F, B> {
         let funA = fa as! Function0<A>
         return Function0(funA.f >> f)
     }
-    
-    public func pure<A>(_ a: A) -> HK<Function0Instances.F, A> {
+}
+
+public class Function0Monad : Function0Functor, Monad {
+    public func pure<A>(_ a: A) -> HK<F, A> {
         return Function0({ a })
     }
     
-    public func flatMap<A, B>(_ fa: HK<Function0Instances.F, A>, _ f: @escaping (A) -> HK<Function0Instances.F, B>) -> HK<Function0Instances.F, B> {
-        let funA = fa as! Function0<A>
-        return f(funA.invoke())
+    public func flatMap<A, B>(_ fa: HK<Function0F, A>, _ f: @escaping (A) -> HK<Function0F, B>) -> HK<Function0F, B> {
+        return f(fa.ev().invoke())
     }
     
     public func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> HK<Function0F, Either<A, B>>) -> HK<Function0F, B> {
         return Function0.tailRecM(a, f)
     }
-    
-    public func coflatMap<A, B>(_ fa: HK<Function0Instances.F, A>, _ f: @escaping (HK<Function0Instances.F, A>) -> B) -> HK<Function0Instances.F, B> {
+}
+
+public class Function0Bimonad : Function0Monad, Bimonad {
+    public func coflatMap<A, B>(_ fa: HK<Function0F, A>, _ f: @escaping (HK<Function0F, A>) -> B) -> HK<Function0F, B> {
         return Function0({ f(fa) })
     }
     
-    public func extract<A>(_ fa: HK<Function0Instances.F, A>) -> A {
-        return (fa as! Function0<A>).invoke()
+    public func extract<A>(_ fa: HK<Function0F, A>) -> A {
+        return fa.ev().invoke()
     }
 }
+
