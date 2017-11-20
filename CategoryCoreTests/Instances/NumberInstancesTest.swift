73326552f77462e6a1f615bee1da8f80706ed05b
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-20 16:13:55 +0100

    Test semigroup laws for numerical types

--- CategoryCoreTests/Instances/NumberInstancesTest.swift
+++ CategoryCoreTests/Instances/NumberInstancesTest.swift
@@ -61,4 +61,103 @@ class NumberInstancesTest: XCTestCase {
     }
     
     
+    func testIntSemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : Int, b : Int, c : Int) in
+            return SemigroupLaws.check(semigroup: Int.sumMonoid, a: a, b: b, c: c, eq: Int.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : Int, b : Int, c : Int) in
+            return SemigroupLaws.check(semigroup: Int.productMonoid, a: a, b: b, c: c, eq: Int.order)
+        }
+    }
+    
+    func testInt8SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : Int8, b : Int8, c : Int8) in
+            return SemigroupLaws.check(semigroup: Int8.sumMonoid, a: a, b: b, c: c, eq: Int8.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : Int8, b : Int8, c : Int8) in
+            return SemigroupLaws.check(semigroup: Int8.productMonoid, a: a, b: b, c: c, eq: Int8.order)
+        }
+    }
+    
+    func testInt16SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : Int16, b : Int16, c : Int16) in
+            return SemigroupLaws.check(semigroup: Int16.sumMonoid, a: a, b: b, c: c, eq: Int16.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : Int16, b : Int16, c : Int16) in
+            return SemigroupLaws.check(semigroup: Int16.productMonoid, a: a, b: b, c: c, eq: Int16.order)
+        }
+    }
+    
+    func testInt32SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : Int32, b : Int32, c : Int32) in
+            return SemigroupLaws.check(semigroup: Int32.sumMonoid, a: a, b: b, c: c, eq: Int32.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : Int32, b : Int32, c : Int32) in
+            return SemigroupLaws.check(semigroup: Int32.productMonoid, a: a, b: b, c: c, eq: Int32.order)
+        }
+    }
+    
+    func testInt64SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : Int64, b : Int64, c : Int64) in
+            return SemigroupLaws.check(semigroup: Int64.sumMonoid, a: a, b: b, c: c, eq: Int64.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : Int64, b : Int64, c : Int64) in
+            return SemigroupLaws.check(semigroup: Int64.productMonoid, a: a, b: b, c: c, eq: Int64.order)
+        }
+    }
+    
+    func testUIntSemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : UInt, b : UInt, c : UInt) in
+            return SemigroupLaws.check(semigroup: UInt.sumMonoid, a: a, b: b, c: c, eq: UInt.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : UInt, b : UInt, c : UInt) in
+            return SemigroupLaws.check(semigroup: UInt.productMonoid, a: a, b: b, c: c, eq: UInt.order)
+        }
+    }
+    
+    func testUInt8SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : UInt8, b : UInt8, c : UInt8) in
+            return SemigroupLaws.check(semigroup: UInt8.sumMonoid, a: a, b: b, c: c, eq: UInt8.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : UInt8, b : UInt8, c : UInt8) in
+            return SemigroupLaws.check(semigroup: UInt8.productMonoid, a: a, b: b, c: c, eq: UInt8.order)
+        }
+    }
+    
+    func testUInt16SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : UInt16, b : UInt16, c : UInt16) in
+            return SemigroupLaws.check(semigroup: UInt16.sumMonoid, a: a, b: b, c: c, eq: UInt16.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : UInt16, b : UInt16, c : UInt16) in
+            return SemigroupLaws.check(semigroup: UInt16.productMonoid, a: a, b: b, c: c, eq: UInt16.order)
+        }
+    }
+    
+    func testUInt32SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : UInt32, b : UInt32, c : UInt32) in
+            return SemigroupLaws.check(semigroup: UInt32.sumMonoid, a: a, b: b, c: c, eq: UInt32.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : UInt32, b : UInt32, c : UInt32) in
+            return SemigroupLaws.check(semigroup: UInt32.productMonoid, a: a, b: b, c: c, eq: UInt32.order)
+        }
+    }
+    
+    func testUInt64SemigroupLaws() {
+        property("Sum semigroup laws") <- forAll { (a : UInt64, b : UInt64, c : UInt64) in
+            return SemigroupLaws.check(semigroup: UInt64.sumMonoid, a: a, b: b, c: c, eq: UInt64.order)
+        }
+        
+        property("Product semigroup laws") <- forAll { (a : UInt64, b : UInt64, c : UInt64) in
+            return SemigroupLaws.check(semigroup: UInt64.productMonoid, a: a, b: b, c: c, eq: UInt64.order)
+        }
+    }
 }
