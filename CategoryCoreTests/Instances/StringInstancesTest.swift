d0605912761bee76bd2efcf01f8df3d4db4c936e
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-20 16:28:28 +0100

    Test monoid laws for String concatenation

--- CategoryCoreTests/Instances/StringInstancesTest.swift
+++ CategoryCoreTests/Instances/StringInstancesTest.swift
@@ -20,6 +20,11 @@ class StringInstancesTest: XCTestCase {
         property("String concatenation semigroup") <- forAll { (a : String, b : String, c : String) in
             return SemigroupLaws.check(semigroup: String.concatMonoid, a: a, b: b, c: c, eq: String.order)
         }
-        
+    }
+    
+    func testMonoidLaws() {
+        property("String concatenation monoid") <- forAll { (a : String) in
+            return MonoidLaws.check(monoid: String.concatMonoid, a: a, eq: String.order)
+        }
     }
 }
