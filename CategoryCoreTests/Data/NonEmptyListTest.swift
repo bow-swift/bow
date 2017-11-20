7c8dbbed6c93cbb8124e3d5bdcf503b04ec5180a
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-20 15:05:37 +0100

    Test applicative laws for NonEmptyList

--- CategoryCoreTests/Data/NonEmptyListTest.swift
+++ CategoryCoreTests/Data/NonEmptyListTest.swift
@@ -15,11 +15,15 @@ class NonEmptyListTest: XCTestCase {
         return { a in NonEmptyList.pure(a) }
     }
     
+    func testEqLaws() {
+        EqLaws.check(eq: NonEmptyList.eq(Int.order), generator: self.generator)
+    }
+    
     func testFunctorLaws() {
         FunctorLaws<NonEmptyListF>.check(functor: NonEmptyList<Int>.functor(), generator: self.generator, eq: NonEmptyList<Int>.eq(Int.order))
     }
     
-    func testEqLaws() {
-        EqLaws.check(eq: NonEmptyList.eq(Int.order), generator: self.generator)
+    func testApplicativeLaws() {
+        ApplicativeLaws<NonEmptyListF>.check(applicative: NonEmptyList<Int>.applicative(), eq: NonEmptyList.eq(Int.order))
     }
 }
