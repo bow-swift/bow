6fba7bbaeb4825610fe7493eae99919a3316c38e
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-21 13:38:34 +0100

    Test functor laws for Function1

---
+++ CategoryCoreTests/Arrow/Function1Test.swift
@@ -0,0 +1,26 @@
+//
+//  Function1Test.swift
+//  CategoryCoreTests
+//
+//  Created by Tomás Ruiz López on 21/11/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryCore
+
+class Function1Test: XCTestCase {
+    
+    class Function1PointEq : Eq {
+        typealias A = HK2<Function1F, Int, Int>
+        
+        func eqv(_ a: HK<HK<Function1F, Int>, Int>, _ b: HK<HK<Function1F, Int>, Int>) -> Bool {
+            return Function1.ev(a).invoke(1) == Function1.ev(b).invoke(1)
+        }
+    }
+    
+    func testFunctorLaws() {
+        FunctorLaws<Function1Partial<Int>>.check(functor: Function1<Int, Int>.functor(), generator: { a in Function1<Int, Int>.pure(a) }, eq: Function1PointEq())
+    }
+    
+}
