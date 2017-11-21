0ec347260c2288b42e811975d02fb2b7bd60043b
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-21 12:36:01 +0100

    Test functor laws for StateT

---
+++ CategoryCoreTests/Transformers/StateTTest.swift
@@ -0,0 +1,32 @@
+//
+//  StateTTest.swift
+//  CategoryCoreTests
+//
+//  Created by Tomás Ruiz López on 21/11/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryCore
+
+class StateTTest: XCTestCase {
+    
+    class StateTUnitEq : Eq {
+        public typealias A = HK3<StateTF, IdF, Int, Int>
+        
+        public func eqv(_ a: HK3<StateTF, IdF, Int, Int>, _ b: HK3<StateTF, IdF, Int, Int>) -> Bool {
+            let x = StateT.ev(a).runM(1, Id<Any>.monad()).ev().value
+            let y = StateT.ev(b).runM(1, Id<Any>.monad()).ev().value
+            return x == y
+        }
+    }
+    
+    var generator : (Int) -> HK3<StateTF, IdF, Int, Int> {
+        return { a in StateT.lift(Id<Int>.pure(a), Id<Any>.monad()) }
+    }
+    
+    func testFunctorLaws() {
+        FunctorLaws<StateTPartial<IdF, Int>>.check(functor: StateT<IdF, Int, ()>.functor(Id<Any>.functor()), generator: self.generator, eq: StateTUnitEq())
+    }
+    
+}
