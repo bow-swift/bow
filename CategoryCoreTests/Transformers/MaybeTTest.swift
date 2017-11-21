cf071415799cad3774dd16b171950a4d53826552
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-21 12:02:40 +0100

    Test eq laws for MaybeT

---
+++ CategoryCoreTests/Transformers/MaybeTTest.swift
@@ -0,0 +1,24 @@
+//
+//  MaybeTTest.swift
+//  CategoryCoreTests
+//
+//  Created by Tomás Ruiz López on 21/11/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryCore
+
+class MaybeTTest: XCTestCase {
+    
+    var generator : (Int) -> HK2<MaybeTF, IdF, Int> {
+        return { a in MaybeT<IdF, Int>.pure(a, Id<Any>.applicative()) }
+    }
+    
+    var eq = MaybeT.eq(Id.eq(Maybe.eq(Int.order)), Id<Any>.functor())
+    
+    func testEqLaws() {
+        EqLaws.check(eq: self.eq, generator: self.generator)
+    }
+    
+}
