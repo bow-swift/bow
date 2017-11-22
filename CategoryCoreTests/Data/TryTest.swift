e9fa1b3d9392f0762611ca455bf83f614dbad773
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-22 11:26:45 +0100

    Test monad laws for Try

--- CategoryCoreTests/Data/TryTest.swift
+++ CategoryCoreTests/Data/TryTest.swift
@@ -26,4 +26,8 @@ class TryTest: XCTestCase {
     func testApplicativeLaws() {
         ApplicativeLaws<TryF>.check(applicative: Try<Int>.applicative(), eq: Try.eq(Int.order))
     }
+    
+    func testMonadLaws() {
+        MonadLaws<TryF>.check(monad: Try<Int>.monad(), eq: Try.eq(Int.order))
+    }
 }
