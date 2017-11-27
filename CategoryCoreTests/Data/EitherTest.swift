5506df4d2786e7a0bfb88144c995a20c074eb127
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-11-27 15:41:19 +0100

    Test Either utility methods

--- CategoryCoreTests/Data/EitherTest.swift
+++ CategoryCoreTests/Data/EitherTest.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Nimble
 @testable import CategoryCore
 
 class EitherTest: XCTestCase {
@@ -47,5 +48,67 @@ class EitherTest: XCTestCase {
     
     func testSemigroupKLaws() {
         SemigroupKLaws<EitherPartial<Int>>.check(semigroupK: Either<Int, Int>.semigroupK(), generator: self.generator, eq: self.eq)
+    }
+    
+    func testCheckers() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        
+        expect(left.isLeft).to(beTrue())
+        expect(left.isRight).to(beFalse())
+        expect(right.isLeft).to(beFalse())
+        expect(right.isRight).to(beTrue())
+    }
+    
+    func testSwap() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        
+        expect(Either.eq(Int.order, String.order).eqv(left.swap(), Either<Int, String>.right("Hello"))).to(beTrue())
+        expect(Either.eq(Int.order, String.order).eqv(right.swap(), Either<Int, String>.left(5))).to(beTrue())
+    }
+    
+    func testExists() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        let isPositive = { (x : Int) in x >= 0 }
+        
+        expect(left.exists(isPositive)).to(beFalse())
+        expect(right.exists(isPositive)).to(beTrue())
+        expect(right.exists(not <<< isPositive)).to(beFalse())
+    }
+    
+    func testToMaybe() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        
+        expect(Maybe.eq(Int.order).eqv(left.toMaybe(), Maybe<Int>.none())).to(beTrue())
+        expect(Maybe.eq(Int.order).eqv(right.toMaybe(), Maybe<Int>.some(5))).to(beTrue())
+    }
+    
+    func testGetOrElse() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        
+        expect(left.getOrElse(10)).to(be(10))
+        expect(right.getOrElse(10)).to(be(5))
+    }
+    
+    func testFilterOrElse() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        let isPositive = { (x : Int) in x >= 0 }
+        
+        expect(Either.eq(String.order, Int.order).eqv(left.filterOrElse(isPositive, "10"), Either<String, Int>.left("Hello"))).to(beTrue())
+        expect(Either.eq(String.order, Int.order).eqv(right.filterOrElse(isPositive, "10"), Either<String, Int>.right(5))).to(beTrue())
+        expect(Either.eq(String.order, Int.order).eqv(right.filterOrElse(not <<< isPositive, "10"), Either<String, Int>.left("10"))).to(beTrue())
+    }
+    
+    func testConversionToString() {
+        let left = Either<String, Int>.left("Hello")
+        let right = Either<String, Int>.right(5)
+        
+        expect(left.description).to(equal("Left(Hello)"))
+        expect(right.description).to(equal("Right(5)"))
     }
 }
