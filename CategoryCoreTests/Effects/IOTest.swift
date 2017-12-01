df44349bd4df1733ca5e3ef398ba5ab9371ae5f4
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-12-01 10:35:36 +0100

    Test functor laws for IO

---
+++ CategoryCoreTests/Effects/IOTest.swift
@@ -0,0 +1,22 @@
+//
+//  IOTest.swift
+//  CategoryCoreTests
+//
+//  Created by Tomás Ruiz López on 1/12/17.
+//  Copyright © 2017 Tomás Ruiz López. All rights reserved.
+//
+
+import XCTest
+@testable import CategoryCore
+
+class IOTest: XCTestCase {
+    
+    let generator = { (a : Int) in IO.pure(a) }
+    let eq = IO.eq(Int.order)
+    let eqUnit = IO.eq(UnitEq())
+    
+    func testFunctorLaws() {
+        FunctorLaws<IOF>.check(functor: IO<Int>.functor(), generator: self.generator, eq: self.eq, eqUnit: self.eqUnit)
+    }
+    
+}
