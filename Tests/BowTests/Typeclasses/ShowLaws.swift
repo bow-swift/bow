493e7634f12647ffc8b64633b21475a6c7c80496
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-04-17 09:51:08 +0200

    Add laws for Show

---
+++ Tests/BowTests/Typeclasses/ShowLaws.swift
@@ -0,0 +1,26 @@
+//
+//  ShowLaws.swift
+//  BowTests
+//
+//  Created by Tomás Ruiz López on 17/4/18.
+//
+
+import XCTest
+import SwiftCheck
+@testable import Bow
+
+class ShowLaws {
+    
+    static func check<Sh, A>(show : Sh, generator : @escaping (Int) -> A) where Sh : Show, Sh.A == A {
+        equality(show, generator)
+    }
+    
+    private static func equality<Sh, A>(_ show : Sh, _ generator : @escaping (Int) -> A) where Sh : Show, Sh.A == A {
+        property("Equal objects must show equal content") <- forAll { (a : Int) in
+            let x1 = generator(a)
+            let x2 = generator(a)
+            return show.show(x1) == show.show(x2)
+        }
+    }
+    
+}
