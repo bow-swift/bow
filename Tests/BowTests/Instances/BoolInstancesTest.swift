e6aab54f8075b04cb53e71792409811ec026d51d
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-05-03 13:41:13 +0200

    Test semigroup and monoid laws for and & or instances

---
+++ Tests/BowTests/Instances/BoolInstancesTest.swift
@@ -0,0 +1,34 @@
+//
+//  BoolInstancesTest.swift
+//  BowTests
+//
+//  Created by Tomás Ruiz López on 3/5/18.
+//
+
+import XCTest
+import SwiftCheck
+@testable import Bow
+
+class BoolInstancesTest: XCTestCase {
+    
+    func testBoolSemigroupLaws() {
+        property("And semigroup laws") <- forAll { (a : Bool, b : Bool, c : Bool) in
+            return SemigroupLaws.check(semigroup: Bool.andMonoid, a: a, b: b, c: c, eq: Bool.eq)
+        }
+        
+        property("Or semigroup laws") <- forAll { (a : Bool, b : Bool, c : Bool) in
+            return SemigroupLaws.check(semigroup: Bool.orMonoid, a: a, b: b, c: c, eq: Bool.eq)
+        }
+    }
+    
+    func testBoolMonoidLaws() {
+        property("And monoid laws") <- forAll { (a : Bool, b : Bool, c : Bool) in
+            return SemigroupLaws.check(semigroup: Bool.andMonoid, a: a, b: b, c: c, eq: Bool.eq)
+        }
+        
+        property("Or monoid laws") <- forAll { (a : Bool, b : Bool, c : Bool) in
+            return SemigroupLaws.check(semigroup: Bool.orMonoid, a: a, b: b, c: c, eq: Bool.eq)
+        }
+    }
+    
+}
