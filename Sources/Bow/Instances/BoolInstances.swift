5f69293b921b8666444b3196513346dcb79bdca6
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-05-03 13:39:07 +0200

    Add bool eq instance

--- Sources/Bow/Instances/BoolInstances.swift
+++ Sources/Bow/Instances/BoolInstances.swift
@@ -28,6 +28,14 @@ public class OrMonoid : OrSemigroup {
     }
 }
 
+public class BoolEq : Eq {
+    public typealias A = Bool
+    
+    public func eqv(_ a: Bool, _ b: Bool) -> Bool {
+        return a == b
+    }
+}
+
 public extension Bool {
     public static var andMonoid : AndMonoid {
         return AndMonoid()
@@ -35,5 +43,9 @@ public extension Bool {
     
     public static var orMonoid : OrMonoid {
         return OrMonoid()
+    }
+    
+    public static var eq : BoolEq {
+        return BoolEq()
     }
 }
