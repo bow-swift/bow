dfa0660d28c8504584d7e4dde41a474a87ee7369
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-06-21 11:23:04 +0200

    Add composition between Traversal and Prism

--- Sources/Bow/Optics/Prism.swift
+++ Sources/Bow/Optics/Prism.swift
@@ -36,6 +36,10 @@ public class PPrism<S, T, A, B> : PPrismOf<S, T, A, B> {
         return lhs.compose(rhs)
     }
     
+    public static func +<C, D>(lhs : PPrism<S, T, A, B>, rhs : PTraversal<A, B, C, D>) -> PTraversal<S, T, C, D> {
+        return lhs.compose(rhs)
+    }
+    
     public static func identity() -> Prism<S, S> {
         return Iso<S, S>.identity().asPrism()
     }
@@ -183,6 +187,10 @@ public class PPrism<S, T, A, B> : PPrismOf<S, T, A, B> {
     
     public func compose<C>(_ other : Fold<A, C>) -> Fold<S, C> {
         return self.asFold().compose(other)
+    }
+    
+    public func compose<C, D>(_ other : PTraversal<A, B, C, D>) -> PTraversal<S, T, C, D> {
+        return self.asTraversal().compose(other)
     }
     
     public func asOptional() -> POptional<S, T, A, B> {
