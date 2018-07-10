e9e08f1185b2bfaf189c6372cf7ef25c0d137d65
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-10 13:51:45 +0200

    Add static methods to create instances of At

--- Sources/Bow/Optics/Typeclasses/At.swift
+++ Sources/Bow/Optics/Typeclasses/At.swift
@@ -7,3 +7,32 @@ public protocol At : Typeclass {
     
     func at(_ i : I) -> Lens<S, A>
 }
+
+public extension At {
+    public static func at<AtType>(_ at : AtType, _ i : I) -> Lens<S, A> where AtType : At, AtType.S == S, AtType.I == I, AtType.A == A {
+        return at.at(i)
+    }
+    
+    public static func fromIso<U, AtType>(_ at : AtType, _ iso : Iso<S, U>) -> AtFromIso<S, I, A, U, AtType> where AtType : At, AtType.S == U, AtType.I == I, AtType.A == A {
+        return AtFromIso(at: at, iso: iso)
+    }
+}
+
+public class AtFromIso<M, N, O, P, AtType> : At where AtType : At, AtType.S == P, AtType.I == N, AtType.A == O {
+    public typealias S = M
+    public typealias I = N
+    public typealias A = O
+    
+    private let atInstance : AtType
+    private let iso : Iso<M, P>
+    
+    public init(at : AtType, iso : Iso<M, P>) {
+        self.atInstance = at
+        self.iso = iso
+    }
+    
+    public func at(_ i: N) -> Lens<M, O> {
+        return iso + atInstance.at(i)
+    }
+    
+}
