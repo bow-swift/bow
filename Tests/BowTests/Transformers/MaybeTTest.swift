040ccaf6ab2152e5bb50f81e52bea27efbe11ee2
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-11 16:43:02 +0200

    Test semigroup and monoid algebra for MaybeT

--- Tests/BowTests/Transformers/MaybeTTest.swift
+++ Tests/BowTests/Transformers/MaybeTTest.swift
@@ -1,4 +1,5 @@
 import XCTest
+import SwiftCheck
 @testable import Bow
 
 class MaybeTTest: XCTestCase {
@@ -30,8 +31,26 @@ class MaybeTTest: XCTestCase {
         SemigroupKLaws<MaybeTPartial<ForId>>.check(semigroupK: MaybeT<ForId, Int>.semigroupK(Id<Any>.monad()), generator: self.generator, eq: self.eq)
     }
     
+    func testSemigroupLaws() {
+        property("Semigroup laws") <- forAll { (a : Int, b : Int, c : Int) in
+            return SemigroupLaws.check(semigroup: MaybeT<ForId, Int>.semigroupK(Id<Int>.monad()).algebra(),
+                                       a: MaybeT<ForId, Int>.pure(a, Id<Int>.applicative()),
+                                       b: MaybeT<ForId, Int>.pure(b, Id<Int>.applicative()),
+                                       c: MaybeT<ForId, Int>.pure(c, Id<Int>.applicative()),
+                                       eq: self.eq)
+        }
+    }
+    
     func testMonoidKLaws() {
         MonoidKLaws<MaybeTPartial<ForId>>.check(monoidK: MaybeT<ForId, Int>.monoidK(Id<Any>.monad()), generator: self.generator, eq: self.eq)
+    }
+    
+    func testMonoidLaws() {
+        property("Monoid laws") <- forAll { (a : Int) in
+            return MonoidLaws.check(monoid: MaybeT<ForId, Int>.monoidK(Id<Int>.monad()).algebra(),
+                                    a: MaybeT<ForId, Int>.pure(a, Id<Int>.applicative()),
+                                    eq: self.eq)
+        }
     }
     
     func testFunctorFilterLaws() {
