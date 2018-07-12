383f79585eea5d7dd125bc89dd3729934df5ac15
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-12 12:12:51 +0200

    Test ApplicativeError laws for Validated

--- Tests/BowTests/Data/ValidatedTest.swift
+++ Tests/BowTests/Data/ValidatedTest.swift
@@ -5,7 +5,7 @@ import SwiftCheck
 class ValidatedTest: XCTestCase {
     
     var generator : (Int) -> Validated<Int, Int> {
-        return { a in (a % 2 == 0) ? Validated.valid(a) : Validated.invalid(a) }
+        return { a in (a % 2 == 0) ? Validated.pure(a) : Validated.invalid(a) }
     }
     
     let eq = Validated.eq(Int.order, Int.order)
@@ -44,5 +44,57 @@ class ValidatedTest: XCTestCase {
     
     func testFoldableLaws() {
         FoldableLaws<ValidatedPartial<Int>>.check(foldable: Validated<Int, Int>.foldable(), generator: self.generator)
+    }
+    
+    func testApplicativeErrorLaws() {
+        ApplicativeErrorLaws<ValidatedPartial<CategoryError>, CategoryError>.check(
+            applicativeError: Validated<CategoryError, Int>.applicativeError(CategoryError.semigroup),
+            eq: Validated.eq(CategoryError.eq, Int.order),
+            eqEither: Validated.eq(CategoryError.eq, Either.eq(CategoryError.eq, Int.order)),
+            gen: { CategoryError.common })
+    }
+    
+    func testCheckers() {
+        property("valid and invalid are mutually exclusive") <- forAll { (x : Int) in
+            let input = self.generator(x)
+            return xor(input.isValid, input.isInvalid)
+        }
+    }
+    
+    func testConversionConsistency() {
+        property("Consistency fromMaybe - toMaybe") <- forAll { (x : Int?, none : String) in
+            let maybe = Maybe<Int>.fromOption(x)
+            let validated = Validated.fromMaybe(maybe, ifNone: constant(none))
+            return Maybe<Int>.eq(Int.order).eqv(validated.toMaybe(), maybe)
+        }
+        
+        property("Consistency fromEither - toEither") <- forAll { (x : Int) in
+            let either = x % 2 == 0 ? Either.left(x) : Either.right(x)
+            let validated = Validated.fromEither(either)
+            return Either<Int, Int>.eq(Int.order, Int.order).eqv(validated.toEither(), either)
+        }
+        
+        property("Consistency fromTry - toList") <- forAll { (x : Int) in
+            let attempt = x % 2 == 0 ? Try.success(x) : Try.failure(TryError.illegalState)
+            let validated = Validated<TryError, Int>.fromTry(attempt)
+            return (validated.isValid && validated.toList() == [x]) ||
+                    (validated.isInvalid && validated.toList() == [])
+        }
+    }
+    
+    func testBimapEquivalence() {
+        property("bimap is equivalent to map and leftMap") <- forAll { (x : Int, f : ArrowOf<Int, Int>, g : ArrowOf<Int, Int>) in
+            let input = self.generator(x)
+            return self.eq.eqv(input.bimap(f.getArrow, g.getArrow),
+                               input.map(g.getArrow).leftMap(f.getArrow))
+        }
+    }
+    
+    func testSwapIsomorphism() {
+        property("swap twice is equivalent to id") <- forAll { (x : Int) in
+            let input = self.generator(x)
+            return self.eq.eqv(input.swap().swap(),
+                               input)
+        }
     }
 }
