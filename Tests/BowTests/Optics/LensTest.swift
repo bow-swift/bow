29f14cff1c6c0a44e0770190c6aebc16323108ab
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-18 15:53:56 +0200

    Test Lens properties

--- Tests/BowTests/Optics/LensTest.swift
+++ Tests/BowTests/Optics/LensTest.swift
@@ -1,4 +1,5 @@
 import XCTest
+import SwiftCheck
 @testable import Bow
 
 class LensTest: XCTestCase {
@@ -17,5 +18,97 @@ class LensTest: XCTestCase {
     
     func testTraversalLaws() {
         TraversalLaws.check(traversal: tokenLens.asTraversal(), eqA: Token.eq, eqB: String.order)
+    }
+    
+    func testLensAsFold() {
+        property("Lens as Fold: size") <- forAll { (token : Token) in
+            return tokenLens.asFold().size(token) == 1
+        }
+        
+        property("Lens as Fold: nonEmpty") <- forAll { (token : Token) in
+            return tokenLens.asFold().nonEmpty(token)
+        }
+        
+        property("Lens as Fold: isEmpty") <- forAll { (token : Token) in
+            return !tokenLens.asFold().isEmpty(token)
+        }
+        
+        property("Lens as Fold: getAll") <- forAll { (token : Token) in
+            return ListK.eq(String.order).eqv(tokenLens.asFold().getAll(token),
+                                              ListK.pure(token.value))
+        }
+        
+        property("Lens as Fold: combineAll") <- forAll { (token : Token) in
+            return tokenLens.asFold().combineAll(String.concatMonoid, token) == token.value
+        }
+        
+        property("Lens as Fold: headMaybe") <- forAll { (token : Token) in
+            return Maybe.eq(String.order).eqv(tokenLens.asFold().headMaybe(token),
+                                              Maybe.some(token.value))
+        }
+        
+        property("Lens as Fold: lastMaybe") <- forAll { (token : Token) in
+            return Maybe.eq(String.order).eqv(tokenLens.asFold().lastMaybe(token),
+                                              Maybe.some(token.value))
+        }
+    }
+    
+    func testLensAsGetter() {
+        property("Lens as Getter: get") <- forAll { (token : Token) in
+            return tokenLens.asGetter().get(token) == tokenGetter.get(token)
+        }
+        
+        property("Lens as Getter: find") <- forAll { (token : Token, predicate : ArrowOf<String, Bool>) in
+            return Maybe.eq(String.order).eqv(tokenLens.asGetter().find(token, predicate.getArrow),
+                                              tokenGetter.find(token, predicate.getArrow))
+        }
+        
+        property("Lens as Getter: exists") <- forAll { (token : Token, predicate : ArrowOf<String, Bool>) in
+            return tokenLens.asGetter().exists(token, predicate.getArrow) ==
+                tokenGetter.exists(token, predicate.getArrow)
+        }
+    }
+    
+    func testLensProperties() {
+        property("Lifting a function should yield the same result as not yielding") <- forAll { (token : Token, value : String) in
+            return Token.eq.eqv(tokenLens.set(token, value),
+                                tokenLens.lift(constant(value))(token))
+        }
+        
+        property("Lifting a function as a functor should yield the same result as not yielding") <- forAll { (token : Token, value : String) in
+            return Maybe.eq(Token.eq).eqv(tokenLens.modifyF(Maybe<String>.functor(), token, constant(Maybe.some(value))),
+                                          tokenLens.liftF(Maybe<String>.functor(), constant(Maybe.some(value)))(token))
+        }
+        
+        property("Finding a target using a predicate within a Lens should be wrapped in the correct option result") <- forAll { (predicate : Bool) in
+            return tokenLens.find(Token(value: "Any value"), constant(predicate)).fold(constant(false), constant(true)) == predicate
+        }
+        
+        property("Checking existence predicate over the target should result in same result as predicate") <- forAll { (predicate : Bool) in
+            return tokenLens.exists(Token(value: "Any value"), constant(predicate)) == predicate
+        }
+        
+        property("Joining two lenses with the same target should yield same result") <- forAll { (tokenValue : String) in
+            let token = Token(value: tokenValue)
+            let user = User(token: token)
+            let userTokenStringLens = userLens + tokenLens
+            let joinedLens = tokenLens.choice(userTokenStringLens)
+            return joinedLens.get(Either.left(token)) == joinedLens.get(Either.right(user))
+        }
+        
+        property("Pairing two disjoint lenses should yield a pair of their results") <- forAll { (token : Token, user : User) in
+            let split = userLens.split(tokenLens)
+            return split.get((user, token)) == (user.token, token.value)
+        }
+        
+        property("Creating a first pair with a type should result in the target to value") <- forAll { (token : Token, value : Int) in
+            let first : Lens<(Token, Int), (String, Int)> = tokenLens.first()
+            return first.get((token, value)) == (token.value, value)
+        }
+        
+        property("Creating a second pair with a type should result in the value to target") <- forAll { (token : Token, value : Int) in
+            let second : Lens<(Int, Token), (Int, String)> = tokenLens.second()
+            return second.get((value, token)) == (value, token.value)
+        }
     }
 }
