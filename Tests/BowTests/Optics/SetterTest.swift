217ebcb9776e3864008f6283da2f61a48d07e400
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-19 09:32:29 +0200

    Test Setter properties

--- Tests/BowTests/Optics/SetterTest.swift
+++ Tests/BowTests/Optics/SetterTest.swift
@@ -1,4 +1,5 @@
 import XCTest
+import SwiftCheck
 @testable import Bow
 
 class SetterTest: XCTestCase {
@@ -8,4 +9,18 @@ class SetterTest: XCTestCase {
         SetterLaws.check(setter: Setter<String, String>.identity(), eqA: String.order)
     }
     
+    func testSetterProperties() {
+        property("Joining two Setters together with same target should yield same result") <- forAll { (value : String) in
+            let userTokenStringSetter = userSetter + tokenSetter
+            let joinedSetter = tokenSetter.choice(userTokenStringSetter)
+            let oldValue = "Old value"
+            let token = Token(value: oldValue)
+            let user = User(token: token)
+            return joinedSetter.set(Either.left(token), value).swap().getOrElse(Token(value: "Wrong value")).value == joinedSetter.set(Either.right(user), value).getOrElse(User(token: Token(value: "Wrong value"))).token.value
+        }
+        
+        property("Lifting a function should yield the same result as direct modify") <- forAll { (token : Token, value : String) in
+            return tokenSetter.modify(token, constant(value)) == tokenSetter.lift(constant(value))(token)
+        }
+    }
 }
