efc4f6819ae211658989973226a8be2a677f88c1
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-20 11:45:33 +0200

    Provide instances of Recursive, Corecursive and Birecursive for Fix

--- Sources/Bow/RecursionSchemes/Data/Fix.swift
+++ Sources/Bow/RecursionSchemes/Data/Fix.swift
@@ -20,3 +20,29 @@ public extension Kind where F == ForFix {
         return Fix<A>.fix(self)
     }
 }
+
+public extension Fix {
+    public static func recursive() -> FixBirecursive {
+        return FixBirecursive()
+    }
+    
+    public static func corecursive() -> FixBirecursive {
+        return FixBirecursive()
+    }
+    
+    public static func birecursive() -> FixBirecursive {
+        return FixBirecursive()
+    }
+}
+
+public class FixBirecursive : Birecursive {
+    public typealias T = ForFix
+    
+    public func projectT<F, Func>(_ tf : Kind<ForFix, F>, _ functor : Func) -> Kind<F, Kind<ForFix, F>> where Func : Functor, Func.F == F {
+        return functor.map(tf.fix().unFix, { x in x.value() })
+    }
+    
+    public func embedT<Func, F>(_ tf : Kind<F, Eval<Kind<ForFix, F>>>, _ functor : Func) -> Eval<Kind<ForFix, F>> where Func : Functor, Func.F == F {
+        return Eval.later { Fix(unFix: tf) }
+    }
+}
