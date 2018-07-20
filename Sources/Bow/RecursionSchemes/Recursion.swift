d87c87262fa239ae56ac96118fa60a1d46e7c8c9
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-07-20 11:04:49 +0200

    Extend Functor with hylo function

--- Sources/Bow/RecursionSchemes/Recursion.swift
+++ Sources/Bow/RecursionSchemes/Recursion.swift
@@ -2,3 +2,14 @@ import Foundation
 
 public typealias Algebra<F, A> = (Kind<F, A>) -> A
 public typealias Coalgebra<F, A> = (A) -> Kind<F, A>
+
+public extension Functor {
+    public func hylo<A, B>(_ algebra : @escaping Algebra<F, Eval<B>>,
+                           _ coalgebra : @escaping Coalgebra<F, A>,
+                           _ a : A) -> B {
+        func h(_ a : A) -> Eval<B> {
+            return algebra(map(coalgebra(a), { x in Eval.deferEvaluation({ h(x) }) }))
+        }
+        return h(a).value()
+    }
+}
