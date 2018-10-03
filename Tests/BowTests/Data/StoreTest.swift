8a5bf4916f6d6cad94d37d0ae860e5dc5f50eb9a
Tomás Ruiz-López <trl.ironside@gmail.com>
2018-10-03 10:50:39 +0200

    Add tests for Store

---
+++ Tests/BowTests/Data/StoreTest.swift
@@ -0,0 +1,38 @@
+import XCTest
+import Nimble
+@testable import Bow
+
+class StoreTest : XCTestCase {
+    let intStore = { (x : Int) in Store(state: x, render: id) }
+    
+    class StoreEq : Eq {
+        typealias A = StoreOf<Int, Int>
+        
+        func eqv(_ a: StoreOf<Int, Int>, _ b: StoreOf<Int, Int>) -> Bool {
+            return Store<Int, Int>.fix(a).extract() == Store<Int, Int>.fix(b).extract()
+        }
+    }
+    
+    let greetingStore = { (name : String) in Store(state: name, render: { name in "Hi \(name)!"}) }
+    
+    func testExtractRendersCurrentState() {
+        let result = greetingStore("Bow")
+        expect(result.extract()).to(equal("Hi Bow!"))
+    }
+    
+    func testCoflatMapCreatesNewStore() {
+        let result = greetingStore("Bow").coflatMap { (store) -> String in
+            if store.state == "Bow" {
+                return "This is my master"
+            } else {
+                return "This is not my master"
+            }
+        }
+        expect(result.extract()).to(equal("This is my master"))
+    }
+    
+    func testMapModifiesRenderResult() {
+        let result = greetingStore("Bow").map { str in str.uppercased() }
+        expect(result.extract()).to(equal("HI BOW!"))
+    }
+}
