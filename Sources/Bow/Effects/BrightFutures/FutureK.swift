9abb93937752d860655c033961fc2244ea087b44
Tomás Ruiz-López <truizlop@gmail.com>
2018-10-08 12:41:55 +0200

    Add instances for FutureK

--- Sources/Bow/Effects/BrightFutures/FutureK.swift
+++ Sources/Bow/Effects/BrightFutures/FutureK.swift
@@ -88,3 +88,116 @@ public class FutureK<E, A> : FutureKOf<E, A> where E : Error {
             .recoverWith { e in FutureK<E, Unit>.fix(callback(Either.left(e))).value }.k()
     }
 }
+
+public extension FutureK {
+    public static func functor() -> FutureKFunctor<E> {
+        return FutureKFunctor<E>()
+    }
+    
+    public static func applicative() -> FutureKApplicative<E> {
+        return FutureKApplicative<E>()
+    }
+    
+    public static func monad() -> FutureKMonad<E> {
+        return FutureKMonad<E>()
+    }
+    
+    public static func applicativeError() -> FutureKApplicativeError<E> {
+        return FutureKApplicativeError<E>()
+    }
+    
+    public static func monadError() -> FutureKMonadError<E> {
+        return FutureKMonadError<E>()
+    }
+    
+    public static func monadDefer() -> FutureKMonadDefer<E> {
+        return FutureKMonadDefer<E>()
+    }
+    
+    public static func async() -> FutureKAsync<E> {
+        return FutureKAsync<E>()
+    }
+    
+    public static func effect() -> FutureKEffect<E> {
+        return FutureKEffect<E>()
+    }
+}
+
+public class FutureKFunctor<E> : Functor where E : Error {
+    public typealias F = FutureKPartial<E>
+    
+    public func map<A, B>(_ fa: FutureKOf<E, A>, _ f: @escaping (A) -> B) -> FutureKOf<E, B> {
+        return FutureK<E, A>.fix(fa).map(f)
+    }
+}
+
+public class FutureKApplicative<E> : FutureKFunctor<E>, Applicative where E : Error {
+    public func pure<A>(_ a: A) -> FutureKOf<E, A> {
+        return FutureK<E, A>.pure(a)
+    }
+    
+    public func ap<A, B>(_ fa: FutureKOf<E, A>, _ ff: FutureKOf<E, (A) -> B>) -> FutureKOf<E, B> {
+        return FutureK<E, A>.fix(fa).ap(ff)
+    }
+}
+
+public class FutureKMonad<E> : FutureKApplicative<E>, Monad where E : Error {
+    public func flatMap<A, B>(_ fa: FutureKOf<E, A>, _ f: @escaping (A) -> FutureKOf<E, B>) -> FutureKOf<E, B> {
+        return FutureK<E, A>.fix(fa).flatMap(f)
+    }
+    
+    public func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> FutureKOf<E, Either<A, B>>) -> FutureKOf<E, B> {
+        return FutureK<E, A>.tailRecM(a, f)
+    }
+}
+
+public class FutureKApplicativeError<Err> : FutureKApplicative<Err>, ApplicativeError where Err : Error {
+    public typealias E = Err
+    
+    public func raiseError<A>(_ e: Err) -> FutureKOf<E, A> {
+        return FutureK<E, A>.raiseError(e)
+    }
+    
+    public func handleErrorWith<A>(_ fa: FutureKOf<E, A>, _ f: @escaping (Err) -> FutureKOf<E, A>) -> FutureKOf<E, A> {
+        return FutureK<E, A>.fix(fa).handleErrorWith(f)
+    }
+}
+
+public class FutureKMonadError<Err> : FutureKMonad<Err>, MonadError where Err : Error {
+    public typealias E = Err
+    
+    public func raiseError<A>(_ e: Err) -> FutureKOf<E, A> {
+        return FutureK<E, A>.raiseError(e)
+    }
+    
+    public func handleErrorWith<A>(_ fa: FutureKOf<E, A>, _ f: @escaping (Err) -> FutureKOf<E, A>) -> FutureKOf<E, A> {
+        return FutureK<E, A>.fix(fa).handleErrorWith(f)
+    }
+}
+
+public class FutureKMonadDefer<Err> : FutureKMonadError<Err>, MonadDefer where Err : Error {
+    public func suspend<A>(_ fa: @escaping () -> FutureKOf<E, A>) -> FutureKOf<E, A> {
+        return FutureK<E, A>.suspend(fa)
+    }
+}
+
+public class FutureKAsync<Err> : FutureKMonadDefer<Err>, Async where Err : Error {
+    public func runAsync<A>(_ fa: @escaping ((Either<Error, A>) -> Unit) throws -> Unit) -> FutureKOf<E, A> {
+        return Future { complete in
+            do {
+                try fa { either in
+                    either.fold({ e in complete(.failure(e as! E)) },
+                                { a in complete(.success(a)) })
+                }
+            } catch {}
+        }.k()
+    }
+}
+
+public class FutureKEffect<Err> : FutureKAsync<Err>, Effect where Err : Error {
+    public func runAsync<A>(_ fa: FutureKOf<E, A>, _ callback: @escaping (Either<Error, A>) -> FutureKOf<E, Unit>) -> FutureKOf<E, Unit> {
+        return FutureK<E, A>.fix(fa).value
+            .flatMap { a in FutureK<E, Unit>.fix(callback(Either.right(a))).value }
+            .recoverWith { e in FutureK<E, Unit>.fix(callback(Either.left(e))).value }.k()
+    }
+}
