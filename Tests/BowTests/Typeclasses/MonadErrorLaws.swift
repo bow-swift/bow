22860ebe9c32598bbd94ce0e145138ddfa362ba4
Tomás Ruiz-López <truizlop@gmail.com>
2018-11-08 12:25:18 +0100

    Use ArrowOf in MonadErrorLaws

--- Tests/BowTests/Typeclasses/MonadErrorLaws.swift
+++ Tests/BowTests/Typeclasses/MonadErrorLaws.swift
@@ -10,9 +10,9 @@ class MonadErrorLaws<F, E> {
     }
     
     private static func leftZero<MonErr, EqF>(_ monadError : MonErr, _ eq : EqF, _ gen : @escaping () -> E) where MonErr : MonadError, MonErr.F == F, MonErr.E == E, EqF : Eq, EqF.A == Kind<F, Int> {
-        property("Left zero") <- forAll { (a : Int) in
+        property("Left zero") <- forAll { (a : Int, g : ArrowOf<Int, Int>) in
             let error = gen()
-            let f = { (_ : Int) in monadError.pure(a) }
+            let f = g.getArrow >>> monadError.pure
             return eq.eqv(monadError.flatMap(monadError.raiseError(error), f),
                           monadError.raiseError(error))
         }
@@ -22,7 +22,7 @@ class MonadErrorLaws<F, E> {
         property("Ensure consistency") <- forAll { (a : Int, p : ArrowOf<Int, Bool>) in
             let error = gen()
             let fa = monadError.pure(a)
-            return eq.eqv(monadError.ensure(fa, error: { error }, predicate: p.getArrow),
+            return eq.eqv(monadError.ensure(fa, error: constant(error), predicate: p.getArrow),
                           monadError.flatMap(fa, { a in p.getArrow(a) ? monadError.pure(a) : monadError.raiseError(error) }))
         }
     }
