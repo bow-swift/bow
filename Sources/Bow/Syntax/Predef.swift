09c2f478f6be69493532ca0cb2a05b45832a4099
Tomás Ruiz-López <truizlop@gmail.com>
2018-11-12 16:39:58 +0100

    Add missing public modifier to some functions

--- Sources/Bow/Syntax/Predef.swift
+++ Sources/Bow/Syntax/Predef.swift
@@ -7,23 +7,23 @@ public func id<A>(_ a : A) -> A {
     return a
 }
 
-func constant<A>(_ a : A) -> () -> A {
+public func constant<A>(_ a : A) -> () -> A {
     return { a }
 }
 
-func constant<A, B>(_ a : A) -> (B) -> A {
+public func constant<A, B>(_ a : A) -> (B) -> A {
     return { _ in a }
 }
 
-func constant<A, B, C>(_ a : A) -> (B, C) -> A {
+public func constant<A, B, C>(_ a : A) -> (B, C) -> A {
     return { _, _ in a }
 }
 
-func constant<A, B, C, D>(_ a : A) -> (B, C, D) -> A {
+public func constant<A, B, C, D>(_ a : A) -> (B, C, D) -> A {
     return { _, _, _ in a }
 }
 
-func constant<A, B, C, D, E>(_ a : A) -> (B, C, D, E) -> A {
+public func constant<A, B, C, D, E>(_ a : A) -> (B, C, D, E) -> A {
     return { _, _, _, _ in a }
 }
 
