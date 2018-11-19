8125414dddda727b418dd50146415ace05f4286f
Tomás Ruiz-López <truizlop@gmail.com>
2018-11-19 13:13:39 +0100

    Add utility methods to avoid passing instances in State

--- Sources/Bow/Data/State.swift
+++ Sources/Bow/Data/State.swift
@@ -4,6 +4,9 @@ public typealias StatePartial<S> = StateTPartial<ForId, S>
 public typealias StateOf<S, A> = StateT<ForId, S, A>
 
 public class State<S, A> : StateOf<S, A> {
+    public static func fix(_ value : StateOf<S, A>) -> State<S, A> {
+        return value as! State<S, A>
+    }
     
     public init(_ run : @escaping (S) -> (S, A)) {
         super.init(Id.pure({ s in Id.pure(run(s)) }))
@@ -19,6 +22,22 @@ public class State<S, A> : StateOf<S, A> {
     
     public func runS(_ s : S) -> S {
         return run(s).0
+    }
+    
+    public func map<B>(_ f : @escaping (A) -> B) -> StateOf<S, B> {
+        return self.map(f, Id<A>.functor())
+    }
+    
+    public func ap<B>(_ ff : StateOf<S, (A) -> B>) -> StateOf<S, B> {
+        return self.ap(ff, Id<A>.monad())
+    }
+    
+    public func flatMap<B>(_ f : @escaping (A) -> StateOf<S, B>) -> StateOf<S, B> {
+        return self.flatMap(f, Id<A>.monad())
+    }
+    
+    public func product<B>(_ sb : State<S, B>) -> StateOf<S, (A, B)> {
+        return self.product(sb, Id<A>.monad())
     }
 }
 
