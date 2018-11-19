6f913b5539f1fabc0415896f7d091eb738f97f71
Tomás Ruiz-López <truizlop@gmail.com>
2018-11-19 12:44:09 +0100

    Add typealias for State

--- Sources/Bow/Data/State.swift
+++ Sources/Bow/Data/State.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-public class State<S, A> : StateT<ForId, S, A> {
+public typealias StatePartial<S> = StateTPartial<ForId, S>
+public typealias StateOf<S, A> = StateT<ForId, S, A>
+
+public class State<S, A> : StateOf<S, A> {
     
     public init(_ run : @escaping (S) -> (S, A)) {
         super.init(Id.pure({ s in Id.pure(run(s)) }))
