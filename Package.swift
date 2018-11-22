e2d704825a52a5241a10ef96e8487b1e1fd580f1
Juan Cazalla Estrella <juan.cazalla@47deg.com>
2018-11-22 19:29:58 +0100

    Updates targets

--- Package.swift
+++ Package.swift
@@ -22,13 +22,25 @@ let package = Package(
         .package(url: "https://github.com/Thomvis/BrightFutures", from: "7.0.0"),
     ],
     targets: [
-        .target(
-            name: "Bow",
-            dependencies: ["RxSwift", "RxCocoa", "Result", "BrightFutures"],
-            path: "Sources"),
-        .testTarget(
-            name: "BowTests",
-            dependencies: ["Bow", "SwiftCheck", "Nimble"],
-            path: "Tests")
+        .target(name: "Bow", dependencies: []),
+        .testTarget(name: "BowTests", dependencies: ["Bow", "BowLaws", "SwiftCheck", "Nimble"]),
+        .target(name:"BowOptics", dependencies: ["Bow"]),
+        .testTarget(name: "BowOpticsTests", dependencies: ["BowOptics", "Bow", "SwiftCheck"]),
+        .target(name:"BowRecursionSchemes", dependencies: ["Bow"]),
+        .testTarget(name: "BowRecursionSchemesTests", dependencies: ["BowRecursionSchemes", "Bow", "BowLaws", "SwiftCheck", "Nimble"]),
+        .target(name:"BowFree", dependencies: ["Bow"]),
+        .testTarget(name: "BowFreeTests", dependencies: ["BowFree", "Bow", "BowLaws", "SwiftCheck", "Nimble"]),
+        .target(name:"BowGeneric", dependencies: ["Bow"]),
+        .testTarget(name: "BowGenericTests", dependencies: ["BowGeneric", "Bow"]),
+        .target(name:"BowEffects", dependencies: ["Bow"]),
+        .testTarget(name: "BowEffectsTests", dependencies: ["BowEffects", "BowEffectsLaws", "Bow", "BowLaws", "SwiftCheck", "Nimble"]),
+        .target(name:"BowResult", dependencies: ["Result", "Bow"]),
+        .testTarget(name: "BowResultTests", dependencies: ["BowResult", "Result", "Bow", "SwiftCheck"]),
+        .target(name:"BowRx", dependencies: ["RxSwift", "RxCocoa", "Bow", "BowEffects"]),
+        .testTarget(name: "BowRxTests", dependencies: ["BowRx", "RxSwift", "RxCocoa", "Bow", "BowLaws", "BowEffects", "BowEffectsLaws", "SwiftCheck", "Nimble"]),
+        .target(name:"BowBrightFutures", dependencies: ["BrightFutures", "Bow", "BowEffects", "BowResult"]),
+        .testTarget(name: "BowBrightFuturesTests", dependencies: ["BowBrightFutures", "BrightFutures", "Bow",  "BowLaws", "BowEffects", "BowEffectsLaws", "BowResult", "Result", "SwiftCheck", "Nimble"]),
+        .testTarget(name:"BowLaws", dependencies: ["Bow", "SwiftCheck", "Nimble"]),
+        .testTarget(name:"BowEffectsLaws", dependencies: ["Bow", "BowEffects", "SwiftCheck", "Nimble"]),
     ]
 )
