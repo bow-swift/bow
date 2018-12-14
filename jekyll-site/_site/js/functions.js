c74f012ae2f3165908aa31885308770cf7bb0c15
Israel <israel.perez.glez@gmail.com>
2018-12-14 11:31:59 +0100

    gitignore

--- jekyll-site/_site/js/functions.js
+++
@@ -1,8 +0,0 @@
-function w3_open() {
-    "use strict";
-    document.getElementById("sidebar").style.display = "block";
-}
-function w3_close() {
-    "use strict";
-    document.getElementById("sidebar").style.display = "none";
-}
