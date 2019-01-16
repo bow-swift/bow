9b94fbed9e45a9f1fc04b4a123f2c06b12e48360
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-16 12:39:40 +0100

    Add docs to StringInstances

--- Sources/Bow/Instances/StringInstances.swift
+++ Sources/Bow/Instances/StringInstances.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/**
+ Instance of `Semigroup` for `String`, using concatenation as combination method.
+ */
 public class StringConcatSemigroup : Semigroup {
     public typealias A = String
     
@@ -8,12 +11,18 @@ public class StringConcatSemigroup : Semigroup {
     }
 }
 
+/**
+ Instance of `Monoid` for `String`, using concatenation as combination method.
+ */
 public class StringConcatMonoid : StringConcatSemigroup, Monoid {
     public var empty : String {
         return ""
     }
 }
 
+/**
+ Instance of `Eq` for `String`.
+ */
 public class StringEq : Eq {
     public typealias A = String
     
@@ -22,6 +31,9 @@ public class StringEq : Eq {
     }
 }
 
+/**
+ Instance of `Order` for `String`.
+ */
 public class StringOrder : StringEq, Order {
     public func compare(_ a: String, _ b: String) -> Int {
         switch a.compare(b) {
@@ -33,18 +45,22 @@ public class StringOrder : StringEq, Order {
 }
 
 public extension String {
+    /// Provides an instance of `Semigroup`.
     public static var concatSemigroup : StringConcatSemigroup {
         return StringConcatSemigroup()
     }
     
+    /// Provides an instance of `Monoid`.
     public static var concatMonoid : StringConcatMonoid {
         return StringConcatMonoid()
     }
     
+    /// Provides an instance of `Eq`.
     public static var eq : StringEq {
         return StringEq()
     }
     
+    /// Provides an instance of `Order`.
     public static var order : StringOrder {
         return StringOrder()
     }
