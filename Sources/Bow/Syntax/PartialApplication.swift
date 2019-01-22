9613a16f37f9c40f55b73a671d4734d7fa3c236f
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-22 17:14:16 +0100

    Add API docs to Syntax group (#110)
    * Add docs to BooleanFunctions
    
    * Add docs to Reverse
    
    * Add docs to Curry
    
    * Add docs to Memoization
    
    * Add docs to Predef
    
    * Add docs to PartialApplication
    
    * Add docs to Typeclass
    
    * Add docs to Kind
    
    * Update Sources/Bow/Syntax/Curry.swift
    
    Co-Authored-By: truizlop <truizlop@gmail.com>

--- Sources/Bow/Syntax/PartialApplication.swift
+++ Sources/Bow/Syntax/PartialApplication.swift
@@ -2,34 +2,58 @@ import Foundation
 
 infix operator |> : AdditionPrecedence
 
+/**
+ Applies an argument to a 1-ary function.
+ */
 public func |><A, B>(_ a : A, _ fun : (A) -> B) -> B {
     return fun(a)
 }
 
+/**
+ Applies the first argument to a 2-ary function, returning a 1-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C>(_ a : A, _ fun : @escaping (A, B) -> C) -> (B) -> C {
     return { b in fun(a,b) }
 }
 
+/**
+ Applies the first argument to a 3-ary function, returning a 2-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D>(_ a : A, _ fun : @escaping (A, B, C) -> D) -> (B, C) -> D {
     return { b, c in fun(a, b, c) }
 }
 
+/**
+ Applies the first argument to a 4-ary function, returning a 3-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D, E>(_ a : A, _ fun : @escaping (A, B, C, D) -> E) -> (B, C, D) -> E {
     return { b, c, d in fun(a, b, c, d) }
 }
 
+/**
+ Applies the first argument to a 5-ary function, returning a 4-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D, E, F>(_ a : A, _ fun : @escaping (A, B, C, D, E) -> F) -> (B, C, D, E) -> F {
     return { b, c, d, e in fun(a, b, c, d, e) }
 }
 
+/**
+ Applies the first argument to a 6-ary function, returning a 5-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D, E, F, G>(_ a : A, _ fun : @escaping (A, B, C, D, E, F) -> G) -> (B, C, D, E, F) -> G {
     return { b, c, d, e, f in fun(a, b, c, d, e, f) }
 }
 
+/**
+ Applies the first argument to a 7-ary function, returning a 6-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D, E, F, G, H>(_ a : A, _ fun : @escaping (A, B, C, D, E, F, G) -> H) -> (B, C, D, E, F, G) -> H {
     return { b, c, d, e, f, g in fun(a, b, c, d, e, f, g) }
 }
 
+/**
+ Applies the first argument to a 8-ary function, returning a 7-ary function with the rest of the arguments of the original function.
+ */
 public func |><A, B, C, D, E, F, G, H, I>(_ a : A, _ fun : @escaping (A, B, C, D, E, F, G, H) -> I) -> (B, C, D, E, F, G, H) -> I {
     return { b, c, d, e, f, g, h in fun(a, b, c, d, e, f, g, h) }
 }
--- Sources/Bow/Syntax/Typeclass.swift
+++ Sources/Bow/Syntax/Typeclass.swift
@@ -1,3 +1,6 @@
 import Foundation
 
+/**
+ Empty protocol to document a protocol is a `Typeclass`.
+ */
 public protocol Typeclass {}
