56a6e91c55f00bfecb6ff1f3c7b34f1a0135cce4
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-01-28 15:09:16 +0100

    Jazzy doc-gen

--- scripts/gen-docs.rb
+++ scripts/gen-docs.rb
@@ -17,8 +17,8 @@ File.open("docs-json/all.json","w") do |f|
   f.write(joined.to_json)
 end
 
-#system 'bundle install --gemfile docs/Gemfile --path vendor/bundle'
-#system 'export BUNDLE_GEMFILE=docs/Gemfile'
-#system 'bundle exec jazzy -o api-docs --sourcekitten-sourcefile ./docs-json/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/api-docs'
+system 'bundle install --gemfile docs/Gemfile --path vendor/bundle'
+system 'export BUNDLE_GEMFILE=docs/Gemfile'
+system 'bundle exec jazzy -o api-docs --sourcekitten-sourcefile ./docs-json/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/api-docs'
 
 #system 'jazzy -o api-docs --sourcekitten-sourcefile ./docs-json/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/api-docs'
