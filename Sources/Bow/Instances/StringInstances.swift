f4cad6722f53c1d21a398908613da83144eb06e1
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-28 13:15:18 +0100

    Regroup instances under String

--- Sources/Bow/Instances/StringInstances.swift
+++ Sources/Bow/Instances/StringInstances.swift
@@ -1,67 +1,67 @@
 import Foundation
 
-/**
- Instance of `Semigroup` for `String`, using concatenation as combination method.
- */
-public class StringConcatSemigroup : Semigroup {
-    public typealias A = String
-    
-    public func combine(_ a : String, _ b : String) -> String {
-        return a + b
-    }
-}
-
-/**
- Instance of `Monoid` for `String`, using concatenation as combination method.
- */
-public class StringConcatMonoid : StringConcatSemigroup, Monoid {
-    public var empty : String {
-        return ""
-    }
-}
-
-/**
- Instance of `Eq` for `String`.
- */
-public class StringEq : Eq {
-    public typealias A = String
-    
-    public func eqv(_ a: String, _ b: String) -> Bool {
-        return a == b
-    }
-}
-
-/**
- Instance of `Order` for `String`.
- */
-public class StringOrder : StringEq, Order {
-    public func compare(_ a: String, _ b: String) -> Int {
-        switch a.compare(b) {
-        case .orderedAscending: return -1
-        case .orderedDescending: return 1
-        case .orderedSame: return 0
+public extension String {
+    /**
+     Instance of `Semigroup` for `String`, using concatenation as combination method.
+     */
+    public class ConcatSemigroupInstance : Semigroup {
+        public typealias A = String
+        
+        public func combine(_ a : String, _ b : String) -> String {
+            return a + b
         }
     }
-}
 
-public extension String {
+    /**
+     Instance of `Monoid` for `String`, using concatenation as combination method.
+     */
+    public class ConcatMonoidInstance : ConcatSemigroupInstance, Monoid {
+        public var empty : String {
+            return ""
+        }
+    }
+
+    /**
+     Instance of `Eq` for `String`.
+     */
+    public class EqInstance : Eq {
+        public typealias A = String
+        
+        public func eqv(_ a: String, _ b: String) -> Bool {
+            return a == b
+        }
+    }
+
+    /**
+     Instance of `Order` for `String`.
+     */
+    public class OrderInstance : EqInstance, Order {
+        public func compare(_ a: String, _ b: String) -> Int {
+            switch a.compare(b) {
+            case .orderedAscending: return -1
+            case .orderedDescending: return 1
+            case .orderedSame: return 0
+            }
+        }
+    }
+
     /// Provides an instance of `Semigroup`.
-    public static var concatSemigroup : StringConcatSemigroup {
-        return StringConcatSemigroup()
+    public static var concatSemigroup : ConcatSemigroupInstance {
+        return ConcatSemigroupInstance()
     }
     
     /// Provides an instance of `Monoid`.
-    public static var concatMonoid : StringConcatMonoid {
-        return StringConcatMonoid()
+    public static var concatMonoid : ConcatMonoidInstance {
+        return ConcatMonoidInstance()
     }
     
     /// Provides an instance of `Eq`.
-    public static var eq : StringEq {
-        return StringEq()
+    public static var eq : EqInstance {
+        return EqInstance()
     }
     
     /// Provides an instance of `Order`.
-    public static var order : StringOrder {
-        return StringOrder()
+    public static var order : OrderInstance {
+        return OrderInstance()
     }
 }
