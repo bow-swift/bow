78ec38025b8a230343cb6b12dbd9af35249d7d4c
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-28 16:02:33 +0100

    Regroup optics instances under ArrayK

--- Sources/BowOptics/Instances/ArrayKOpticsInstances.swift
+++ Sources/BowOptics/Instances/ArrayKOpticsInstances.swift
@@ -6,71 +6,71 @@ public extension ArrayK {
         return ArrayKTraversal<A>()
     }
     
-    public static func each() -> ArrayKEach<A> {
-        return ArrayKEach<A>()
+    public static func each() -> EachInstance<A> {
+        return EachInstance<A>()
     }
     
-    public static func index() -> ArrayKIndex<A> {
-        return ArrayKIndex<A>()
+    public static func index() -> IndexInstance<A> {
+        return IndexInstance<A>()
     }
     
-    public static func filterIndex() -> ArrayKFilterIndex<A> {
-        return ArrayKFilterIndex<A>()
+    public static func filterIndex() -> FilterIndexInstance<A> {
+        return FilterIndexInstance<A>()
     }
-}
-
-fileprivate class ArrayKTraversal<A> : Traversal<ArrayK<A>, A> {
-    override func modifyF<Appl, F>(_ applicative: Appl, _ s: ArrayK<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, ArrayK<A>> where Appl : Applicative, F == Appl.F {
-        return applicative.map(s.traverse(f, applicative), { x in ArrayK.fix(x) })
-    }
-}
-
-public class ArrayKEach<E> : Each {
-    public typealias S = ArrayK<E>
-    public typealias A = E
     
-    public func each() -> Traversal<ArrayK<E>, E> {
-        return ArrayK.traversal()
+    public class EachInstance<E> : Each {
+        public typealias S = ArrayK<E>
+        public typealias A = E
+        
+        public func each() -> Traversal<ArrayK<E>, E> {
+            return ArrayK<E>.traversal()
+        }
     }
-}
-
-public class ArrayKIndex<E> : Index {
-    public typealias S = ArrayK<E>
-    public typealias I = Int
-    public typealias A = E
     
-    public func index(_ i: Int) -> Optional<ArrayK<E>, E> {
-        return Optional<ArrayK<E>, E>(
-            set: { arrayK, e in
-                arrayK.asArray.enumerated().map { x in
-                    return (x.offset == i) ? e : x.element
-                }.k()
-        }, getOrModify: { array in
+    public class IndexInstance<E> : Index {
+        public typealias S = ArrayK<E>
+        public typealias I = Int
+        public typealias A = E
+        
+        public func index(_ i: Int) -> Optional<ArrayK<E>, E> {
+            return Optional<ArrayK<E>, E>(
+                set: { arrayK, e in
+                    arrayK.asArray.enumerated().map { x in
+                        return (x.offset == i) ? e : x.element
+                        }.k()
+            }, getOrModify: { array in
                 array.getOrNone(i).fold({ Either<ArrayK<E>, E>.left(array) }, Either<ArrayK<E>, E>.right)
-        })
-    }
-}
-
-public class ArrayKFilterIndex<E> : FilterIndex {
-    public typealias S = ArrayK<E>
-    public typealias I = Int
-    public typealias A = E
-    
-    public func filter(_ predicate: @escaping (Int) -> Bool) -> Traversal<ArrayK<E>, E> {
-        return ArrayKFilterIndexTraversal<E>(predicate: predicate)
-    }
-}
-
-fileprivate class ArrayKFilterIndexTraversal<A> : Traversal<ArrayK<A>, A> {
-    private let predicate : (Int) -> Bool
-    
-    init(predicate : @escaping (Int) -> Bool) {
-        self.predicate = predicate
+            })
+        }
     }
     
-    override func modifyF<Appl, F>(_ applicative: Appl, _ s: ArrayK<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, ArrayK<A>> where Appl : Applicative, F == Appl.F {
-        return applicative.map(s.asArray.enumerated().map(id).k().traverse({ x in
-            self.predicate(x.offset) ? f(x.element) : applicative.pure(x.element)
-        }, applicative), { l in l.fix() })
+    public class FilterIndexInstance<E> : FilterIndex {
+        public typealias S = ArrayK<E>
+        public typealias I = Int
+        public typealias A = E
+        
+        public func filter(_ predicate: @escaping (Int) -> Bool) -> Traversal<ArrayK<E>, E> {
+            return ArrayKFilterIndexTraversal<E>(predicate: predicate)
+        }
+    }
+    
+    fileprivate class ArrayKFilterIndexTraversal<A> : Traversal<ArrayK<A>, A> {
+        private let predicate : (Int) -> Bool
+        
+        init(predicate : @escaping (Int) -> Bool) {
+            self.predicate = predicate
+        }
+        
+        override func modifyF<Appl, F>(_ applicative: Appl, _ s: ArrayK<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, ArrayK<A>> where Appl : Applicative, F == Appl.F {
+            return applicative.map(s.asArray.enumerated().map(id).k().traverse({ x in
+                self.predicate(x.offset) ? f(x.element) : applicative.pure(x.element)
+            }, applicative), { l in l.fix() })
+        }
+    }
+    
+    fileprivate class ArrayKTraversal<A> : Traversal<ArrayK<A>, A> {
+        override func modifyF<Appl, F>(_ applicative: Appl, _ s: ArrayK<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, ArrayK<A>> where Appl : Applicative, F == Appl.F {
+            return applicative.map(s.traverse(f, applicative), { x in ArrayK<A>.fix(x) })
+        }
     }
 }
