b0ae1d3f5ee0c7a448ced1a6f76fccaae6770225
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-28 11:32:51 +0100

    Regroup instances under Coproduct

--- Sources/Bow/Data/Coproduct.swift
+++ Sources/Bow/Data/Coproduct.swift
@@ -53,111 +53,111 @@ public class Coproduct<F, G, A> : CoproductOf<F, G, A> {
 }
 
 public extension Coproduct {
-    public static func functor<FuncF, FuncG>(_ functorF : FuncF, _ functorG : FuncG) -> CoproductFunctor<F, G, FuncF, FuncG> {
-        return CoproductFunctor<F, G, FuncF, FuncG>(functorF, functorG)
+    public static func functor<FuncF, FuncG>(_ functorF : FuncF, _ functorG : FuncG) -> FunctorInstance<F, G, FuncF, FuncG> {
+        return FunctorInstance<F, G, FuncF, FuncG>(functorF, functorG)
     }
     
-    public static func comonad<ComonF, ComonG>(_ comonadF : ComonF, _ comonadG : ComonG) -> CoproductComonad<F, G, ComonF, ComonG> {
-        return CoproductComonad<F, G, ComonF, ComonG>(comonadF, comonadG)
+    public static func comonad<ComonF, ComonG>(_ comonadF : ComonF, _ comonadG : ComonG) -> ComonadInstance<F, G, ComonF, ComonG> {
+        return ComonadInstance<F, G, ComonF, ComonG>(comonadF, comonadG)
     }
     
-    public static func foldable<FoldF, FoldG>(_ foldableF : FoldF, _ foldableG : FoldG) -> CoproductFoldable<F, G, FoldF, FoldG> {
-        return CoproductFoldable<F, G, FoldF, FoldG>(foldableF, foldableG)
+    public static func foldable<FoldF, FoldG>(_ foldableF : FoldF, _ foldableG : FoldG) -> FoldableInstance<F, G, FoldF, FoldG> {
+        return FoldableInstance<F, G, FoldF, FoldG>(foldableF, foldableG)
     }
     
-    public static func traverse<TravF, TravG>(_ traverseF : TravF, _ traverseG : TravG) -> CoproductTraverse<F, G, TravF, TravG>{
-        return CoproductTraverse<F, G, TravF, TravG>(traverseF, traverseG)
+    public static func traverse<TravF, TravG>(_ traverseF : TravF, _ traverseG : TravG) -> TraverseInstance<F, G, TravF, TravG>{
+        return TraverseInstance<F, G, TravF, TravG>(traverseF, traverseG)
     }
     
-    public static func eq<EqA>(_ eq : EqA) -> CoproductEq<F, G, A, EqA> {
-        return CoproductEq<F, G, A, EqA>(eq)
+    public static func eq<EqA>(_ eq : EqA) -> EqInstance<F, G, A, EqA> {
+        return EqInstance<F, G, A, EqA>(eq)
     }
-}
 
-public class CoproductFunctor<G, H, FuncG, FuncH> : Functor where FuncG : Functor, FuncG.F == G, FuncH : Functor, FuncH.F == H {
-    public typealias F = CoproductPartial<G, H>
-    
-    private let functorG : FuncG
-    private let functorH : FuncH
-    
-    public init(_ functorG : FuncG, _ functorH : FuncH) {
-        self.functorG = functorG
-        self.functorH = functorH
+    public class FunctorInstance<G, H, FuncG, FuncH> : Functor where FuncG : Functor, FuncG.F == G, FuncH : Functor, FuncH.F == H {
+        public typealias F = CoproductPartial<G, H>
+        
+        private let functorG : FuncG
+        private let functorH : FuncH
+        
+        public init(_ functorG : FuncG, _ functorH : FuncH) {
+            self.functorG = functorG
+            self.functorH = functorH
+        }
+        
+        public func map<A, B>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (A) -> B) -> CoproductOf<G, H, B> {
+            return Coproduct<G, H, A>.fix(fa).map(functorG, functorH, f)
+        }
     }
-    
-    public func map<A, B>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (A) -> B) -> CoproductOf<G, H, B> {
-        return Coproduct.fix(fa).map(functorG, functorH, f)
-    }
-}
 
-public class CoproductComonad<G, H, ComonG, ComonH> : CoproductFunctor<G, H, ComonG, ComonH>, Comonad where ComonG : Comonad, ComonG.F == G, ComonH : Comonad, ComonH.F == H {
-    
-    private let comonadG : ComonG
-    private let comonadH : ComonH
-    
-    override public init(_ comonadG : ComonG, _ comonadH : ComonH) {
-        self.comonadG = comonadG
-        self.comonadH = comonadH
-        super.init(comonadG, comonadH)
+    public class ComonadInstance<G, H, ComonG, ComonH> : FunctorInstance<G, H, ComonG, ComonH>, Comonad where ComonG : Comonad, ComonG.F == G, ComonH : Comonad, ComonH.F == H {
+        
+        private let comonadG : ComonG
+        private let comonadH : ComonH
+        
+        override public init(_ comonadG : ComonG, _ comonadH : ComonH) {
+            self.comonadG = comonadG
+            self.comonadH = comonadH
+            super.init(comonadG, comonadH)
+        }
+        
+        public func coflatMap<A, B>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (CoproductOf<G, H, A>) -> B) -> CoproductOf<G, H, B> {
+            return Coproduct<G, H, A>.fix(fa).coflatMap(comonadG, comonadH, f)
+        }
+        
+        public func extract<A>(_ fa: CoproductOf<G, H, A>) -> A {
+            return Coproduct<G, H, A>.fix(fa).extract(comonadG, comonadH)
+        }
     }
-    
-    public func coflatMap<A, B>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (CoproductOf<G, H, A>) -> B) -> CoproductOf<G, H, B> {
-        return Coproduct.fix(fa).coflatMap(comonadG, comonadH, f)
-    }
-    
-    public func extract<A>(_ fa: CoproductOf<G, H, A>) -> A {
-        return Coproduct.fix(fa).extract(comonadG, comonadH)
-    }
-}
 
-public class CoproductFoldable<G, H, FoldG, FoldH> : Foldable where FoldG : Foldable, FoldG.F == G, FoldH : Foldable, FoldH.F == H {
-    public typealias F = CoproductPartial<G, H>
-    
-    private let foldableG : FoldG
-    private let foldableH : FoldH
-    
-    public init(_ foldableG : FoldG, _ foldableH : FoldH) {
-        self.foldableG = foldableG
-        self.foldableH = foldableH
+    public class FoldableInstance<G, H, FoldG, FoldH> : Foldable where FoldG : Foldable, FoldG.F == G, FoldH : Foldable, FoldH.F == H {
+        public typealias F = CoproductPartial<G, H>
+        
+        private let foldableG : FoldG
+        private let foldableH : FoldH
+        
+        public init(_ foldableG : FoldG, _ foldableH : FoldH) {
+            self.foldableG = foldableG
+            self.foldableH = foldableH
+        }
+        
+        public func foldLeft<A, B>(_ fa: CoproductOf<G, H, A>, _ b: B, _ f: @escaping (B, A) -> B) -> B {
+            return Coproduct<G, H, A>.fix(fa).foldLeft(b, f, foldableG, foldableH)
+        }
+        
+        public func foldRight<A, B>(_ fa: CoproductOf<G, H, A>, _ b: Eval<B>, _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+            return Coproduct<G, H, A>.fix(fa).foldRight(b, f, foldableG, foldableH)
+        }
     }
-    
-    public func foldLeft<A, B>(_ fa: CoproductOf<G, H, A>, _ b: B, _ f: @escaping (B, A) -> B) -> B {
-        return Coproduct.fix(fa).foldLeft(b, f, foldableG, foldableH)
-    }
-    
-    public func foldRight<A, B>(_ fa: CoproductOf<G, H, A>, _ b: Eval<B>, _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
-        return Coproduct.fix(fa).foldRight(b, f, foldableG, foldableH)
-    }
-}
 
-public class CoproductTraverse<G, H, TravG, TravH> : CoproductFoldable<G, H, TravG, TravH>, Traverse where TravG : Traverse, TravG.F == G, TravH : Traverse, TravH.F == H {
-    
-    private let traverseG : TravG
-    private let traverseH : TravH
-    
-    override public init(_ traverseG : TravG, _ traverseH : TravH) {
-        self.traverseG = traverseG
-        self.traverseH = traverseH
-        super.init(traverseG, traverseH)
+    public class TraverseInstance<G, H, TravG, TravH> : FoldableInstance<G, H, TravG, TravH>, Traverse where TravG : Traverse, TravG.F == G, TravH : Traverse, TravH.F == H {
+        
+        private let traverseG : TravG
+        private let traverseH : TravH
+        
+        override public init(_ traverseG : TravG, _ traverseH : TravH) {
+            self.traverseG = traverseG
+            self.traverseH = traverseH
+            super.init(traverseG, traverseH)
+        }
+        
+        public func traverse<I, A, B, Appl>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (A) -> Kind<I, B>, _ applicative: Appl) -> Kind<I, CoproductOf<G, H, B>> where I == Appl.F, Appl : Applicative {
+            return Coproduct<G, H, A>.fix(fa).traverse(f, applicative, traverseG, traverseH)
+        }
     }
-    
-    public func traverse<I, A, B, Appl>(_ fa: CoproductOf<G, H, A>, _ f: @escaping (A) -> Kind<I, B>, _ applicative: Appl) -> Kind<I, CoproductOf<G, H, B>> where I == Appl.F, Appl : Applicative {
-        return Coproduct.fix(fa).traverse(f, applicative, traverseG, traverseH)
-    }
-}
 
-public class CoproductEq<F, G, B, EqB> : Eq where EqB : Eq, EqB.A == EitherOf<Kind<F, B>, Kind<G, B>>{
-    public typealias A = CoproductOf<F, G, B>
-    
-    private let eq : EqB
-    
-    public init(_ eq : EqB) {
-        self.eq = eq
-    }
-    
-    public func eqv(_ a: CoproductOf<F, G, B>, _ b: CoproductOf<F, G, B>) -> Bool {
-        let a = Coproduct.fix(a)
-        let b = Coproduct.fix(b)
-        return eq.eqv(a.run, b.run)
+    public class EqInstance<F, G, B, EqB> : Eq where EqB : Eq, EqB.A == EitherOf<Kind<F, B>, Kind<G, B>>{
+        public typealias A = CoproductOf<F, G, B>
+        
+        private let eq : EqB
+        
+        public init(_ eq : EqB) {
+            self.eq = eq
+        }
+        
+        public func eqv(_ a: CoproductOf<F, G, B>, _ b: CoproductOf<F, G, B>) -> Bool {
+            let a = Coproduct<F, G, B>.fix(a)
+            let b = Coproduct<F, G, B>.fix(b)
+            return eq.eqv(a.run, b.run)
+        }
     }
 }
