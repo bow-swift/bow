c620d13486efea508c644716993e101b1d4895af
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-28 16:05:09 +0100

    Regroup optics instances under Option

--- Sources/BowOptics/Instances/OptionOpticsInstances.swift
+++ Sources/BowOptics/Instances/OptionOpticsInstances.swift
@@ -6,22 +6,23 @@ public extension Option {
         return OptionTraversal<A>()
     }
     
-    public static func each() -> OptionEach<A> {
-        return OptionEach<A>()
+    public static func each() -> EachInstance<A> {
+        return EachInstance<A>()
     }
-}
-
-fileprivate class OptionTraversal<A> : Traversal<OptionOf<A>, A> {
-    override func modifyF<Appl, F>(_ applicative: Appl, _ s: OptionOf<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, OptionOf<A>> where Appl : Applicative, F == Appl.F {
-        return s.fix().traverse(f, applicative)
-    }
-}
-
-public class OptionEach<E> : Each {
-    public typealias S = OptionOf<E>
-    public typealias A = E
     
-    public func each() -> Traversal<OptionOf<E>, E> {
-        return Option<E>.traversal()
+    fileprivate class OptionTraversal<A> : Traversal<OptionOf<A>, A> {
+        override func modifyF<Appl, F>(_ applicative: Appl, _ s: OptionOf<A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, OptionOf<A>> where Appl : Applicative, F == Appl.F {
+            return s.fix().traverse(f, applicative)
+        }
+    }
+    
+    public class EachInstance<E> : Each {
+        public typealias S = OptionOf<E>
+        public typealias A = E
+        
+        public func each() -> Traversal<OptionOf<E>, E> {
+            return Option<E>.traversal()
+        }
     }
 }
+
