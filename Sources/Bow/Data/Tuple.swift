97e668a8e53e4ce4f6ff19e481d5607b44100e30
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-28 12:36:31 +0100

    Regroup instances under Tuple

--- Sources/Bow/Data/Tuple.swift
+++ Sources/Bow/Data/Tuple.swift
@@ -1,23 +1,23 @@
 import Foundation
 
 public class Tuple<A, B> {
-    public static func eq<EqA, EqB>(_ eqa : EqA, _ eqb : EqB) -> TupleEq<A, B, EqA, EqB> {
-        return TupleEq<A, B, EqA, EqB>(eqa, eqb)
-    }
-}
-
-public class TupleEq<M, N, EqM, EqN> : Eq where EqM : Eq, EqM.A == M, EqN : Eq, EqN.A == N {
-    public typealias A = (M, N)
-    
-    private let eqm : EqM
-    private let eqn : EqN
-    
-    public init(_ eqm : EqM, _ eqn : EqN) {
-        self.eqm = eqm
-        self.eqn = eqn
+    public static func eq<EqA, EqB>(_ eqa : EqA, _ eqb : EqB) -> EqInstance<A, B, EqA, EqB> {
+        return EqInstance<A, B, EqA, EqB>(eqa, eqb)
     }
     
-    public func eqv(_ a: (M, N), _ b: (M, N)) -> Bool {
-        return eqm.eqv(a.0, b.0) && eqn.eqv(a.1, b.1)
+    public class EqInstance<M, N, EqM, EqN> : Eq where EqM : Eq, EqM.A == M, EqN : Eq, EqN.A == N {
+        public typealias A = (M, N)
+        
+        private let eqm : EqM
+        private let eqn : EqN
+        
+        public init(_ eqm : EqM, _ eqn : EqN) {
+            self.eqm = eqm
+            self.eqn = eqn
+        }
+        
+        public func eqv(_ a: (M, N), _ b: (M, N)) -> Bool {
+            return eqm.eqv(a.0, b.0) && eqn.eqv(a.1, b.1)
+        }
     }
 }
