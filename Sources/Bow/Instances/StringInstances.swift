9719b234f5730ddaeb2a6b6d3298e22bdde63c07
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-29 12:27:20 +0100

    Reformat docs for StringInstances

--- Sources/Bow/Instances/StringInstances.swift
+++ Sources/Bow/Instances/StringInstances.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 public extension String {
-    /**
-     Instance of `Semigroup` for `String`, using concatenation as combination method.
-     */
+    /// Instance of `Semigroup` for `String`, using concatenation as combination method.
+    ///
+    /// Use `String.concatSemigroup` to obtain an instance of this type.
     public class ConcatSemigroupInstance : Semigroup {
         public typealias A = String
         
@@ -12,18 +12,18 @@ public extension String {
         }
     }
 
-    /**
-     Instance of `Monoid` for `String`, using concatenation as combination method.
-     */
+    /// Instance of `Monoid` for `String`, using concatenation as combination method.
+    ///
+    /// Use `String.concatMonoid` to obtain an instance of this type.
     public class ConcatMonoidInstance : ConcatSemigroupInstance, Monoid {
         public var empty : String {
             return ""
         }
     }
 
-    /**
-     Instance of `Eq` for `String`.
-     */
+    /// Instance of `Eq` for `String`.
+    ///
+    /// Use `String.eq` to obtain an instance of this type.
     public class EqInstance : Eq {
         public typealias A = String
         
@@ -32,9 +32,9 @@ public extension String {
         }
     }
 
-    /**
-     Instance of `Order` for `String`.
-     */
+    /// Instance of `Order` for `String`.
+    ///
+    /// Use `String.order` to obtain an instance of this type.
     public class OrderInstance : EqInstance, Order {
         public func compare(_ a: String, _ b: String) -> Int {
             switch a.compare(b) {
@@ -45,22 +45,22 @@ public extension String {
         }
     }
 
-    /// Provides an instance of `Semigroup`.
+    /// Provides an instance of `Semigroup` for `String`, using concatenation as combination method.
     public static var concatSemigroup : ConcatSemigroupInstance {
         return ConcatSemigroupInstance()
     }
     
-    /// Provides an instance of `Monoid`.
+    /// Provides an instance of `Monoid` for `String`, using concatenation as combination method.
     public static var concatMonoid : ConcatMonoidInstance {
         return ConcatMonoidInstance()
     }
     
-    /// Provides an instance of `Eq`.
+    /// Provides an instance of `Eq` for `String`.
     public static var eq : EqInstance {
         return EqInstance()
     }
     
-    /// Provides an instance of `Order`.
+    /// Provides an instance of `Order` for `String`.
     public static var order : OrderInstance {
         return OrderInstance()
     }
