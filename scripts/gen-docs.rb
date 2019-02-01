19693a7ac5f47d0fba41773201c339980e5fae75
Juan Valencia <juanvalencia@calvellido.es>
2019-02-01 17:52:05 +0100

    Docs generation (#130)
    * Streamline Gemfile dependencies ✌
    
    * Add temporary link to API docs
    
    * Travis homebrew dependency installation
    
    * Make gen-docs.rb executable
    
    * Jazzy doc-gen
    
    * Update Bundler for Travis and set cache for it
    
    * Set env var setting in same exec line as Jazzy
    
    * Set two stages for Travis CI/CD
    
    * Set two stages for Travis CI/CD
    
    * Set two stages for Travis CI/CD
    
    * Set two stages for Travis CI/CD
    
    * Set two stages for Travis CI/CD
    
    * Trying to set different configs per stage and jobs
    
    * Trying to set different envs into specific stage
    
    * Set matrix as part of testing stage
    
    * Same stage multiple times to emulate matrix behaviour inside stages
    
    * Check if an addon can be set just for a specific stage only
    
    * Add conditions inside stages
    
    * Add conditions to deploy docs when on master and new version when tags
    
    * Set proper condition for publishing
    
    * Add publishing GH pages capabilities to the deploy docs stage
    
    * Set proper directory to be deployed
    
    * Set branch temporarily as origin to deploy docs
    
    * Remove temporary testing conditions

---
+++ scripts/gen-docs.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'json'
+
+system 'swift package clean'
+system 'swift build'
+system 'mkdir docs-json || true'
+
+modules = ["BowOptics", "BowRecursionSchemes", "BowGeneric", "BowFree", \
+  "BowEffects", "BowResult", "BowRx", "BowBrightFutures", "Bow"]
+modules.each { |m| system "sourcekitten doc --spm-module #{m} > ./docs-json/#{m}.json" }
+
+joined = []
+modules.map { |m| JSON.parse(File.read("./docs-json/#{m}.json")) } \
+  .each { |json| joined += json }
+
+File.open("./docs-json/all.json","w") do |f|
+  f.write(joined.to_json)
+end
+
+system 'bundle install --gemfile ./docs/Gemfile --path vendor/bundle'
+system 'BUNDLE_GEMFILE=./docs/Gemfile bundle exec jazzy -o ./docs/api-docs --sourcekitten-sourcefile ./docs-json/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/api-docs'
