37ebee2ee04d4d50563b640acbfedb386735c09b
Tomás Ruiz-López <truizlop@gmail.com>
2019-02-05 17:08:31 +0100

    Add new encoding for Invariant

--- Sources/Bow/Typeclasses/Invariant.swift
+++ Sources/Bow/Typeclasses/Invariant.swift
@@ -5,3 +5,13 @@ public protocol Invariant : Typeclass {
     
     func imap<A, B>(_ fa : Kind<F, A>, _ f : @escaping (A) -> B, _ g : @escaping (B) -> A) -> Kind<F, B>
 }
+
+public protocol Invariant2: Typeclass {
+    static func imap<A, B>(_ fa : Kind<Self, A>, _ f : @escaping (A) -> B, _ g : @escaping (B) -> A) -> Kind<Self, B>
+}
+
+public extension Kind where F: Invariant2 {
+    func imap<B>(_ f : @escaping (A) -> B, _ g : @escaping (B) -> A) -> Kind<F, B> {
+        return F.imap(self, f, g)
+    }
+}
