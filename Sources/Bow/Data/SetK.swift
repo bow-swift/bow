915adce7f39841462f0f031b035b199cfd959b5f
Tomás Ruiz-López <truizlop@gmail.com>
2019-02-15 10:25:13 +0100

    Delete obsolete code

--- Sources/Bow/Data/SetK.swift
+++ Sources/Bow/Data/SetK.swift
@@ -9,11 +9,7 @@ public final class SetK<A: Hashable>: SetKOf<A> {
 	public static func +(lhs: SetK<A>, rhs: SetK<A>) -> SetK<A> {
 		return SetK(lhs.set.union(rhs.set))
 	}
-
-//    public static func pure(_ a: A) -> SetK<A> {
-//        return SetK(Set([a]))
-//    }
-
+    
 	public static func fix(_ fa: SetKOf<A>) -> SetK<A> {
 		return fa as! SetK<A>
 	}
@@ -25,25 +21,6 @@ public final class SetK<A: Hashable>: SetKOf<A> {
 	public var asSet: Set<A> {
 		return set
 	}
-
-//    public var isEmpty: Bool {
-//        return set.isEmpty
-//    }
-
-	public func foldLeft<B>(_ b: B, _ f: (B, A) -> B) -> B {
-		return set.reduce(b, f)
-	}
-
-//    public func foldRight<B>(_ b: Eval<B>, _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
-//        func loop(_ skw: SetK<A>) -> Eval<B> {
-//            if skw.set.isEmpty {
-//                return b
-//            } else {
-//                return f(skw.set.first!, Eval.deferEvaluation({ loop(Set<A>(skw.set.dropFirst()).k()) }))
-//            }
-//        }
-//        return Eval.deferEvaluation({ loop(self) })
-//    }
 
 	public func combineK(_ y: SetK<A>) -> SetK<A> {
 		return self + y
