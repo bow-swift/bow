75b94fcf6fcdac2277b79ca9fb60ed36dd656343
Tomás Ruiz-López <truizlop@gmail.com>
2019-02-27 09:48:20 +0100

    Semigroup and Monoid docs (#142)
    * Add Semigroup docs
    
    * Add Monoid docs

--- Sources/Bow/Typeclasses/Monoid.swift
+++ Sources/Bow/Typeclasses/Monoid.swift
@@ -1,5 +1,16 @@
 import Foundation
 
+/// A monoid is an algebraic structure that has the same properties as a semigroup, with an empty element.
 public protocol Monoid: Semigroup {
+
+    /// Empty element.
+    ///
+    /// The empty element must obey the monoid laws:
+    ///
+    ///     a.combine(empty()) == empty().combine(a) == a
+    ///
+    /// That is, combining any element with `empty` must return the original element.
+    ///
+    /// - Returns: A value of the implementing type satisfying the monoid laws.
     static func empty() -> Self
 }
