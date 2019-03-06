5f84a9d3ceaddecf501d5d604e4048d8f3d248e2
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-06 16:12:30 +0100

    Add docs for MonadCombine, MonadReader, MonadState and MonadWriter (#179)
    * Add docs for MonadCombine
    
    * Add docs for MonadReader
    
    * Add docs for MonadState
    
    * Add docs for MonadWriter

--- Sources/Bow/Typeclasses/MonadReader.swift
+++ Sources/Bow/Typeclasses/MonadReader.swift
@@ -1,13 +1,29 @@
 import Foundation
 
+/// A MonadReader is a `Monad` with capabilities to read values from a shared environment.
 public protocol MonadReader: Monad {
+    /// Type of the shared environment
     associatedtype D
     
+    /// Retrieves the shared environment.
+    ///
+    /// - Returns: Shared environment.
     static func ask() -> Kind<Self, D>
+
+    /// Executes a computation in a modified environment.
+    ///
+    /// - Parameters:
+    ///   - fa: Computation to execute.
+    ///   - f: Funtion to modify the environment.
+    /// - Returns: Computation in the modified environment.
     static func local<A>(_ fa: Kind<Self, A>, _ f: @escaping (D) -> D) -> Kind<Self, A>
 }
 
 public extension MonadReader {
+    /// Retrieves a function of the current environment.
+    ///
+    /// - Parameter f: Selector function to apply to the environment.
+    /// - Returns: A value extracted from the environment, in the context implementing this instance.
     public static func reader<A>(_ f: @escaping (D) -> A) -> Kind<Self, A> {
         return map(ask(), f)
     }
@@ -16,14 +32,32 @@ public extension MonadReader {
 // MARK: Syntax for MonadReader
 
 public extension Kind where F: MonadReader {
+    /// Retrieves the shared environment.
+    ///
+    /// This is a convenience method to call `MonadReader.ask` as a static method of this type.
+    ///
+    /// - Returns: Shared environment.
     public static func ask() -> Kind<F, F.D> {
         return F.ask()
     }
 
+    /// Executes this computation in a modified environment.
+    ///
+    /// This is a convenience method to call `MonadReader.local` as an instance method of this type.
+    ///
+    /// - Parameters:
+    ///   - f: Funtion to modify the environment.
+    /// - Returns: Computation in the modified environment.
     public func local(_ f: @escaping (F.D) -> F.D) -> Kind<F, A> {
         return F.local(self, f)
     }
 
+    /// Retrieves a function of the current environment.
+    ///
+    /// This is a convenience method to call `MonadReader.reader` as a static method of this type.
+    ///
+    /// - Parameter f: Selector function to apply to the environment.
+    /// - Returns: A value extracted from the environment, in the context implementing this instance.
     public static func reader(_ f: @escaping (F.D) -> A) -> Kind<F, A> {
         return F.reader(f)
     }
--- Sources/Bow/Typeclasses/MonadState.swift
+++ Sources/Bow/Typeclasses/MonadState.swift
@@ -1,13 +1,27 @@
 import Foundation
 
+/// A MonadState is a `Monad` that maintains a state.
 public protocol MonadState: Monad {
+    /// Type of the state maintained in this instance
     associatedtype S
     
+    /// Retrieves the state from the internals of the monad.
+    ///
+    /// - Returns: Maintained state.
     static func get() -> Kind<Self, S>
+
+    /// Replaces the state inside the monad.
+    ///
+    /// - Parameter s: New state.
+    /// - Returns: Unit.
     static func set(_ s: S) -> Kind<Self, ()>
 }
 
 public extension MonadState {
+    /// Embeds a state action into the monad.
+    ///
+    /// - Parameter f: A function that receives the state and computes a value and a new state.
+    /// - Returns: A value with the output of the function and the new state.
     public static func state<A>(_ f: @escaping (S) -> (S, A)) -> Kind<Self, A> {
         return flatMap(get(), { s in
             let result = f(s)
@@ -15,10 +29,18 @@ public extension MonadState {
         })
     }
     
+    /// Modifies the internal state.
+    ///
+    /// - Parameter f: Function that modifies the state.
+    /// - Returns: Unit.
     public static func modify(_ f: @escaping (S) -> S) -> Kind<Self, ()> {
         return flatMap(get(), { s in set(f(s))})
     }
     
+    /// Retrieves a specific component of the state.
+    ///
+    /// - Parameter f: Projection function to obtain part of the state.
+    /// - Returns: A specific part of the state.
     public static func inspect<A>(_ f: @escaping (S) -> A) -> Kind<Self, A> {
         return map(get(), f)
     }
@@ -27,22 +49,51 @@ public extension MonadState {
 // MARK: Syntax for MonadState
 
 public extension Kind where F: MonadState {
+    /// Retrieves the state from the internals of the monad.
+    ///
+    /// This is a convenience method to call `MonadState.get` as a static method of this type.
+    ///
+    /// - Returns: Maintained state.
     public static func get() -> Kind<F, F.S> {
         return F.get()
     }
 
+    /// Replaces the state inside the monad.
+    ///
+    /// This is a convenience method to call `MonadState.set` as a static method of this type.
+    ///
+    /// - Parameter s: New state.
+    /// - Returns: Unit.
     public static func set(_ s: F.S) -> Kind<F, ()> {
         return F.set(s)
     }
 
+    /// Embeds a state action into the monad.
+    ///
+    /// This is a convenience method to call `MonadState.state` as a static method of this type.
+    ///
+    /// - Parameter f: A function that receives the state and computes a value and a new state.
+    /// - Returns: A value with the output of the function and the new state.
     public static func state(_ f: @escaping (F.S) -> (F.S, A)) -> Kind<F, A> {
         return F.state(f)
     }
 
+    /// Modifies the internal state.
+    ///
+    /// This is a convenience method to call `MonadState.modify` as a static method of this type.
+    ///
+    /// - Parameter f: Function that modifies the state.
+    /// - Returns: Unit.
     public static func modify(_ f: @escaping (F.S) -> F.S) -> Kind<F, ()> {
         return F.modify(f)
     }
 
+    /// Retrieves a specific component of the state.
+    ///
+    /// This is a convenience method to call `MonadState.inspect` as a static method of this type.
+    ///
+    /// - Parameter f: Projection function to obtain part of the state.
+    /// - Returns: A specific part of the state.
     public static func inspect(_ f: @escaping (F.S) -> A) -> Kind<F, A> {
         return F.inspect(f)
     }
