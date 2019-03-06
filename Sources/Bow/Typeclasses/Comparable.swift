3c369a6d95c59ad6eb24582e49fe8fa4321fe4f5
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-06 16:25:27 +0100

    API docs for EquatableK and Comparable (#158)
    * Add docs for EquatableK
    
    * Add docs for Comparable
    
    * Typo
    
    * Add equatable laws to docs

--- Sources/Bow/Typeclasses/Comparable.swift
+++ Sources/Bow/Typeclasses/Comparable.swift
@@ -1,14 +1,32 @@
 import Foundation
 
 public extension Comparable {
+    /// Sorts two values.
+    ///
+    /// - Parameters:
+    ///   - a: 1st value.
+    ///   - b: 2nd value.
+    /// - Returns: A tuple with the two values sorted.
     static func sort(_ a: Self, _ b: Self) -> (Self, Self) {
         return a >= b ? (a, b) : (b, a)
     }
 
+    /// Gets the maximum of two values.
+    ///
+    /// - Parameters:
+    ///   - a: 1st value.
+    ///   - b: 2nd value.
+    /// - Returns: Maximum of both values.
     static func max(_ a: Self, _ b: Self) -> Self {
         return a >= b ? a : b
     }
 
+    /// Gets the minimum of two values.
+    ///
+    /// - Parameters:
+    ///   - a: 1st value.
+    ///   - b: 2nd value.
+    /// - Returns: Minimum of both values.
     static func min(_ a: Self, _ b: Self) -> Self {
         return a <= b ? a : b
     }
