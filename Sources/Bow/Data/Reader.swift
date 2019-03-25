47c875db7334af28261f65f470b3f1c6cf5cbd42
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-25 16:06:34 +0100

    Add API docs for Kleisli (#203)
    * Add API docs for Kleisli
    
    * Add API docs for Reader

--- Sources/Bow/Data/Reader.swift
+++ Sources/Bow/Data/Reader.swift
@@ -1,3 +1,4 @@
 import Foundation
 
+/// Reader is a special case of ReaderT / Kleisli where F is `Id`, so it is equivalent to functions `(D) -> A`.
 public typealias Reader<D, A> = ReaderT<ForId, D, A>
