903d4d54b62f609777716d2b5ae9283f45d98991
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-26 14:46:04 +0100

    Add API docs for Try (#205)

--- Sources/Bow/Data/Try.swift
+++ Sources/Bow/Data/Try.swift
@@ -1,27 +1,50 @@
 import Foundation
 
-public enum TryError : Error {
+/// Models an error in an invocation to some methods of `Try`.
+///
+/// - illegalState: The value is in an illegal state when an operation is invoked.
+/// - unsupportedOperation: The invoked operation is unsupported for the value receiving it.
+public enum TryError: Error {
     case illegalState
-    case predicateError(String)
     case unsupportedOperation(String)
 }
 
+/// Witness for the `Try<A>` data type. To be used in simulated Higher Kinded Types.
 public final class ForTry {}
+
+/// Higher Kinded Type alias to improve readability over `Kind<ForTry, A>`.
 public typealias TryOf<A> = Kind<ForTry, A>
 
+/// Describes the result of an operation that may have thrown errors or succeeded. The type parameter corresponds to the result type of the operation.
 public class Try<A>: TryOf<A> {
+    /// Creates a successful Try value.
+    ///
+    /// - Parameter value: Value to be wrapped in a successful Try.
+    /// - Returns: A `Try` value wrapping the successful value.
     public static func success(_ value: A) -> Try<A> {
         return Success<A>(value)
     }
     
+    /// Creates a failed Try value.
+    ///
+    /// - Parameter error: An error.
+    /// - Returns: A `Try` value wrapping the error.
     public static func failure(_ error: Error) -> Try<A> {
         return Failure<A>(error)
     }
 
+    /// Creates a failed Try value.
+    ///
+    /// - Parameter error: An error.
+    /// - Returns: A `Try` value wrapping the error.
     public static func raise(_ error: Error) -> Try<A> {
         return failure(error)
     }
     
+    /// Invokes a closure that may throw errors and wraps the result in a `Try` value.
+    ///
+    /// - Parameter f: Closure to be invoked.
+    /// - Returns: A `Try` value wrapping the result of the invocation or any error that it may have thrown.
     public static func invoke(_ f: () throws -> A) -> Try<A> {
         do {
             let result = try f()
@@ -31,10 +54,20 @@ public class Try<A>: TryOf<A> {
         }
     }
     
+    /// Safe downcast.
+    ///
+    /// - Parameter fa: Value in the higher-kind form.
+    /// - Returns: Value cast to Try.
     public static func fix(_ fa: TryOf<A>) -> Try<A> {
         return fa as! Try<A>
     }
     
+    /// Applies the provided closures based on the content of this `Try` value.
+    ///
+    /// - Parameters:
+    ///   - fe: Closure to apply if the contained value in this `Try` is an error.
+    ///   - fa: Closure to apply if the contained value in this `Try` is a successful value.
+    /// - Returns: Result of applying the corresponding closure to this value.
     public func fold<B>(_ fe: (Error) -> B, _ fa: (A) throws -> B) -> B {
         switch self {
             case let failure as Failure<A>: return fe(failure.error)
@@ -49,19 +82,34 @@ public class Try<A>: TryOf<A> {
         }
     }
     
+    /// Obtains the wrapped error in this `Try`.
+    ///
+    /// - Returns: A successful error or a failure with `TryError.unsupportedOperation` if this value was not an error.
     public func failed() -> Try<Error> {
         return fold(Try<Error>.success,
                     { _ in Try<Error>.failure(TryError.unsupportedOperation("Success.failed"))})
     }
     
+    /// Obtains the value wrapped in this `Try` or a default value if it contains an error.
+    ///
+    /// - Parameter defaultValue: Default value for the failure case.
+    /// - Returns: Value wrapped in this `Try`, or the default value if it was an error.
     public func getOrElse(_ defaultValue: A) -> A {
         return fold(constant(defaultValue), id)
     }
     
+    /// Attempts to recover if the contained value in this `Try` is an error.
+    ///
+    /// - Parameter f: Recovery function.
+    /// - Returns: A `Try` value from the recovery, or the original value if it was not an error.
     public func recoverWith(_ f: (Error) -> Try<A>) -> Try<A> {
         return fold(f, Try.success)
     }
     
+    /// Recovers if the contained value in this `Try` is an error.
+    ///
+    /// - Parameter f: Recovery function.
+    /// - Returns: A `Try` value from the recovery, or the original value if it was not an error.
     public func recover(_ f: @escaping (Error) -> A) -> Try<A> {
         return fold(f >>> Try.success, Try.success)
     }
@@ -91,6 +139,7 @@ class Failure<A>: Try<A> {
     }
 }
 
+// MARK: Conformance of `Try` to `CustomStringConvertible`
 extension Try: CustomStringConvertible {
     public var description : String {
         return fold({ error in "Failure(\(error))" },
@@ -98,6 +147,7 @@ extension Try: CustomStringConvertible {
     }
 }
 
+// MARK: Conformance of `Try` to `CustomDebugStringConvertible`
 extension Try: CustomDebugStringConvertible where A: CustomDebugStringConvertible {
     public var debugDescription: String {
         return fold({ error in "Failure(\(error))" },
@@ -105,6 +155,7 @@ extension Try: CustomDebugStringConvertible where A: CustomDebugStringConvertibl
     }
 }
 
+// MARK: Instance of `EquatableK` for `Try`
 extension ForTry: EquatableK {
     public static func eq<A>(_ lhs: Kind<ForTry, A>, _ rhs: Kind<ForTry, A>) -> Bool where A : Equatable {
         let tl = Try.fix(lhs)
@@ -114,12 +165,14 @@ extension ForTry: EquatableK {
     }
 }
 
+// MARK: Instance of `Functor` for `Try`
 extension ForTry: Functor {
     public static func map<A, B>(_ fa: Kind<ForTry, A>, _ f: @escaping (A) -> B) -> Kind<ForTry, B> {
         return Try.fix(fa).fold(Try.failure, Try.success <<< f)
     }
 }
 
+// MARK: Instance of `Applicative` for `Try`
 extension ForTry: Applicative {
     public static func pure<A>(_ a: A) -> Kind<ForTry, A> {
         return Try.success(a)
@@ -129,6 +182,7 @@ extension ForTry: Applicative {
 // MARK: Instance of `Selective` for `Try`
 extension ForTry: Selective {}
 
+// MARK: Instance of `Monad` for `Try`
 extension ForTry: Monad {
     public static func flatMap<A, B>(_ fa: Kind<ForTry, A>, _ f: @escaping (A) -> Kind<ForTry, B>) -> Kind<ForTry, B> {
         let trya = Try<A>.fix(fa)
@@ -144,6 +198,7 @@ extension ForTry: Monad {
     }
 }
 
+// MARK: Instance of `ApplicativeError` for `Try`
 extension ForTry: ApplicativeError {
     public typealias E = Error
 
@@ -156,8 +211,10 @@ extension ForTry: ApplicativeError {
     }
 }
 
+// MARK: Instance of `MonadError` for `Try`
 extension ForTry: MonadError {}
 
+// MARK: Instance of `Foldable` for `Try`
 extension ForTry: Foldable {
     public static func foldLeft<A, B>(_ fa: Kind<ForTry, A>, _ b: B, _ f: @escaping (B, A) -> B) -> B {
         return Try.fix(fa).fold(constant(b),
@@ -170,6 +227,7 @@ extension ForTry: Foldable {
     }
 }
 
+// MARK: Instance of `Traverse` for `Try`
 extension ForTry: Traverse {
     public static func traverse<G: Applicative, A, B>(_ fa: Kind<ForTry, A>, _ f: @escaping (A) -> Kind<G, B>) -> Kind<G, Kind<ForTry, B>> {
         return Try.fix(fa).fold({ _ in G.pure(Try.raise(TryError.illegalState)) },
