1eb48334ca742fca9e4db9dd21e3a0c772138784
Tomás Ruiz-López <truizlop@gmail.com>
2019-03-27 19:28:00 +0100

    Add API docs for Cokleisli and Coreader (#220)
    * Add API docs for Cokleisli
    
    * Add docs for Coreader

--- Sources/Bow/Data/Coreader.swift
+++ Sources/Bow/Data/Coreader.swift
@@ -1,11 +1,19 @@
 import Foundation
 
-public class Coreader<A, B> : CoreaderT<ForId, A, B> {
-    public init(_ run : @escaping (A) -> B) {
+/// Coreader is a special case of CoreaderT / Cokleisli where F is `Id`, so it is equivalent to functions `(A) -> B`.
+public class Coreader<A, B>: CoreaderT<ForId, A, B> {
+    /// Convenience initializer for Coreader
+    ///
+    /// - Parameter run: Function to be enclosed in this Coreader.
+    public init(_ run: @escaping (A) -> B) {
         super.init({ idA in run((idA as! Id<A>).extract()) })
     }
     
-    public func runId(_ a : A) -> B {
+    /// Runs this Coreader function with the given input.
+    ///
+    /// - Parameter a: Input value for the function.
+    /// - Returns: Output of the function.
+    public func runId(_ a: A) -> B {
         return self.run(Id<A>.pure(a))
     }
 }
