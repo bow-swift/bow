97895ab5684c63c83ac0d349f858ef77cbd02744
Juan Cazalla Estrella <juan.cazalla@47deg.com>
2019-03-29 13:49:20 +0100

    Covers with tests the new functions

--- Tests/BowTests/Transformers/OptionTTest.swift
+++ Tests/BowTests/Transformers/OptionTTest.swift
@@ -5,7 +5,7 @@ import SwiftCheck
 
 class OptionTTest: XCTestCase {
     
-    var generator : (Int) -> OptionTOf<ForId, Int> {
+    var generator: (Int) -> OptionTOf<ForId, Int> {
         return { a in OptionT<ForId, Int>.pure(a) }
     }
 
@@ -39,5 +39,33 @@ class OptionTTest: XCTestCase {
     
     func testFunctorFilterLaws() {
         FunctorFilterLaws<OptionTPartial<ForId>>.check(generator: self.generator)
+    }
+
+    func testToLeftWithSome() {
+        property("toLeft for .some should build a correct EitherT") <- forAll { (a: Int, b: String) in
+            let optionT = OptionT<ForId, Int>.fromOption(.some(a))
+            return optionT.toLeft({ b }) == EitherT<ForId, Int, String>.left(a)
+        }
+    }
+
+    func testToLeftWithNone() {
+        property("toLeft for .none should build a correct EitherT") <- forAll { (b: String) in
+            let optionT = OptionT<ForId, Int>.fromOption(.none())
+            return optionT.toLeft({ b }) == EitherT<ForId, Int, String>.right(b)
+        }
+    }
+
+    func testToRightWithSome() {
+        property("toRight for .some should build a correct EitherT") <- forAll { (a: Int, b: String) in
+            let optionT = OptionT<ForId, String>.fromOption(.some(b))
+            return optionT.toRight({ a }) == EitherT<ForId, Int, String>.right(b)
+        }
+    }
+
+    func testToRightWithNome() {
+        property("toRight for .none should build a correct EitherT") <- forAll { (a: Int) in
+            let optionT = OptionT<ForId, String>.fromOption(.none())
+            return optionT.toRight({ a }) == EitherT<ForId, Int, String>.left(a)
+        }
     }
 }
