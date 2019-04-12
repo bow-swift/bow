88698f1f7ed205d9b981687f9b61f8574b5bd146
Tomás Ruiz-López <truizlop@gmail.com>
2019-04-12 10:58:34 +0200

    Add missing laws to Async laws

--- Tests/BowEffectsLaws/AsyncLaws.swift
+++ Tests/BowEffectsLaws/AsyncLaws.swift
@@ -3,11 +3,15 @@ import SwiftCheck
 import Bow
 import BowEffects
 
-class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
+public class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
     
-    static func check() {
+    public static func check() {
         success()
         error()
+        continueOnJumpsThreads()
+        asyncConstructor()
+        continueOnComprehension()
+        asyncCanBeDerivedFromAsyncF()
     }
     
     private static func success() {
@@ -21,5 +25,56 @@ class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
             return F.async({ ff in ff(Either<F.E, Int>.left(error)) }) ==
                 F.raiseError(error)
         }
+    }
+
+    private static func continueOnJumpsThreads() {
+        property("continueOnJumpsThreads") <- forAll { (id1: String, id2: String) in
+            let queue1 = DispatchQueue(label: id1)
+            let queue2 = DispatchQueue(label: id2)
+
+            return F.pure(())
+                .continueOn(queue1)
+                .map { _ in currentQueueLabel }
+                .continueOn(queue2)
+                .map { x in x + currentQueueLabel } == F.pure(id1 + id2)
+        }
+    }
+
+    private static func asyncConstructor() {
+        property("asyncConstructor") <- forAll { (id1: String, id2: String) in
+            let queue1 = DispatchQueue(label: id1)
+            let queue2 = DispatchQueue(label: id2)
+
+            return F.delay(queue1) { currentQueueLabel }
+                .flatMap { x in F.delay(queue2) { x + currentQueueLabel } }
+                == F.pure(id1 + id2)
+        }
+    }
+
+    private static func continueOnComprehension() {
+        property("continueOnComprehension") <- forAll { (id1: String, id2: String) in
+            let queue1 = DispatchQueue(label: id1)
+            let queue2 = DispatchQueue(label: id2)
+            let fa = F.pure(())
+            return F.binding(
+                { fa.continueOn(queue1) },
+                { _ in F.pure(currentQueueLabel) },
+                { _, b in F.pure(b).continueOn(queue2) },
+                { _, _, c in F.pure(c + currentQueueLabel) }
+            ) == F.pure(id1 + id2)
+        }
+    }
+
+    private static func asyncCanBeDerivedFromAsyncF() {
+        property("asyncCanBeDerivedFromAsyncF") <- forAll { (x: Int) in
+            let either = Either<F.E, Int>.right(x)
+            let k: Proc<F.E, Int> = { f in f(either) }
+
+            return F.async(k) == F.asyncF { cb in F.delay { try k(cb) } }
+        }
+    }
+
+    private static var currentQueueLabel: String {
+        return OperationQueue.current?.underlyingQueue?.label ?? ""
     }
 }
