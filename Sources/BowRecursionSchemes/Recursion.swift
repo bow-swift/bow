f55dda8c1d914e16738aa2eaad997de268d2e938
Juan Cazalla Estrella <juan.cazalla@47deg.com>
2019-04-16 13:56:02 +0200

    Removes warnings in BowRecursionSchemes

--- Sources/BowRecursionSchemes/Recursion.swift
+++ Sources/BowRecursionSchemes/Recursion.swift
@@ -5,7 +5,7 @@ public typealias Algebra<F, A> = (Kind<F, A>) -> A
 public typealias Coalgebra<F, A> = (A) -> Kind<F, A>
 
 public extension Functor {
-    public static func hylo<A, B>(_ algebra : @escaping Algebra<Self, Eval<B>>,
+    static func hylo<A, B>(_ algebra : @escaping Algebra<Self, Eval<B>>,
                            _ coalgebra : @escaping Coalgebra<Self, A>,
                            _ a : A) -> B {
         func h(_ a : A) -> Eval<B> {
@@ -17,9 +17,9 @@ public extension Functor {
 
 // MARK: Syntax for F-Algebras
 public extension Kind where F: Functor {
-    public static func hylo<B>(_ algebra: @escaping Algebra<F, Eval<B>>,
-                               _ coalgebra: @escaping Coalgebra<F, A>,
-                               _ a: A) -> B {
+    static func hylo<B>(_ algebra: @escaping Algebra<F, Eval<B>>,
+                        _ coalgebra: @escaping Coalgebra<F, A>,
+                        _ a: A) -> B {
         return F.hylo(algebra, coalgebra, a)
     }
 }
