fa5f0f3034f22d9ba81f29e8564354aef4db730c
Tomás <truizlop@gmail.com>
2019-06-05 17:31:37 +0200

    Instance of ArbitraryK for Ior

--- Tests/BowGenerators/Data/Ior+Gen.swift
+++ Tests/BowGenerators/Data/Ior+Gen.swift
@@ -11,3 +11,11 @@ extension Ior: Arbitrary where A: Arbitrary, B: Arbitrary {
         return Gen.one(of: [left, right, both])
     }
 }
+
+// MARK: Instance of `ArbitraryK` for `Ior`
+
+extension IorPartial: ArbitraryK where L: Arbitrary {
+    public static func generate<A: Arbitrary>() -> Kind<IorPartial<L>, A> {
+        return Ior.arbitrary.generate
+    }
+}
