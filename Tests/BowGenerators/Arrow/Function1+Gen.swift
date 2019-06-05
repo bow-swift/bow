64e6cd566d235531fbc276a6894c140953783742
Tomás <truizlop@gmail.com>
2019-06-05 17:39:52 +0200

    Instance of ArbitraryK for Function1

--- Tests/BowGenerators/Arrow/Function1+Gen.swift
+++ Tests/BowGenerators/Arrow/Function1+Gen.swift
@@ -8,3 +8,11 @@ extension Function1: Arbitrary where I: CoArbitrary & Hashable, O: Arbitrary {
         return ArrowOf<I, O>.arbitrary.map { arrow in Function1(arrow.getArrow) }
     }
 }
+
+// MARK: Instance of `ArbitraryK` for `Function1`
+
+extension Function1Partial: ArbitraryK where I: CoArbitrary & Hashable {
+    public static func generate<A: Arbitrary>() -> Kind<Function1Partial<I>, A> {
+        return Function1.arbitrary.generate
+    }
+}
