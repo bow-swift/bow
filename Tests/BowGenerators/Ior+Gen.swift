43ee5da0fef89801ce079714b5cc0e289a897300
Tomás <truizlop@gmail.com>
2019-06-05 15:34:33 +0200

    Add Ior Gen

---
+++ Tests/BowGenerators/Ior+Gen.swift
@@ -0,0 +1,13 @@
+import Bow
+import SwiftCheck
+
+// MARK: Generator for Property-based Testing
+
+extension Ior: Arbitrary where A: Arbitrary, B: Arbitrary {
+    public static var arbitrary: Gen<Ior<A, B>> {
+        let left = A.arbitrary.map(Ior.left)
+        let right = B.arbitrary.map(Ior.right)
+        let both = Gen.zip(A.arbitrary, B.arbitrary).map(Ior.both)
+        return Gen.one(of: [left, right, both])
+    }
+}
