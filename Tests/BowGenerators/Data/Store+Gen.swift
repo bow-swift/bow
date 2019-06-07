3403be035fbc441d6706b4bb324c2b2bbbd212b9
Tomás <truizlop@gmail.com>
2019-06-07 14:02:12 +0200

    Add Store Gen

--- Tests/BowGenerators/Data/Store+Gen.swift
+++ Tests/BowGenerators/Data/Store+Gen.swift
@@ -1,10 +1,18 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension Store: Arbitrary where S: CoArbitrary & Hashable & Arbitrary, V: Arbitrary {
+    public static var arbitrary: Gen<Store<S, V>> {
+        return Gen.from(StorePartial.generate >>> Store.fix)
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `Store`
 
 extension StorePartial: ArbitraryK where S: CoArbitrary & Hashable & Arbitrary {
-    public static func generate<A>() -> Kind<StorePartial<S>, A> where A : Arbitrary {
+    public static func generate<A: Arbitrary>() -> Kind<StorePartial<S>, A> {
         return Store(state: S.arbitrary.generate, render: ArrowOf<S, A>.arbitrary.generate.getArrow)
     }
 }
