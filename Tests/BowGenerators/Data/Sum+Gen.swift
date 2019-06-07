4fcd5795a8b163f49772641ccc0f61524086f08e
Tomás <truizlop@gmail.com>
2019-06-07 14:01:19 +0200

    Add Sum Gen

--- Tests/BowGenerators/Data/Sum+Gen.swift
+++ Tests/BowGenerators/Data/Sum+Gen.swift
@@ -1,6 +1,14 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension Sum: Arbitrary where F: ArbitraryK, G: ArbitraryK, V: Arbitrary {
+    public static var arbitrary: Gen<Sum<F, G, V>> {
+        return Gen.from(SumPartial.generate >>> Sum.fix)
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `Sum`
 
 extension SumPartial: ArbitraryK where F: ArbitraryK, G: ArbitraryK {
