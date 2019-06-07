57b94295ac71c14d2d3c62e92f58ecc333bf9c01
Tomás <truizlop@gmail.com>
2019-06-07 13:38:39 +0200

    Add Kleisli Gen

--- Tests/BowGenerators/Arrow/Kleisli+Gen.swift
+++ Tests/BowGenerators/Arrow/Kleisli+Gen.swift
@@ -1,6 +1,15 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension Kleisli: Arbitrary where F: ArbitraryK, A: Arbitrary {
+    public static var arbitrary: Gen<Kleisli<F, D, A>> {
+        let kleisli: Kleisli<F, D, A> = KleisliPartial.generate()^
+        return Gen.pure(kleisli)
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `Kleisli`
 
 extension KleisliPartial: ArbitraryK where F: ArbitraryK {
