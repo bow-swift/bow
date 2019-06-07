3be080b35335cf8550786f1d6edb1773c5431075
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-06-07 13:21:55 +0200

    Update Sum+Gen.swift
    using arbitrary to generate sum - directy from random left/right sum values

--- Tests/BowGenerators/Data/Sum+Gen.swift
+++ Tests/BowGenerators/Data/Sum+Gen.swift
@@ -1,14 +1,23 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension Sum: Arbitrary where F: ArbitraryK, G: ArbitraryK, V: Arbitrary {
+    public static var arbitrary: Gen<Sum<F, G, V>> {
+        let fa: Kind<F, V> = F.generate()
+        let ga: Kind<G, V> = G.generate()
+        let left = Sum.left(fa, ga)
+        let right = Sum.right(fa, ga)
+        
+        return Gen.one(of: [Gen.pure(left), Gen.pure(right)])
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `Sum`
 
 extension SumPartial: ArbitraryK where F: ArbitraryK, G: ArbitraryK {
     public static func generate<A: Arbitrary>() -> Kind<SumPartial<F, G>, A> {
-        let fa: Kind<F, A> = F.generate()
-        let ga: Kind<G, A> = G.generate()
-        let left = Sum.left(fa, ga)
-        let right = Sum.right(fa, ga)
-        return (Int.arbitrary.generate % 2 == 0) ? left : right
+        return Sum.arbitrary.generate
     }
 }
