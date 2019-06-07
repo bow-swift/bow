dd34de08ee1a50e99bacfefb20f3beb4a5b31193
Tomás <truizlop@gmail.com>
2019-06-07 15:31:20 +0200

    Add StateT Gen

--- Tests/BowGenerators/Transformers/StateT+Gen.swift
+++ Tests/BowGenerators/Transformers/StateT+Gen.swift
@@ -1,6 +1,14 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension StateT: Arbitrary where F: ArbitraryK & Applicative, S: Arbitrary, A: Arbitrary {
+    public static var arbitrary: Gen<StateT<F, S, A>> {
+        return Gen.from(StateTPartial.generate >>> StateT.fix)
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `StateT`
 
 extension StateTPartial: ArbitraryK where F: ArbitraryK & Applicative, S: Arbitrary {
