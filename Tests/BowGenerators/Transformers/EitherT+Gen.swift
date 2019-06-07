8020f13359438cdb19355840a9462ee853512548
Tomás <truizlop@gmail.com>
2019-06-07 13:48:00 +0200

    Add EitherT Gen

--- Tests/BowGenerators/Transformers/EitherT+Gen.swift
+++ Tests/BowGenerators/Transformers/EitherT+Gen.swift
@@ -1,6 +1,14 @@
 import Bow
 import SwiftCheck
 
+// MARK: Generator for Property-based Testing
+
+extension EitherT: Arbitrary where F: ArbitraryK, A: Arbitrary, B: Arbitrary {
+    public static var arbitrary: Gen<EitherT<F, A, B>> {
+        return Gen.pure(EitherTPartial.generate()^)
+    }
+}
+
 // MARK: Instance of `ArbitraryK` for `EitherT`
 
 extension EitherTPartial: ArbitraryK where F: ArbitraryK, L: Arbitrary {
