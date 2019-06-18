0a828d8392aeead40edb3cca77388a02cbd3782f
Tomás <truizlop@gmail.com>
2019-06-18 17:34:16 +0200

    Add instances for String

--- Sources/BowOptics/Instances/StringOpticsInstances.swift
+++ Sources/BowOptics/Instances/StringOpticsInstances.swift
@@ -1,29 +1,26 @@
 import Foundation
 import Bow
 
+// MARK: Optics extensions
 public extension String {
-//    static func traversal() -> Traversal<String, Character> {
-//        return StringTraversal()
-//    }
-//
-//    static func each() -> EachInstance {
-//        return EachInstance()
-//    }
-//
-//    private class StringTraversal: Traversal<String, Character> {
-//        override func modifyF<F: Applicative>(_ s: String, _ f: @escaping (Character) -> Kind<F, Character>) -> Kind<F, String> {
-//            return F.map(s.map(id).k().traverse(f), { x in
-//                String(ArrayK.fix(x).asArray)
-//            })
-//        }
-//    }
-//
-//    class EachInstance : Each {
-//        public typealias S = String
-//        public typealias A = Character
-//
-//        public func each() -> Traversal<String, Character> {
-//            return String.traversal()
-//        }
-//    }
+    static var traversal: Traversal<String, Character> {
+        return StringTraversal()
+    }
+}
+
+// MARK: Instance of `Each` for String
+extension String: Each {
+    public typealias EachFoci = Character
+    
+    public static var each: Traversal<String, Character> {
+        return traversal
+    }
+}
+
+private class StringTraversal: Traversal<String, Character> {
+    override func modifyF<F: Applicative>(_ s: String, _ f: @escaping (Character) -> Kind<F, Character>) -> Kind<F, String> {
+        return F.map(s.map(id).k().traverse(f), { x in
+            String(ArrayK.fix(x).asArray)
+        })
+    }
 }
