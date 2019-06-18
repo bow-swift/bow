82c9e26e61d11e6b3e49a8167cabee454c7cbf09
Tomás <truizlop@gmail.com>
2019-06-18 17:37:32 +0200

    Add instances for Try

--- Sources/BowOptics/Instances/TryOpticsInstances.swift
+++ Sources/BowOptics/Instances/TryOpticsInstances.swift
@@ -1,27 +1,22 @@
 import Foundation
 import Bow
 
+// MARK: Optics extensions
 public extension Try {
-//    static func traversal() -> Traversal<TryOf<A>, A> {
-//        return TryTraversal<A>()
-//    }
-//    
-//    static func each() -> TryEach<A> {
-//        return TryEach<A>()
-//    }
-//    
-//    private class TryTraversal<A>: Traversal<TryOf<A>, A> {
-//        override func modifyF<F: Applicative>(_ s: Kind<ForTry, A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, Kind<ForTry, A>> {
-//            return s.traverse(f)
-//        }
-//    }
-//    
-//    class TryEach<E> : Each {
-//        public typealias S = TryOf<E>
-//        public typealias A = E
-//        
-//        public func each() -> Traversal<TryOf<E>, E> {
-//            return Try<E>.traversal()
-//        }
-//    }
+    static var fixIso: Iso<Try<A>, TryOf<A>> {
+        return Iso(get: id, reverseGet: Try.fix)
+    }
+    
+    static var traversal: Traversal<Try<A>, A> {
+        return fixIso + traversalK
+    }
+}
+
+// MARK: Instance of `Each` for `Try`
+extension Try: Each {
+    public typealias EachFoci = A
+    
+    public static var each: PTraversal<Try<A>, Try<A>, A, A> {
+        return traversal
+    }
 }
