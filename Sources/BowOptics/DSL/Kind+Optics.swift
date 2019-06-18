d33950fe6743cc2bc75dd293646d50bcefedd0ec
Tomás <truizlop@gmail.com>
2019-06-18 17:10:33 +0200

    Derive Traversal for every Traverse

---
+++ Sources/BowOptics/DSL/Kind+Optics.swift
@@ -0,0 +1,14 @@
+import Bow
+
+// MARK: Optics extensions
+public extension Kind where F: Traverse {
+    static func traversalK() -> Traversal<Kind<F, A>, A> {
+        return KindTraversal<F, A>()
+    }
+}
+
+private class KindTraversal<G: Traverse, A>: Traversal<Kind<G, A>, A> {
+    override func modifyF<F: Applicative>(_ s: Kind<G, A>, _ f: @escaping (A) -> Kind<F, A>) -> Kind<F, Kind<G, A>> {
+        return s.traverse(f)
+    }
+}
