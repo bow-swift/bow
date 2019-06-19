f6c0f84d3162de518033e763190e4094e44f54ab
Tomás <truizlop@gmail.com>
2019-06-19 11:26:46 +0200

    Add extension functions to At

--- Sources/BowOptics/Typeclasses/At.swift
+++ Sources/BowOptics/Typeclasses/At.swift
@@ -8,30 +8,20 @@ public protocol At {
     static func at(_ i: AtIndex) -> Lens<Self, AtFoci>
 }
 
-//public extension At {
-//    static func at<AtType>(_ at: AtType, _ i: I) -> Lens<S, A> where AtType: At, AtType.S == S, AtType.I == I, AtType.A == A {
-//        return at.at(i)
-//    }
-//
-//    static func fromIso<U, AtType>(_ at: AtType, _ iso: Iso<S, U>) -> AtFromIso<S, I, A, U, AtType> where AtType: At, AtType.S == U, AtType.I == I, AtType.A == A {
-//        return AtFromIso(at: at, iso: iso)
-//    }
-//}
-//
-//public class AtFromIso<M, N, O, P, AtType>: At where AtType: At, AtType.S == P, AtType.I == N, AtType.A == O {
-//    public typealias S = M
-//    public typealias I = N
-//    public typealias A = O
-//
-//    private let atInstance: AtType
-//    private let iso: Iso<M, P>
-//
-//    public init(at: AtType, iso: Iso<M, P>) {
-//        self.atInstance = at
-//        self.iso = iso
-//    }
-//
-//    public func at(_ i: N) -> Lens<M, O> {
-//        return iso + atInstance.at(i)
-//    }
-//}
+public extension At {
+    func remove<A>(_ i: AtIndex) -> Self where AtFoci == Option<A> {
+        return Self.at(i).set(self, .none())
+    }
+    
+    func remove<A>(_ i: AtIndex) -> Self where AtFoci == A? {
+        return Self.at(i).set(self, nil)
+    }
+    
+    static func at<B>(_ i: AtIndex, iso: Iso<AtFoci, B>) -> Lens<Self, B> {
+        return Self.at(i) + iso
+    }
+    
+    static func at<B>(_ i: AtIndex, iso: Iso<B, Self>) -> Lens<B, AtFoci> {
+        return iso + Self.at(i)
+    }
+}
