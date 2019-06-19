74a9e2a29aef468459a484c39d4aba6ade1108cd
Tomás <truizlop@gmail.com>
2019-06-19 17:39:30 +0200

    Remove unused imports

--- Sources/BowOptics/Typeclasses/Each.swift
+++ Sources/BowOptics/Typeclasses/Each.swift
@@ -1,6 +1,3 @@
-import Foundation
-import Bow
-
 public protocol Each {
     associatedtype EachFoci
 
--- Sources/BowOptics/Typeclasses/Index.swift
+++ Sources/BowOptics/Typeclasses/Index.swift
@@ -1,6 +1,3 @@
-import Foundation
-import Bow
-
 public protocol Index {
     associatedtype IndexType
     associatedtype IndexFoci
