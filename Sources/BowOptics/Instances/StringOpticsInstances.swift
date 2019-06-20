ac5305013c0434cba2178ac0fea42b17fbdd1ecf
Tomás <truizlop@gmail.com>
2019-06-20 10:28:10 +0200

    Add missing instances for String

--- Sources/BowOptics/Instances/StringOpticsInstances.swift
+++ Sources/BowOptics/Instances/StringOpticsInstances.swift
@@ -17,6 +17,44 @@ extension String: Each {
     }
 }
 
+// MARK: Instance of `Index` for String
+extension String: Index {
+    public typealias IndexType = Int
+    public typealias IndexFoci = Character
+    
+    public static func index(_ i: Int) -> Optional<String, Character> {
+        return Array<Character>.index(i, iso: String.toArray)
+    }
+}
+
+// MARK: Instance of `FilterIndex` for String
+extension String: FilterIndex {
+    public typealias FilterIndexType = Int
+    public typealias FilterIndexFoci = Character
+    
+    public static func filter(_ predicate: @escaping (Int) -> Bool) -> Traversal<String, Character> {
+        return Array<Character>.filter(predicate, iso: String.toArray)
+    }
+}
+
+// MARK: Instance of `Cons` for String
+extension String: Cons {
+    public typealias First = Character
+    
+    public static var cons: Prism<String, (Character, String)> {
+        return Array<Character>.cons(String.toArray)
+    }
+}
+
+// MARK: Instace of `Snoc` for String
+extension String: Snoc {
+    public typealias Last = Character
+    
+    public static var snoc: Prism<String, (String, Character)> {
+        return Array<Character>.snoc(String.toArray)
+    }
+}
+
 private class StringTraversal: Traversal<String, Character> {
     override func modifyF<F: Applicative>(_ s: String, _ f: @escaping (Character) -> Kind<F, Character>) -> Kind<F, String> {
         return F.map(s.map(id).k().traverse(f), { x in
