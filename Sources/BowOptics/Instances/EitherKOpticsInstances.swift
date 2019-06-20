e18fa7dd2843a8af1505f8b9f336e2e89af10913
Tomás <truizlop@gmail.com>
2019-06-20 10:44:03 +0200

    Add missing optics for EitherK

---
+++ Sources/BowOptics/Instances/EitherKOpticsInstances.swift
@@ -0,0 +1,14 @@
+import Bow
+
+// MARK: Optics extensions
+public extension EitherK {
+    static var fixIso: Iso<EitherK<F, G, A>, EitherKOf<F, G, A>> {
+        return Iso(get: id, reverseGet: EitherK.fix)
+    }
+}
+
+public extension EitherK where F: Traverse, G: Traverse {
+    static var traversal: Traversal<EitherK<F, G, A>, A> {
+        return fixIso + traversalK
+    }
+}
