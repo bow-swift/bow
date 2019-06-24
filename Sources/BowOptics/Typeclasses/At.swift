56697cece3a3a9be1f2b0fa398a6d33a0fdc0725
Tomás Ruiz-López <truizlop@gmail.com>
2019-06-24 12:38:48 +0200

    Add API reference to optics type classes (#357)
    * Add API reference for At
    
    * Add API reference for Cons
    
    * Add API reference for Each
    
    * Add API reference for FilterIndex
    
    * Add API reference to Index
    
    * Add API reference for Snoc

--- Sources/BowOptics/Typeclasses/At.swift
+++ Sources/BowOptics/Typeclasses/At.swift
@@ -1,25 +1,51 @@
 import Bow
 
+/// `At` provides a `Lens` for a this structure to focus at `AtFoci` at a given index `AtIndex`.
 public protocol At {
     associatedtype AtIndex
     associatedtype AtFoci
 
+    /// Obtains a `Lens` for a structure of this type with focus in `AtFoci` at index `AtIndex`.
+    ///
+    /// - Parameter i: Index to zoom into this structure and find focus.
+    /// - Returns: A `Lens` with focus on `AtFoci` at the given index.
     static func at(_ i: AtIndex) -> Lens<Self, AtFoci>
 }
 
+// MARK: Related functions
 public extension At {
+    /// Deletes a value associated with an index by setting it to `Option.none`
+    ///
+    /// - Parameter i: Index of the value to remove.
+    /// - Returns: A new structure where the element at the given index has been removed.
     func remove<A>(_ i: AtIndex) -> Self where AtFoci == Option<A> {
         return Self.at(i).set(self, .none())
     }
     
+    /// Deletes a value associated with an index by setting it to nil.
+    ///
+    /// - Parameter i: Index of the value to remove.
+    /// - Returns: A new structure where the element at the given index has been removed.
     func remove<A>(_ i: AtIndex) -> Self where AtFoci == A? {
         return Self.at(i).set(self, nil)
     }
     
+    /// Post-composes the lens at a given index with an isomorphism.
+    ///
+    /// - Parameters:
+    ///   - i: Index for the value to focus on.
+    ///   - iso: An isomorphism for post-composition.
+    /// - Returns: A Lens that focuses on the provided index, transformed with the provided isomophism.
     static func at<B>(_ i: AtIndex, iso: Iso<AtFoci, B>) -> Lens<Self, B> {
         return Self.at(i) + iso
     }
     
+    /// Pre-composes the lens at a given index with an isomorphism.
+    ///
+    /// - Parameters:
+    ///   - i: Index for the value to focus on.
+    ///   - iso: An isomorphism for pre-composition.
+    /// - Returns: A Lens that focuses on the provided index, where the containing structure has been transformed with the provided isomorphsim.
     static func at<B>(_ i: AtIndex, iso: Iso<B, Self>) -> Lens<B, AtFoci> {
         return iso + Self.at(i)
     }
--- Sources/BowOptics/Typeclasses/Each.swift
+++ Sources/BowOptics/Typeclasses/Each.swift
@@ -1,14 +1,25 @@
+/// `Each` provides a `Traversal` that can focus into this structure to see all its foci of type `EachFoci`.
 public protocol Each {
     associatedtype EachFoci
 
+    /// Provides a `Traversal` for this structure with focus on `EachFoci`.
     static var each: Traversal<Self, EachFoci> { get }
 }
 
+// MARK: Related functions
 public extension Each {
+    /// Pre-composes the provided `Traversal` from this `Each` with an isomorphism.
+    ///
+    /// - Parameter iso: An isomorphism.
+    /// - Returns: A `Traversal` on a structure that is isomorphic to this structure and has the same foci.
     static func each<B>(_ iso: Iso<B, Self>) -> Traversal<B, EachFoci> {
         return iso + each
     }
     
+    /// Post-composes the provided `Traversal` from this `Each` with an isomorphism.
+    ///
+    /// - Parameter iso: An isomorphism.
+    /// - Returns: A `Traversal` on this structure with a new foci that is isomorphic to the original one.
     static func each<B>(_ iso: Iso<EachFoci, B>) -> Traversal<Self, B> {
         return each + iso
     }
