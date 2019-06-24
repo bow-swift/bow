96fe8747b68a918fdbfb1be24ef3a8843c67788a
Tomás <truizlop@gmail.com>
2019-06-24 14:55:33 +0200

    Document optics for Option

--- Sources/BowOptics/STD/Option+Optics.swift
+++ Sources/BowOptics/STD/Option+Optics.swift
@@ -1,18 +1,26 @@
 import Foundation
 import Bow
 
+// MARK: Optics extensions
 public extension Option {
+    /// Provides a polymorphic Iso between Option and Swift Optional.
+    ///
+    /// - Returns: A polymorphic Iso between Option and Swift Optional.
     static func toPOption<B>() -> PIso<Option<A>, Option<B>, A?, B?> {
         return PIso<Option<A>, Option<B>, A?, B?>(
             get: { x in x.toOptional() },
             reverseGet: Option<B>.fromOptional)
     }
 
+    /// Provides an Iso between Option and Swift Optional.
     static var toOption: Iso<Option<A>, A?> {
         return toPOption()
     }
 
-    static func PSomePrism<B>() -> PPrism<Option<A>, Option<B>, A, B> {
+    /// Provides a polymorphic prism focused on the some side of an Option.
+    ///
+    /// - Returns: A polymorphic prism focused on the some side of an Option.
+    static func pSomePrism<B>() -> PPrism<Option<A>, Option<B>, A, B> {
         return PPrism<Option<A>, Option<B>, A, B>(
             getOrModify: { option in
                 option.fold({ Either<Option<B>, A>.left(Option<B>.none()) },
@@ -21,10 +29,12 @@ public extension Option {
             reverseGet: Option<B>.some)
     }
 
+    /// Provides a prism focused on the some side of an Option.
     static var somePrism: Prism<Option<A>, A> {
-        return PSomePrism()
+        return pSomePrism()
     }
 
+    /// Provides a prism focused on the none side of an Option.
     static var nonePrism: Prism<Option<A>, ()> {
         return Prism<Option<A>, ()>(
             getOrModify: { option in
@@ -34,6 +44,9 @@ public extension Option {
             reverseGet: { Option<A>.none() })
     }
 
+    /// Provides a polymorphic Iso between Option and Either.
+    ///
+    /// - Returns: A polymorphic Iso between Option and Either.
     static func toPEither<B>() -> PIso<Option<A>, Option<B>, Either<(), A>, Either<(), B>> {
         return PIso<Option<A>, Option<B>, Either<(), A>, Either<(), B>>(
             get: { option in option.fold({ Either.left(unit) },
@@ -42,6 +55,7 @@ public extension Option {
                                                 { b in Option<B>.some(b) })})
     }
 
+    /// Provides a polymorphic Iso between Option and Either.
     static var toEither: Iso<Option<A>, Either<(), A>> {
         return toPEither()
     }
