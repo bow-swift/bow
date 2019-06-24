357cb1e7e5fb2f0ff3f332511753582b6717b1e2
Tomás <truizlop@gmail.com>
2019-06-24 14:34:34 +0200

    Document optics for Id

--- Sources/BowOptics/STD/Id+Optics.swift
+++ Sources/BowOptics/STD/Id+Optics.swift
@@ -1,13 +1,18 @@
 import Foundation
 import Bow
 
+// MARK: Optics extensions
 public extension Id {
+    /// Provides a polymorphic Iso between Id and a raw type.
+    ///
+    /// - Returns: A polymorphic Iso between Id and a raw type.
     static func toPValue<B>() -> PIso<Id<A>, Id<B>, A, B> {
         return PIso<Id<A>, Id<B>, A, B>(
             get: { x in x.value },
             reverseGet: Id<B>.init)
     }
 
+    /// Provides an Iso bewteen Id and its wrapped type.
     static var toValue: Iso<Id<A>, A> {
         return toPValue()
     }
