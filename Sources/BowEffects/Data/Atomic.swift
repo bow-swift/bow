d659c087e7d4c2bce43eacdfd32873774030600a
Tomás <truizlop@gmail.com>
2019-06-26 11:48:38 +0200

    Add utilities to Atomic

--- Sources/BowEffects/Data/Atomic.swift
+++ Sources/BowEffects/Data/Atomic.swift
@@ -18,6 +18,12 @@ final class Atomic<A> {
             transform(&self._value)
         }
     }
+    
+    func set(_ newValue: A) {
+        queue.sync {
+            self._value = newValue
+        }
+    }
 
     func getAndSet(_ newValue: A) -> A {
         var oldValue: A? = nil
@@ -26,6 +32,24 @@ final class Atomic<A> {
             self._value = newValue
         }
         return oldValue!
+    }
+    
+    func getAndUpdate(_ f: @escaping (A) -> A) -> A {
+        var oldValue: A? = nil
+        queue.sync {
+            oldValue = self._value
+            self._value = f(self._value)
+        }
+        return oldValue!
+    }
+    
+    func updateAndGet(_ f: @escaping (A) -> A) -> A {
+        var newValue: A? = nil
+        queue.sync {
+            self._value = f(self._value)
+            newValue = self._value
+        }
+        return newValue!
     }
 
     func setIfNil<AA>(_ newValue: AA) -> Bool where A == AA? {
@@ -46,3 +70,15 @@ final class Atomic<A> {
     }
 }
 
+extension Atomic where A: Equatable {
+    func compare(_ test: A, andSet newValue: A) -> Bool {
+        var equals = false
+        queue.sync {
+            if _value == test {
+                equals = true
+                _value = newValue
+            }
+        }
+        return equals
+    }
+}
