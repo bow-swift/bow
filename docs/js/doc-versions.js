28164fac24d02625c4d05320b44997abefad9d9e
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-06-27 16:11:02 +0200

    Slight fixes

--- docs/js/doc-versions.js
+++ docs/js/doc-versions.js
@@ -21,3 +21,18 @@ function closeDropdown(e) {
   if (relatedTarget.parentNode !== dropdown) dropdown.classList.remove("show");
   window.removeEventListener("click", closeDropdown);
 }
+
+
+// const dropdownContainer = document.querySelector('.dropdown.dropdown-content');
+//
+// // Instantiate the item with the existing HTML template
+// const template = document.querySelector('#template-dropdown-item');
+//
+// // Clone the new row and insert it into the table
+// const clone = document.importNode(template.content, true);
+// const itemLink = clone.querySelector(".dropdown-item-link");
+// const itemLinkTextContent = clone.querySelector(".dropdown-item-link span");
+// itemLink.href = "next/docs";
+// itemLinkTextContent.textContent = "nexty";
+//
+// dropdownContainer.appendChild(clone);
