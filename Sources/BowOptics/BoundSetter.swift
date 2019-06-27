4bdcb2c98dfd5a42a0564567c5084fe8a3acec7d
Tomás <truizlop@gmail.com>
2019-06-27 11:58:12 +0200

    Add API docs for BoundSetter

--- Sources/BowOptics/BoundSetter.swift
+++ Sources/BowOptics/BoundSetter.swift
@@ -1,40 +1,77 @@
 import Foundation
 import Bow
 
+/// A BoundSetter is a `Setter` that is already bound to a concrete source.
 public class BoundSetter<S, A> {
-    let value : S
-    let setter : Setter<S, A>
+    /// Bound source.
+    let value: S
     
-    public init(value : S, setter : Setter<S, A>) {
+    /// Setter for the source.
+    let setter: Setter<S, A>
+    
+    /// Initializes a BoundSetter.
+    ///
+    /// - Parameters:
+    ///   - value: Bound source.
+    ///   - setter: Setter.
+    public init(value: S, setter: Setter<S, A>) {
         self.value = value
         self.setter = setter
     }
     
-    public func modify(_ f : @escaping (A) -> A) -> S {
+    /// Modifies the source of the bound setter.
+    ///
+    /// - Parameter f: Function modifying the focus.
+    /// - Returns: Modified source.
+    public func modify(_ f: @escaping (A) -> A) -> S {
         return setter.modify(value, f)
     }
     
-    public func set(_ a : A) -> S {
+    /// Sets a new value for the focus.
+    ///
+    /// - Parameter a: New focus.
+    /// - Returns: Modified source.
+    public func set(_ a: A) -> S {
         return setter.set(value, a)
     }
     
-    public func compose<T>(_ other : Setter<A, T>) -> BoundSetter<S, T> {
+    /// Composes with a `Setter`.
+    ///
+    /// - Parameter other: Value to compose with.
+    /// - Returns: A `BoundSetter` with the same bound source and a `Setter` resulting from the sequential application of the two optics.
+    public func compose<T>(_ other: Setter<A, T>) -> BoundSetter<S, T> {
         return BoundSetter<S, T>(value: value, setter: setter + other)
     }
     
-    public func compose<T>(_ other : Optional<A, T>) -> BoundSetter<S, T> {
+    /// Composes with an `Optional`.
+    ///
+    /// - Parameter other: Value to compose with.
+    /// - Returns: A `BoundSetter` with the same bound source and a `Setter` resulting from the sequential application of the two optics.
+    public func compose<T>(_ other: Optional<A, T>) -> BoundSetter<S, T> {
         return BoundSetter<S, T>(value: value, setter: setter + other)
     }
     
-    public func compose<T>(_ other : Prism<A, T>) -> BoundSetter<S, T> {
+    /// Composes with a `Prism`.
+    ///
+    /// - Parameter other: Value to compose with.
+    /// - Returns: A `BoundSetter` with the same bound source and a `Setter` resulting from the sequential application of the two optics.
+    public func compose<T>(_ other: Prism<A, T>) -> BoundSetter<S, T> {
         return BoundSetter<S, T>(value: value, setter: setter + other)
     }
     
-    public func compose<T>(_ other : Lens<A, T>) -> BoundSetter<S, T> {
+    /// Composes with a `Lens`.
+    ///
+    /// - Parameter other: Value to compose with.
+    /// - Returns: A `BoundSetter` with the same bound source and a `Setter` resulting from the sequential application of the two optics.
+    public func compose<T>(_ other: Lens<A, T>) -> BoundSetter<S, T> {
         return BoundSetter<S, T>(value: value, setter: setter + other)
     }
     
-    public func compose<T>(_ other : Iso<A, T>) -> BoundSetter<S, T> {
+    /// Composes with an `Iso`.
+    ///
+    /// - Parameter other: Value to compose with.
+    /// - Returns: A `BoundSetter` with the same bound source and a `Setter` resulting from the sequential application of the two optics.
+    public func compose<T>(_ other: Iso<A, T>) -> BoundSetter<S, T> {
         return BoundSetter<S, T>(value: value, setter: setter + other)
     }
 }
