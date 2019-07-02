1ec0ce474410c53793a51d9dbc14d920701a51d9
AntonioMateoGomez <antonio.mateo@47deg.com>
2019-07-02 16:11:50 +0200

    Docs Bow version selector (#334)
    * WIP doc version selector
    
    * Added nav doc version selector
    
    * Added header doc version selector
    
    * Added doc version selector button in docs sidebar
    
    * Versions deployment creator and web selector (#384)
    
    * Testing versiones pipeline
    
    * Set to always use URLs relative to base
    
    * Some logging
    
    * Fix GitHub pages generation bad symlink
    
    * Fix image CSS path
    
    * Testing different version as default
    
    * Improve script empty values checking
    
    * Set master as the default version for now
    
    * Script comments and logic improvements
    
    * Get back testing in CI
    
    * Remove unneeded comments
    
    * Script generalization improvements
    
    * Add auxilisry function
    
    * Fix exists
    
    * Fix var name
    
    * Fix json_files_dir var
    
    * Remove unnecessary function argument
    
    * json_files_dir
    
    * No tags test
    
    * Latest version test
    
    * Change publishing dir name
    
    * More options to the tags generation
    
    * Fix bad code order
    
    * Move instead of copy
    
    * Bad var naming
    
    * Fix typo
    
    * Proper version handling layouts and JS logic
    
    * Initial version writing logic
    
    * Slight fixes
    
    * Temporary work
    
    * Test everything together now
    
    * Fix versions path
    
    * Fix paths
    
    * Remove unnecessary content
    
    * Fix paths, once and for all
    
    * Fix source dir path ¯\_(ツ)_/¯
    
    * Relative URL links fixes tryout
    
    * Relative URL links fixes tryout #2
    
    * Fix version links URLs
    
    * Set proper URL links for doc versions links
    
    * Include API docs dropdown selector
    
    * Restore tests
    
    * 😊
    
    * Remove unnecessary file
    
    * Remove bow prefix from paths

--- scripts/gen-docs.rb
+++ scripts/gen-docs.rb
@@ -5,42 +5,63 @@ require 'json'
 
 # If you want a specific version to be served as default, set this value to the
 # branch/tag you want. Otherwise it will be the latest version on alphabetical order.
-default_version = "master"
+# If there's no tags, then `master` branch content will be served at root path.
+$default_version = "master"
 
-# This is a list to filter -out- tags we know are not valuable to generate docs for.
-invalid_tags = ["0.1.0", "0.2.0", "0.3.0"]
+# If you want to build the current branch (usually `master`) and serve it,
+# set the path/name in here. If you leave it empty no site will be built for it.
+$current_branch_path = "next"
+
+# This is a list to filter -out- tags we know are not valuable to generate docs
+# for. If you set one tag in the default_version, you can add it here, so it's
+# not generated twice. Unless you want to have the same content at two paths.
+$invalid_tags = ["0.1.0", "0.2.0", "0.3.0"]
 
 # This is a list to filter -in- tags. Unless it's empty, where it will be ignored.
 # If you want an empty tags list in the end (for some reason ¯\_(ツ)_/¯)
 # you can cancel these filterings with both lists having the same values:
 # invalid_tags = ["0.1.0"], valid_tags = ["0.1.0"]
-valid_tags = []
+$valid_tags = []
+
+# The path of the dir where the Jekyll source is located.
+$source_dir = "docs"
+
+# The path of the dir that will be published. Check out GitHub Pages/Travis for this.
+$publishing_dir = "pub-dir"
+
+# The path of the dir to temporarily store the different sites content.
+$gen_docs_dir = "gen-docs"
+
+# The path of the dir to temporarily store the JSON files for API sites.
+$json_files_dir = "json-files"
 
 # This a list of modules in which the Swift project is split on
-modules = ["BowOptics", "BowRecursionSchemes", "BowGeneric", "BowFree", \
+$modules = ["BowOptics", "BowRecursionSchemes", "BowGeneric", "BowFree", \
   "BowEffects", "BowRx", "BowBrightFutures", "Bow"]
 
+
+
 # Generate the JSON files that will be needed later.
+# Modules present in the project are used to split the JSON info.
 #
 # @param version [String] The version for which the JSON will be generated.
-# @param modules [Array] The modules present in the project to split the JSON info into.
 # @return [nil] nil.
-def generate_json(version, modules)
-  `mkdir -p docs-json/#{version}`
-  modules.each { |m| `sourcekitten doc --spm-module #{m} > ./docs-json/#{version}/#{m}.json` }
+def generate_json(version)
+  `mkdir -p #{$json_files_dir}/#{version}`
+  $modules.each { |m| `sourcekitten doc --spm-module #{m} > #{$json_files_dir}/#{version}/#{m}.json` }
 end
 
 # Join the previously generated JSON files into one single file.
+# The array of modules in the project is used to join the JSON info.
 #
 # @param version [String] The version for which the JSON will be generated.
-# @param modules [Array] The modules present in the project to join the JSON info.
 # @return [nil] nil.
-def join_json(version, modules)
+def join_json(version)
   joined = []
-  modules.map { |m| JSON.parse(File.read("./docs-json/#{version}/#{m}.json")) } \
+  $modules.map { |m| JSON.parse(File.read("#{$json_files_dir}/#{version}/#{m}.json")) } \
     .each { |json| joined += json }
 
-  File.open("./docs-json/#{version}/all.json","w") do |f|
+  File.open("#{$json_files_dir}/#{version}/all.json","w") do |f|
     f.write(joined.to_json)
   end
 end
@@ -48,13 +69,21 @@ end
 # Generate the Jekyll site through Nef based on the contents source.
 #
 # @param version [String] The version for which the nef docs site will be generated.
+# @param versions_list [Array] The list of versions available to select in the whole project.
 # @return [nil] nil.
-def generate_nef_site(version)
+def generate_nef_site(version, versions_list)
   system "echo Generating nef site for #{version}"
+  this_versions = versions_list.dup;
+  this_versions[versions_list.find_index("title" => version)] = {
+    "title" => version,
+    "this" => true
+  };
+  `mkdir -p #{$source_dir}/_data`
+  File.write("#{$source_dir}/_data/versions.json", JSON.pretty_generate(this_versions))
   system "nef jekyll --project contents/Documentation --output docs --main-page contents/Home.md"
-  system "JEKYLL_ENV=production BUNDLE_GEMFILE=./docs/Gemfile bundle exec jekyll build -s ./docs -d ./docs/#{version} -b bow/#{version}"
-  system "ls -la docs"
-  system "ls -la docs/#{version}"
+  system "JEKYLL_ENV=production BUNDLE_GEMFILE=./docs/Gemfile bundle exec jekyll build -s #{$source_dir} -d #{$gen_docs_dir}/#{version} -b #{version}"
+  system "ls -la #{$source_dir}"
+  system "ls -la #{$gen_docs_dir}/#{version}"
 end
 
 # Generate the Jazzy site based on previouly created JSON file.
@@ -63,51 +92,111 @@ end
 # @return [nil] nil.
 def generate_api_site(version)
   system "echo Generating API site for #{version}"
-  system "BUNDLE_GEMFILE=./docs/Gemfile bundle exec jazzy -o ./docs/#{version}/api-docs --sourcekitten-sourcefile ./docs-json/#{version}/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/#{version}/api-docs --theme docs/extra/bow-jazzy-theme"
-  system "ls -la docs"
-  system "ls -la docs/#{version}"
+  system "BUNDLE_GEMFILE=#{$source_dir}/Gemfile bundle exec jazzy -o #{$gen_docs_dir}/#{version}/api-docs --sourcekitten-sourcefile #{$json_files_dir}/#{version}/all.json --author Bow --author_url https://bow-swift.io --github_url https://github.com/bow-swift/bow --module Bow --root-url https://bow-swift.io/#{version}/api-docs --theme docs/extra/bow-jazzy-theme"
+  system "ls -la #{$source_dir}"
+  system "ls -la #{$gen_docs_dir}/#{version}"
 end
 
 
+# Initially, we save the name of the current branch/tag to be used later
+current_branch_tag = `git name-rev --name-only HEAD`
+
+#This is the list of versions that will be built, and used, as part of the process
+versions = []
+versions.unshift({
+  "title" => $default_version,
+})
+
+# Besides default, another version that will be available to select will be
+# the current branch/tag, if desired through the use of $current_branch_path
+if !$current_branch_path.to_s.empty?
+  versions.push({
+    "title" => $current_branch_path,
+  })
+end
+
+# Directory initialization
+`mkdir -p #{$json_files_dir}`
+`mkdir -p #{$publishing_dir}`
+`mkdir -p #{$gen_docs_dir}`
+
 # Initial generic logic and dependencies for the docs site
-`mkdir -p docs-json`
 system "swift package clean"
 system "swift build"
-system "bundle install --gemfile ./docs/Gemfile --path vendor/bundle"
+system "bundle install --gemfile #{$source_dir}/Gemfile --path vendor/bundle"
 
 # Following logic will process and generate the different releases specific sites
-
-# Initially, we generate the content available at master to be at /next path
-generate_json("next", modules)
-join_json("next", modules)
-generate_nef_site("next")
-generate_api_site("next")
 
 # Then, tags will contain the list of Git tags present in the repo
 tags = `git tag`.split("\n")
 
 # This is done to avoid the need to write down all the tags when we want everything in
-if !valid_tags.any?
-  valid_tags = tags
+if !$valid_tags.any?
+  $valid_tags = tags
 end
 
 if tags.any?
-  filtered_out_tags = tags.reject { |t| invalid_tags.include? t }
-  filtered_tags = filtered_out_tags.select { |t| valid_tags.include? t }
+  filtered_out_tags = tags.reject { |t| $invalid_tags.include? t }
+  filtered_tags = filtered_out_tags.select { |t| $valid_tags.include? t }
+  # First iteration is done to have the list of versions available
+  filtered_tags.each { |t|
+                        versions.push({
+                          "title" => t,
+                        })
+                      }
   filtered_tags.each { |t|
                         system "git checkout #{t}"
                         system "swift package clean"
                         system "swift build"
-                        generate_nef_site("#{t}")
-                        generate_json("#{t}", modules)
-                        join_json("#{t}", modules)
+                        generate_nef_site("#{t}", versions)
+                        generate_json("#{t}")
+                        join_json("#{t}")
                         generate_api_site("#{t}")
                       }
+
+  if filtered_tags.any?
+    if $default_version.to_s.empty?
+      $default_version = filtered_tags.last
+    end
+  else
+    $default_version = "master"
+  end
 end
 
-# The content available in the default branch will be generated by GH Pages itself
-if tags.any? && default_version.to_s.empty?
-  `git checkout #{tags.last}`
-else
-  `git checkout #{default_version}`
+
+# Now, we generate the content available at the initial branch (master?)
+# to be at $current_branch_path (/next?) path
+if !$current_branch_path.to_s.empty?
+  `git checkout #{current_branch_tag}`
+  generate_json($current_branch_path)
+  join_json($current_branch_path)
+  generate_nef_site($current_branch_path, versions)
+  generate_api_site($current_branch_path)
 end
+
+
+# The content available in the default branch will be generated by GH Pages itself
+`git checkout #{$default_version}`
+# Let's generate the versions file for the default version
+`mkdir -p #{$source_dir}/_data`
+this_versions = versions.dup;
+this_versions[this_versions.find_index("title" => $default_version)] = {
+  "title" => $default_version,
+  "this" => true
+};
+File.write("#{$source_dir}/_data/versions.json", JSON.pretty_generate(this_versions))
+
+# And then, we need to generate the API docs for the default version
+generate_json("#{$default_version}")
+join_json("#{$default_version}")
+generate_api_site("#{$default_version}")
+
+# Now we need to move default API docs to the default publishing location too.
+`mv #{$gen_docs_dir}/#{$default_version}/api-docs #{$gen_docs_dir}/`
+
+# We also move the version generated sites to its publishing destination
+`mv #{$gen_docs_dir}/* #{$publishing_dir}/`
+
+# And finally we move the source to the directory that will be published.
+# Remember that this should be the same directory set in GH Pages/Travis.
+`mv #{$source_dir}/* #{$publishing_dir}/`
