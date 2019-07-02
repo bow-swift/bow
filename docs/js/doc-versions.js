808ffcd69766c153bc0a2bc0d0e109008375be7c
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2019-07-02 13:09:46 +0200

    Include API docs dropdown selector

--- docs/js/doc-versions.js
+++ docs/js/doc-versions.js
@@ -4,20 +4,33 @@
 function displayToggle(e) {
   e.preventDefault();
   e.stopPropagation();
-  const dropdown = document.querySelector("#version-dropdown > .dropdown-content");
-  dropdown.classList.toggle("show");
-  if (dropdown.classList.contains("show")) {
-    window.addEventListener("click", closeDropdown);
-  }
-  else {
-    window.removeEventListener("click", closeDropdown);
+  // Calling close func. in case we're clicking another dropdown with one opened
+  closeDropdown(e);
+  const parent = e.target.closest("div[id$='version-dropdown']");
+  if (parent) {
+    const dropdown = parent.querySelector(".dropdown-content");
+    if (dropdown) {
+      dropdown.classList.toggle("show");
+      if (dropdown.classList.contains("show")) {
+        document.documentElement.addEventListener("click", closeDropdown);
+      }
+      else {
+        document.documentElement.removeEventListener("click", closeDropdown);
+      }
+    }
   }
 }
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks (only) outside of it
 function closeDropdown(e) {
-  const dropdown = document.querySelector("#version-dropdown > .dropdown-content");
-  const relatedTarget = e.relatedTarget || {};
-  if (relatedTarget.parentNode !== dropdown) dropdown.classList.remove("show");
-  window.removeEventListener("click", closeDropdown);
+  const dropdown = document.querySelector("div[id$='version-dropdown'] > .dropdown-content.show");
+  if (dropdown) {
+    const currentTarget = e.currentTarget || {};
+    const currentTargetParent = currentTarget.closest("div[id$='version-dropdown']");
+    const dropdownParent = dropdown.closest("div[id$='version-dropdown']");
+    if (currentTargetParent !== dropdownParent) {
+      dropdown.classList.remove("show");
+    }
+    document.documentElement.removeEventListener("click", closeDropdown);
+  }
 }
