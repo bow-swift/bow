bb0d70139be7c2b5a237bbf47ec27fb67b78a84d
Tomás Ruiz-López <truizlop@gmail.com>
2019-07-08 15:07:59 +0200

    Add API reference for Coproducts (#388)
    * Add API docs for Coproduct10
    
    * Add API docs for Coproduct9
    
    * Add API docs for Coproduct8
    
    * Add API docs for Coproduct7
    
    * Add API docs for Coproduct6
    
    * Add API docs for Coproduct5
    
    * Add API docs for Coproduct4
    
    * Add API docs for Coproduct3
    
    * Add API docs for Coproduct2

--- Sources/BowGeneric/Coproduct4.swift
+++ Sources/BowGeneric/Coproduct4.swift
@@ -1,8 +1,12 @@
 import Bow
 
+/// Witness for the `Coproduct4` data type. To be used in simulated Higher Kinded Types.
 public final class ForCoproduct4 {}
+
+/// Higher Kinded Type alias to improve readability over `Kind4`.
 public typealias Coproduct4Of<A, B, C, D> = Kind4<ForCoproduct4, A, B, C, D>
 
+/// Represents a sum type of 4 different types.
 public final class Coproduct4<A, B, C, D>: Coproduct4Of<A, B, C, D> {
     private let cop: Cop4<A, B, C, D>
 
@@ -10,22 +14,46 @@ public final class Coproduct4<A, B, C, D>: Coproduct4Of<A, B, C, D> {
         self.cop = cop
     }
 
+    /// Creates an instance of a coproduct providing a value of the first type.
+    ///
+    /// - Parameter a: Value of the first type of the coproduct.
+    /// - Returns: A coproduct with a value of the first type.
     public static func first(_ a: A) -> Coproduct4<A, B, C, D> {
         return Coproduct4(.first(a))
     }
 
+    /// Creates an instance of a coproduct providing a value of the second type.
+    ///
+    /// - Parameter b: Value of the second type of the coproduct.
+    /// - Returns: A coproduct with a value of the second type.
     public static func second(_ b: B) -> Coproduct4<A, B, C, D> {
         return Coproduct4(.second(b))
     }
 
+    /// Creates an instance of a coproduct providing a value of the third type.
+    ///
+    /// - Parameter c: Value of the third type of the coproduct.
+    /// - Returns: A coproduct with a value of the third type.
     public static func third(_ c: C) -> Coproduct4<A, B, C, D> {
         return Coproduct4(.third(c))
     }
 
+    /// Creates an instance of a coproduct providing a value of the fourth type.
+    ///
+    /// - Parameter d: Value of the fourth type of the coproduct.
+    /// - Returns: A coproduct with a value of the fourth type.
     public static func fourth(_ d: D) -> Coproduct4<A, B, C, D> {
         return Coproduct4(.fourth(d))
     }
 
+    /// Applies a function depending on the content of the coproduct.
+    ///
+    /// - Parameters:
+    ///   - fa: Function to apply with a value of the first type.
+    ///   - fb: Function to apply with a value of the second type.
+    ///   - fc: Function to apply with a value of the third type.
+    ///   - fd: Function to apply with a value of the fourth type.
+    /// - Returns: A value resulting of the application of the corresponding function based on the content of the coproduct.
     public func fold<Z>(_ fa: (A) -> Z,
                         _ fb: (B) -> Z,
                         _ fc: (C) -> Z,
@@ -38,18 +66,22 @@ public final class Coproduct4<A, B, C, D>: Coproduct4Of<A, B, C, D> {
         }
     }
 
+    /// Retrieves the value of the first type, if present.
     public var first: Option<A> {
         return fold(Option.some, constant(Option.none()), constant(Option.none()), constant(Option.none()))
     }
 
+    /// Retrieves the value of the second type, if present.
     public var second: Option<B> {
         return fold(constant(Option.none()), Option.some, constant(Option.none()), constant(Option.none()))
     }
 
+    /// Retrieves the value of the third type, if present.
     public var third: Option<C> {
         return fold(constant(Option.none()), constant(Option.none()), Option.some, constant(Option.none()))
     }
 
+    /// Retrieves the value of the fourth type, if present.
     public var fourth: Option<D> {
         return fold(constant(Option.none()), constant(Option.none()), constant(Option.none()), Option.some)
     }
