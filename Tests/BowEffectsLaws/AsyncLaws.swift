f4a06dee8cdf499d44d361a56afca66ccb3d0484
Tomás <truizlop@gmail.com>
2019-07-08 16:51:40 +0200

    Make effects laws public

--- Tests/BowEffectsLaws/AsyncLaws.swift
+++ Tests/BowEffectsLaws/AsyncLaws.swift
@@ -3,9 +3,8 @@ import SwiftCheck
 import Bow
 import BowEffects
 
-class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
-    
-    static func check() {
+public class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
+    public static func check() {
         success()
         error()
     }
@@ -17,7 +16,6 @@ class AsyncLaws<F: Async & EquatableK> where F.E: Arbitrary {
     }
     
     private static func error() {
-        
         property("Error equivalence") <- forAll { (error: F.E) in
             return F.runAsync({ ff in ff(Either<F.E, Int>.left(error)) }) ==
                 F.raiseError(error)
