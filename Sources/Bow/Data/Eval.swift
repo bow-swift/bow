667c7e0d96027285f56ff50d975ccd0d13a7052a
Tomás <truizlop@gmail.com>
2019-07-12 16:26:55 +0200

    Revamp Eval

--- Sources/Bow/Data/Eval.swift
+++ Sources/Bow/Data/Eval.swift
@@ -4,7 +4,6 @@ public final class ForEval {}
 public typealias EvalOf<A> = Kind<ForEval, A>
 
 public class Eval<A>: EvalOf<A> {
-
     public static func now(_ a: A) -> Eval<A> {
         return Now<A>(a)
     }
@@ -17,8 +16,8 @@ public class Eval<A>: EvalOf<A> {
         return Always<A>(f)
     }
 
-    public static func deferEvaluation(_ f: @escaping () -> Eval<A>) -> Eval<A> {
-        return Call<A>(f)
+    public static func `defer`(_ f: @escaping () -> Eval<A>) -> Eval<A> {
+        return Defer<A>(f)
     }
 
     public static var Unit: Eval<()> {
@@ -62,8 +61,8 @@ public postfix func ^<A>(_ fa: EvalOf<A>) -> Eval<A> {
     return Eval.fix(fa)
 }
 
-class Now<A>: Eval<A> {
-    fileprivate let a: A
+private class Now<A>: Eval<A> {
+    private let a: A
 
     init(_ a: A) {
         self.a = a
@@ -78,9 +77,9 @@ class Now<A>: Eval<A> {
     }
 }
 
-class Later<A>: Eval<A> {
-    fileprivate lazy var a: A = f()
-    fileprivate let f: () -> A
+private class Later<A>: Eval<A> {
+    private lazy var a: A = f()
+    private let f: () -> A
 
     init(_ f: @escaping () -> A) {
         self.f = f
@@ -95,8 +94,8 @@ class Later<A>: Eval<A> {
     }
 }
 
-class Always<A>: Eval<A> {
-    fileprivate let f: () -> A
+private class Always<A>: Eval<A> {
+    private  let f: () -> A
 
     init(_ f: @escaping () -> A) {
         self.f = f
@@ -111,8 +110,8 @@ class Always<A>: Eval<A> {
     }
 }
 
-private class Call<A>: Eval<A> {
-    fileprivate let thunk: () -> Eval<A>
+private class Defer<A>: Eval<A> {
+    private  let thunk: () -> Eval<A>
 
     init(_ thunk: @escaping () -> Eval<A>) {
         self.thunk = thunk
@@ -123,161 +122,54 @@ private class Call<A>: Eval<A> {
     }
 
     override func value() -> A {
-        return Call.collapse(self).value()
-    }
-
-    static func collapse(_ fa: Eval<A>) -> Eval<A> {
-        switch fa {
-        case is Call<A>:
-            let faCall = fa as! Call
-            return collapse(faCall.thunk())
-        case is Compute<A>:
-            return Call2Compute<A>(fa as! Compute<A>)
-        default:
-            return fa
-        }
+        return thunk().value()
     }
 }
 
-private class Compute<A>: Eval<A> {
-    func start<S>() -> Eval<S> {
-        fatalError("Must be implemented by Compute subclass")
-    }
-
-    func run<S>(_ s: S) -> Eval<A> {
-        fatalError("Must be implemented by Compute subclass")
-    }
-
-    override func memoize() -> Eval<A> {
-        return Eval<A>.later(value)
-    }
-
-    override func value() -> A {
-        var curr: Eval<A> = self
-        var fs: [(AnyObject) -> Eval<A>] = []
-
-        while true {
-            switch curr {
-            case is Compute:
-                let currComp = curr as! Compute
-                let startResult = currComp.start() as Eval<A>
-                switch startResult {
-                case is Compute:
-                    let inStartFunc: (AnyObject) -> Eval<A> = { s in (startResult as! Compute).run(s as! A) }
-                    let outStartFunc: (AnyObject) -> Eval<A> = { s in currComp.run(s as! A) }
-                    curr = (startResult as! Compute).start()
-                    fs = [inStartFunc, outStartFunc] + fs
-                default:
-                    curr = currComp.run(startResult.value())
-                }
-            default:
-                if !fs.isEmpty {
-                    let f = fs.removeFirst()
-                    curr = f(curr.value() as AnyObject)
-                } else {
-                    return curr.value()
-                }
-            }
-        }
-    }
-}
-
-private class Call2Compute<A>: Compute<A> {
-    private let fa: Compute<A>
-
-    init(_ fa: Compute<A>) {
-        self.fa = fa
-    }
-
-    override func start<S>() -> Eval<S> {
-        return fa.start()
-    }
-
-    override func run<S>(_ s: S) -> Eval<A> {
-        return Call.collapse(fa.run(s))
-    }
-}
-
-private class FlatmapCompute<A, B>: Compute<A> {
-    private let compute: Compute<B>
-    private let f: (B) -> Eval<A>
-
-    init(_ compute: Compute<B>, _ f: @escaping (B) -> Eval<A>) {
-        self.compute = compute
-        self.f = f
-    }
-
-    override func start<S>() -> Eval<S> {
-        return compute.start()
-    }
-
-    override func run<S>(_ s: S) -> Eval<A> {
-        return RunCompute(compute, f, s)
-    }
-}
-
-private class RunCompute<A, B, S>: Compute<A> {
-    private let compute: Compute<B>
-    private let f: (B) -> Eval<A>
-    private let s: S
-
-    init(_ compute: Compute<B>, _ f: @escaping (B) -> Eval<A>, _ s: S) {
-        self.compute = compute
-        self.f = f
-        self.s = s
-    }
-
-    override func start<S1>() -> Eval<S1> {
-        return compute.run(s) as! Eval<S1>
-    }
-
-    override func run<S1>(_ s: S1) -> Eval<A> {
-        return f(s as! B)
-    }
-}
-
-private class FlatmapCall<A, B>: Compute<A> {
-    private let call: Call<B>
-    private let f: (B) -> Eval<A>
-
-    init(_ call: Call<B>, _ f: @escaping (B) -> Eval<A>) {
-        self.call = call
-        self.f = f
-    }
-
-    override func start<S>() -> Eval<S> {
-        return call.thunk() as! Eval<S>
-    }
-
-    override func run<S>(_ s: S) -> Eval<A> {
-        return f(s as! B)
-    }
-}
-
-private class FlatmapDefault<A, B>: Compute<A> {
-    private let eval: Eval<B>
-    private let f: (B) -> Eval<A>
-
-    init(_ eval: Eval<B>, _ f: @escaping (B) -> Eval<A>) {
+private class FMap<A, B>: Eval<B> {
+    private let eval: Eval<A>
+    private let f: (A) -> B
+    
+    init(_ eval: Eval<A>, _ f: @escaping (A) -> B) {
         self.eval = eval
         self.f = f
     }
-
-    override func start<S>() -> Eval<S> {
-        return eval as! Eval<S>
+    
+    override func memoize() -> Eval<B> {
+        return Eval<B>.later { self.value() }
     }
-
-    override func run<S>(_ s: S) -> Eval<A> {
-        return f(s as! B)
+    
+    override func value() -> B {
+        return f(eval.value())
     }
 }
 
+private class Bind<A, B>: Eval<B> {
+    private let eval: Eval<A>
+    private let f: (A) -> EvalOf<B>
+    
+    init(_ eval: Eval<A>, _ f: @escaping (A) -> EvalOf<B>) {
+        self.eval = eval
+        self.f = f
+    }
+    
+    override func memoize() -> Eval<B> {
+        return Eval.later { self.value() }
+    }
+    
+    override func value() -> B {
+        return f(eval.value())^.value()
+    }
+}
+
+// MARK: Instance of `Functor` for `Eval`
 extension ForEval: Functor {
     public static func map<A, B>(_ fa: Kind<ForEval, A>, _ f: @escaping (A) -> B) -> Kind<ForEval, B> {
-        return flatMap(fa, { a in Eval.now(f(a)) })
+        return FMap(fa^, f)
     }
 }
 
+// MARK: Instance of `Applicative` for `Eval`
 extension ForEval: Applicative {
     public static func pure<A>(_ a: A) -> Kind<ForEval, A> {
         return Eval.now(a)
@@ -287,29 +179,37 @@ extension ForEval: Applicative {
 // MARK: Instance of `Selective` for `Eval`
 extension ForEval: Selective {}
 
+// MARK: Instance of `Monad` for `Eval`
 extension ForEval: Monad {
     public static func flatMap<A, B>(_ fa: Kind<ForEval, A>, _ f: @escaping (A) -> Kind<ForEval, B>) -> Kind<ForEval, B> {
-        let ff: (A) -> Eval<B> = { a in Eval.fix(f(a)) }
-        switch fa {
-        case let compute as Compute<A>:
-            return FlatmapCompute(compute, ff)
-        case let call as Call<A>:
-            return FlatmapCall(call, ff)
-        default:
-            return FlatmapDefault(Eval.fix(fa), ff)
-        }
+        return Bind(fa^, f)
     }
 
     public static func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> Kind<ForEval, Either<A, B>>) -> Kind<ForEval, B> {
-        return Eval.fix(f(a)).flatMap{ either in
+        return f(a).flatMap{ either in
             either.fold({ a in tailRecM(a, f) },
                         Eval<B>.pure)
         }
     }
 }
 
+// MARK: Instance of `Comonad` for `Eval`
+extension ForEval: Comonad {
+    public static func coflatMap<A, B>(_ fa: Kind<ForEval, A>, _ f: @escaping (Kind<ForEval, A>) -> B) -> Kind<ForEval, B> {
+        return Eval.later { f(fa) }
+    }
+    
+    public static func extract<A>(_ fa: Kind<ForEval, A>) -> A {
+        return fa^.value()
+    }
+}
+
+// MARK: Instance of `Bimonad` for `Eval`
+extension ForEval: Bimonad {}
+
+// MARK: Instance of `EquatableK` for `Eval`
 extension ForEval: EquatableK {
-    public static func eq<A>(_ lhs: Kind<ForEval, A>, _ rhs: Kind<ForEval, A>) -> Bool where A: Equatable {
+    public static func eq<A: Equatable>(_ lhs: Kind<ForEval, A>, _ rhs: Kind<ForEval, A>) -> Bool {
         return Eval.fix(lhs).value() == Eval.fix(rhs).value()
     }
 }
--- Sources/BowRecursionSchemes/Recursion.swift
+++ Sources/BowRecursionSchemes/Recursion.swift
@@ -9,7 +9,7 @@ public extension Functor {
                            _ coalgebra : @escaping Coalgebra<Self, A>,
                            _ a : A) -> B {
         func h(_ a : A) -> Eval<B> {
-            return algebra(map(coalgebra(a), { x in Eval.deferEvaluation({ h(x) }) }))
+            return algebra(map(coalgebra(a), { x in Eval.`defer`({ h(x) }) }))
         }
         return h(a).value()
     }
