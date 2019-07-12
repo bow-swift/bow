633b9e6da8405b93c59035ee2db5568864bd0c4b
Tomás <truizlop@gmail.com>
2019-07-12 10:56:54 +0200

    Fix ambiguity in operators in tests

--- Tests/BowOpticsLaws/PrismLaws.swift
+++ Tests/BowOpticsLaws/PrismLaws.swift
@@ -1,6 +1,7 @@
 import SwiftCheck
 import Bow
 import BowOptics
+import BowLaws
 
 public class PrismLaws<A: Arbitrary & Equatable, B: Arbitrary & CoArbitrary & Hashable & Equatable> {
     
@@ -14,38 +15,38 @@ public class PrismLaws<A: Arbitrary & Equatable, B: Arbitrary & CoArbitrary & Ha
     }
     
     private static func partialRoundTripOneWay(_ prism : Prism<A, B>) {
-        property("Partial round trip one way") <- forAll { (a : A) in
+        property("Partial round trip one way") <~ forAll { (a : A) in
             return prism.getOrModify(a).fold(id, prism.reverseGet) == a
         }
     }
     
     private static func roundTripOtherWay(_ prism : Prism<A, B>) {
-        property("Round trip other way") <- forAll { (b : B) in
+        property("Round trip other way") <~ forAll { (b : B) in
             return prism.getOption(prism.reverseGet(b)) == Option.some(b)
         }
     }
     
     private static func modifyId(_ prism : Prism<A, B>) {
-        property("Modify id") <- forAll { (a : A) in
+        property("Modify id") <~ forAll { (a : A) in
             return prism.modify(a, id) == a
         }
     }
     
     private static func composeModify(_ prism : Prism<A, B>) {
-        property("Compose modify") <- forAll { (a : A, f : ArrowOf<B, B>, g : ArrowOf<B, B>) in
+        property("Compose modify") <~ forAll { (a : A, f : ArrowOf<B, B>, g : ArrowOf<B, B>) in
             return prism.modify(prism.modify(a, f.getArrow), g.getArrow) ==
                 prism.modify(a, g.getArrow <<< f.getArrow)
         }
     }
     
     private static func consistentSetModify(_ prism : Prism<A, B>) {
-        property("Consistent set - modify") <- forAll { (a : A, b : B) in
+        property("Consistent set - modify") <~ forAll { (a : A, b : B) in
             return prism.set(a, b) == prism.modify(a, constant(b))
         }
     }
     
     private static func consistentModifyModifyFId(_ prism : Prism<A, B>) {
-        property("Consistent modify - modifyF Id") <- forAll { (a : A, f : ArrowOf<B, B>) in
+        property("Consistent modify - modifyF Id") <~ forAll { (a : A, f : ArrowOf<B, B>) in
             return prism.modify(a, f.getArrow) == Id.fix(prism.modifyF(a, { b in Id(f.getArrow(b)) })).value
         }
     }
