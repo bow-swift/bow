b12db0f6615ea31aff55fb269a926ed0c9834b1e
Tomás <truizlop@gmail.com>
2019-09-11 15:25:55 +0200

    Update MaybeK to use the new Effects type class hierarchy

--- Sources/BowRx/MaybeK.swift
+++ Sources/BowRx/MaybeK.swift
@@ -19,8 +19,24 @@ public final class MaybeK<A>: MaybeKOf<A> {
         return value as! MaybeK<A>
     }
 
-    public static func from(_ f: @escaping () -> A) -> MaybeK<A> {
-        return MaybeK.fix(suspend { MaybeK.fix(pure(f())) })
+    public static func from(_ f: @escaping () throws -> A) -> MaybeK<A> {
+        return ForMaybeK.defer {
+            do {
+                return pure(try f())
+            } catch {
+                return raiseError(error)
+            }
+        }^
+    }
+    
+    public static func invoke(_ f: @escaping () throws -> MaybeKOf<A>) -> MaybeK<A> {
+        return ForMaybeK.defer {
+            do {
+                return try f()
+            } catch {
+                return raiseError(error)
+            }
+        }^
     }
 
     public init(_ value: Maybe<A>) {
@@ -49,12 +65,14 @@ public postfix func ^<A>(_ value: MaybeKOf<A>) -> MaybeK<A> {
     return MaybeK.fix(value)
 }
 
+// MARK: Instance of `Functor` for `MaybeK`
 extension ForMaybeK: Functor {
     public static func map<A, B>(_ fa: Kind<ForMaybeK, A>, _ f: @escaping (A) -> B) -> Kind<ForMaybeK, B> {
         return MaybeK.fix(fa).value.map(f).k()
     }
 }
 
+// MARK: Instance of `Applicative` for `MaybeK`
 extension ForMaybeK: Applicative {
     public static func pure<A>(_ a: A) -> Kind<ForMaybeK, A> {
         return Maybe.just(a).k()
@@ -64,6 +82,7 @@ extension ForMaybeK: Applicative {
 // MARK: Instance of `Selective` for `MaybeK`
 extension ForMaybeK: Selective {}
 
+// MARK: Instance of `Monad` for `MaybeK`
 extension ForMaybeK: Monad {
     public static func flatMap<A, B>(_ fa: Kind<ForMaybeK, A>, _ f: @escaping (A) -> Kind<ForMaybeK, B>) -> Kind<ForMaybeK, B> {
         return MaybeK.fix(fa).value.flatMap { a in MaybeK<B>.fix(f(a)).value }.k()
@@ -76,6 +95,7 @@ extension ForMaybeK: Monad {
     }
 }
 
+// MARK: Instance of `Foldable` for `MaybeK`
 extension ForMaybeK: Foldable {
     public static func foldLeft<A, B>(_ fa: Kind<ForMaybeK, A>, _ b: B, _ f: @escaping (B, A) -> B) -> B {
         return MaybeK.fix(fa).fold(constant(b), { a in f(b, a) })
@@ -86,6 +106,7 @@ extension ForMaybeK: Foldable {
     }
 }
 
+// MARK: Instance of `ApplicativeError` for `MaybeK`
 extension ForMaybeK: ApplicativeError {
     public typealias E = Error
 
@@ -98,31 +119,32 @@ extension ForMaybeK: ApplicativeError {
     }
 }
 
+// MARK: Instance of `MonadError` for `MaybeK`
 extension ForMaybeK: MonadError {}
 
+// MARK: Instance of `MonadDefer` for `MaybeK`
 extension ForMaybeK: MonadDefer {
     public static func `defer`<A>(_ fa: @escaping () -> Kind<ForMaybeK, A>) -> Kind<ForMaybeK, A> {
-        fatalError("TODO: implement this")
-    }
-
-    public static func bracketCase<A, B>(_ fa: Kind<ForMaybeK, A>, _ release: @escaping (A, ExitCase<Error>) -> Kind<ForMaybeK, ()>, _ use: @escaping (A) throws -> Kind<ForMaybeK, B>) -> Kind<ForMaybeK, B> {
-        fatalError("TODO: implement this")
-    }
-
-    public static func suspend<A>(_ fa: @escaping () -> Kind<ForMaybeK, A>) -> Kind<ForMaybeK, A> {
-        return Maybe.deferred { MaybeK<A>.fix(fa()).value }.k()
+        return Maybe.deferred { fa()^.value }.k()
     }
 }
 
+// MARK: Instance of `Async` for `MaybeK`
 extension ForMaybeK: Async {
     public static func asyncF<A>(_ procf: @escaping (@escaping (Either<Error, A>) -> ()) -> Kind<ForMaybeK, ()>) -> Kind<ForMaybeK, A> {
-        fatalError("TODO: implement this")
+        return Maybe<A>.create { emitter in
+            return procf { either in
+                either.fold(
+                    { error in emitter(.error(error)) },
+                    { value in emitter(.success(value)) })
+            }^.value.subscribe(onError: { e in emitter(.error(e)) })
+        }.k()
     }
-
+    
     public static func continueOn<A>(_ fa: Kind<ForMaybeK, A>, _ queue: DispatchQueue) -> Kind<ForMaybeK, A> {
-        fatalError("TODO: implement this")
+        return fa^.value.observeOn(SerialDispatchQueueScheduler(queue: queue, internalSerialQueueName: queue.label)).k()
     }
-
+    
     public static func runAsync<A>(_ fa: @escaping ((Either<Error, A>) -> ()) throws -> ()) -> Kind<ForMaybeK, A> {
         return Maybe.create { emitter in
             do {
@@ -136,8 +158,50 @@ extension ForMaybeK: Async {
     }
 }
 
+// MARK: Instance of `Effect` for `MaybeK`
 extension ForMaybeK: Effect {
     public static func runAsync<A>(_ fa: Kind<ForMaybeK, A>, _ callback: @escaping (Either<ForMaybeK.E, A>) -> Kind<ForMaybeK, ()>) -> Kind<ForMaybeK, ()> {
         return MaybeK<A>.fix(fa).runAsync(callback)
+    }
+}
+
+// MARK: Instance of `Concurrent` for `MaybeK`
+extension ForMaybeK: Concurrent {
+    public static func parMap<A, B, Z>(_ fa: Kind<ForMaybeK, A>, _ fb: Kind<ForMaybeK, B>, _ f: @escaping (A, B) -> Z) -> Kind<ForMaybeK, Z> {
+        return Maybe.zip(fa^.value, fb^.value, resultSelector: f).k()
+    }
+    
+    public static func parMap<A, B, C, Z>(_ fa: Kind<ForMaybeK, A>, _ fb: Kind<ForMaybeK, B>, _ fc: Kind<ForMaybeK, C>, _ f: @escaping (A, B, C) -> Z) -> Kind<ForMaybeK, Z> {
+        return Maybe.zip(fa^.value, fb^.value, fc^.value, resultSelector: f).k()
+    }
+}
+
+// MARK: Instance of `Bracket` for `MaybeK`
+extension ForMaybeK: Bracket {
+    public static func bracketCase<A, B>(
+        _ fa: Kind<ForMaybeK, A>,
+        _ release: @escaping (A, ExitCase<Error>) -> Kind<ForMaybeK, ()>,
+        _ use: @escaping (A) throws -> Kind<ForMaybeK, B>) -> Kind<ForMaybeK, B> {
+        return Maybe.create { emitter in
+            return fa.handleErrorWith { t in MaybeK.from { emitter(.error(t)) }.value.flatMap { Maybe.error(t) }.k() }
+                .flatMap { a in
+                    MaybeK.invoke { try use(a)^ }^
+                    .value
+                    .do(onError: { t in
+                            let _ = MaybeK.defer { release(a, .error(t)) }^.value
+                                .subscribe(onSuccess: { _ in emitter(.error(t)) },
+                                           onError: { e in emitter(.error(e)) })
+                        },
+                        afterCompleted: {
+                            let _ = MaybeK.defer { release(a, .completed) }^.value
+                                .subscribe(onSuccess: { emitter(.completed) },
+                                           onError: { e in emitter(.error(e)) })
+                        },
+                        onDispose: {
+                            let _ = MaybeK.defer { release(a, .canceled) }^.value.subscribe()
+                        })
+                    .k()
+                }^.value.subscribe(onSuccess: { b in emitter(.success(b)) })
+        }.k()
     }
 }
