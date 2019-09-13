efa9dfc6a153a1fda92384988f2f1724b3e6aa4a
Tomás <truizlop@gmail.com>
2019-09-13 12:41:38 +0200

    Add API docs for ConsoleIO

--- Sources/BowEffects/Foundation/ConsoleIO.swift
+++ Sources/BowEffects/Foundation/ConsoleIO.swift
@@ -1,10 +1,13 @@
 import Foundation
 
+/// Utilities to read and write to the standard input/output in a functional manner.
 public enum ConsoleIO {
+    /// IO suspended version of `Swift.print(_:separator:terminator:)`. Refer to that function for further documentation.
     public static func print<E: Error>(_ line: Any, separator: String = " ", terminator: String = "\n") -> IO<E, ()> {
         return IO.invoke { Swift.print(line, separator: separator, terminator: terminator) }
     }
     
+    /// IO suspended version of `Swift.readLine(strippingNewline:)`. Refer to that function for further documentation.
     public static func readLine<E: Error>(strippingNewline: Bool = true) -> IO<E, String?> {
         return IO.invoke { Swift.readLine(strippingNewline: strippingNewline) }
     }
