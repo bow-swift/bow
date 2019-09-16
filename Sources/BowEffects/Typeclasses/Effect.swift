a1c4321e5ffc23ca1e86c7bb0974a484f61f541f
Tomás <truizlop@gmail.com>
2019-09-16 12:06:05 +0200

    Add API docs for Effect

--- Sources/BowEffects/Typeclasses/Effect.swift
+++ Sources/BowEffects/Typeclasses/Effect.swift
@@ -1,12 +1,23 @@
 import Foundation
 import Bow
 
+/// Effect is a Monad that can suspend side effects into the context implementing it and supports lazy and potentially asynchronous evaluation.
 public protocol Effect: Async {
+    /// Evaluates a side-effectful computation.
+    ///
+    /// - Parameters:
+    ///   - fa: Computation to be evaluated.
+    ///   - callback: Callback to process the result of the computation.
+    /// - Returns: A computation describing the evaluation.
     static func runAsync<A>(_ fa: Kind<Self, A>, _ callback: @escaping (Either<E, A>) -> Kind<Self, ()>) -> Kind<Self, ()>
 }
 
 // MARK: Syntax for Effect
 public extension Kind where F: Effect {
+    /// Evaluates a side-effectful computation.
+    ///
+    /// - Parameter callback: Callback to process the result of the computation.
+    /// - Returns: A computation describing the evaluation.
     func runAsync(_ callback: @escaping (Either<F.E, A>) -> Kind<F, ()>) -> Kind<F, ()> {
         return F.runAsync(self, callback)
     }
