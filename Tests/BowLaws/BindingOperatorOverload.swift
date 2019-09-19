e747c2240dbe8b87be6cdae7b2521486fd477ec1
Tomás <truizlop@gmail.com>
2019-09-19 09:39:25 +0200

    Remove incorrect helpers for other monads

--- Tests/BowLaws/BindingOperatorOverload.swift
+++ Tests/BowLaws/BindingOperatorOverload.swift
@@ -10,10 +10,6 @@ public func <--<F: Monad, A>(_ bound: BoundVar<F, A>, _ fa: @autoclosure @escapi
     return bound <- fa()
 }
 
-public func <--<F: MonadWriter, A, B>(_ bounds: (BoundVar<F, B>, BoundVar<F, A>), _ handler: ListenHandler<F, B>) -> BindingExpression<F> {
-    return bounds <- handler
-}
-
-public func <--<F: MonadWriter, A>(_ bound: BoundVar<F, A>, _ handler: CensorHandler<F>) -> BindingExpression<F> {
-    return bound <- handler
+public func <--<F: Monad, A, B>(_ bound: (BoundVar<F, A>, BoundVar<F, B>), _ fa: @autoclosure @escaping () -> Kind<F, (A, B)>) -> BindingExpression<F> {
+    return bound <- fa()
 }
--- Tests/BowTests/Arrow/KleisliTest.swift
+++ Tests/BowTests/Arrow/KleisliTest.swift
@@ -27,23 +27,4 @@ class KleisliTest: XCTestCase {
     func testMonadErrorLaws() {
         MonadErrorLaws<KleisliPartial<EitherPartial<CategoryError>, Int>>.check()
     }
-    
-    func testReaderComprehension() {
-        property("Monad comprehensions with MonadReader utilities") <~ forAll { (v1: Int, v2: Int) in
-            let a = KleisliPartial<ForId, Int>.var(Int.self)
-            
-            let x = binding(
-                |<-askReader(),
-                localReader { x in 2 * x },
-                a <-- KleisliPartial<ForId, Int>.pure(v1),
-                yield: a.get)^.invoke(v2)^.value
-            
-            let y = KleisliPartial<ForId, Int>.ask()
-                .local { x in 2 * x }
-                .flatMap { _ in KleisliPartial<ForId, Int>.pure(v1) }^
-                .invoke(v2)^.value
-            
-            return x == y
-        }
-    }
 }
