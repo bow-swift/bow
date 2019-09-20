240c0f95d0f9412738ba847dbee39aaa5e2b6349
Tomás Ruiz-López <truizlop@gmail.com>
2019-09-20 09:31:57 +0200

    Fix Monad comprehensions (#429)
    * Remove incorrect helpers for other monads
    
    * Fix wrong implementation for Monad comprehensions
    
    * Overload binding operator for testing
    
    * Add test for equivalence of flatMap and comprehensions

--- Tests/BowLaws/MonadWriterLaws.swift
+++ Tests/BowLaws/MonadWriterLaws.swift
@@ -7,7 +7,6 @@ public class MonadWriterLaws<F: MonadWriter & EquatableK> where F.W == Int {
         tellFusion()
         listenPure()
         listenWriterProperty()
-        comprehension()
     }
     
     private static func writerPure() {
@@ -33,26 +32,6 @@ public class MonadWriterLaws<F: MonadWriter & EquatableK> where F.W == Int {
         property("Listen writer") <~ forAll { (a: Int, w: Int) in
             let tuple = (w, a)
             return isEqual(F.listen(F.writer(tuple)), F.map(F.tell(tuple.0), { _ in tuple }))
-        }
-    }
-    
-    private static func comprehension() {
-        property("Bindings") <~ forAll { (w: Int) in
-            let v1 = F.var(String.self)
-            let v2 = F.var(Void.self)
-            
-            let x = binding(
-                tellWriter(w),
-                |<-censorWriter { n in 2 * n },
-                (v1, v2) <-- listensWriter { x in "\(x)" },
-                yield: v1.get + "\(v2.get)")
-            
-            let y = F.tell(w)
-                .censor { n in 2 * n }
-                .listens { x in "\(x)" }
-                .map { x in x.0 + "\(x.1)" }
-            
-            return x == y
         }
     }
 }
