b06e0c849a213d0405d85991023f8dd532f4629c
Devesh Shetty <devesh.shetty007@gmail.com>
2019-10-19 22:00:48 -0400

    Add monoidal and monoidal laws

---
+++ Sources/Bow/Typeclasses/Monoidal.swift
@@ -0,0 +1,23 @@
+/// The Monoidal type class adds an identity element to Semigroupal type class by defining the function identity
+///
+/// identity returns a specific identity `Kind<F, A>` value for a given type F and A.
+///
+/// This type class complies with the following law:
+/// fa.product(identity) == identity.product(fa) == identity
+///
+/// In addition, the laws of Semigroupal type class also apply.
+///
+/// Currently, Monoidal instance is defined for Option
+///
+public protocol Monoidal: Semigroupal {
+    
+    /// Given a type A, create a "identity" for a F<A> value
+    static func identity<A>() -> Kind<Self, A>
+}
+
+// MARK: Syntax for Monoidal
+public extension Kind where F: Monoidal {
+    static func identity() -> Kind<F, A> {
+        F.identity()
+    }
+}
