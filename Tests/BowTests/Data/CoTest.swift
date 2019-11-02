20bd19d63fc6623a9813e0edd4c7598988aedb7d
Brandon Kase <bkase@o1labs.org>
2019-11-02 15:55:53 -0700

    Add Co, Pairing

---
+++ Tests/BowTests/Data/CoTest.swift
@@ -0,0 +1,26 @@
+import XCTest
+import BowLaws
+import Bow
+import BowGenerators
+
+/// Test Co and pairing (with zap) is used for the EquatableK instance
+
+extension CoPartial: EquatableK where W == ForId {
+    public static func eq<A>(_ lhs: Kind<CoPartial<W>, A>, _ rhs: Kind<CoPartial<W>, A>) -> Bool where A : Equatable {
+        return Co<W, A>.pair().zap(Id({ $0 }), ga: Co.fix(lhs)) == Co<W, A>.pair().zap(Id({ $0 }), ga: Co.fix(rhs))
+    }
+}
+
+class CoTest: XCTestCase {
+    func testFunctorLaws() {
+            FunctorLaws<CoPartial<ForId>>.check()
+    }
+    
+    func testApplicativeLaws() {
+        ApplicativeLaws<CoPartial<ForId>>.check()
+    }
+    
+    func testMonadLaws() {
+        MonadLaws<CoPartial<ForId>>.check()
+    }
+}
