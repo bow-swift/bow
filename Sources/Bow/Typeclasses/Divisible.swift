bc78230bab74b73e31ea30e016e7ee6c6350b49d
Devesh Shetty <devesh.shetty007@gmail.com>
2019-11-02 00:55:10 -0400

    Address comments

--- Sources/Bow/Typeclasses/Divisible.swift
+++ Sources/Bow/Typeclasses/Divisible.swift
@@ -1,11 +1,12 @@
 /// Divisible extends Divide by providing an empty value.
 public protocol Divisible: Divide {
-    /// Provides an empty value for Kind<Self, A>
+    /// Provides an empty value for this Divisible instance
     static func conquer<A>() -> Kind<Self, A>
 }
 
 // MARK: Syntax for Divisible
 public extension Kind where F: Divisible {
+    /// Provides an empty value for this Divisible instance
     static func conquer() -> Kind<F, A> {
         F.conquer()
     }
