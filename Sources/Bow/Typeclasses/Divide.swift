69c8103c59f61df39cfaeaeb153ed4ae8d2b96b5
Tomás <truizlop@gmail.com>
2019-11-20 12:40:51 +0100

    Reformat Divide API docs

--- Sources/Bow/Typeclasses/Divide.swift
+++ Sources/Bow/Typeclasses/Divide.swift
@@ -1,24 +1,19 @@
-/// Divide is a typeclass that models the divide part of divide and conquer.
-///
-/// Divide basically states: Given a Kind<F, A> and a Kind<F, B> and a way to turn C into a tuple of A and B it gives you a Kind<F, C>.
-///
+/// Divide is a type class that models the divide part of divide and conquer.
 public protocol Divide: Contravariant {
-    /// divide takes two data-types of type Kind<Self, A> and Kind<Self, B> and produces a type of Kind<Self, C> when given
-    /// a function from (C) -> (A, B).
-    /// - Parameter fa: data of type Kind<Self, A>
-    /// - Parameter fb: data of type Kind<Self, B>
-    /// - Parameter f: a function to transform C into (A, B)
-    /// - Returns: Kind<Self, C>
+    /// Takes two computations and provides a computation using a function that specifies how to divide the returned type into the two provided types.
+    /// - Parameter fa: 1st computation.
+    /// - Parameter fb: 2nd computation.
+    /// - Parameter f: Dividing function.
+    /// - Returns: A computation that merges the divided parts.
     static func divide<A, B, C>(_ fa: Kind<Self, A>, _ fb: Kind<Self, B>, _ f: (C) -> (A, B)) -> Kind<Self, C>
 }
 
 // MARK: Syntax for Divide
 public extension Kind where F: Divide {
-    /// divide takes a data-type of type Kind<F, B> and produces a type of Kind<F, C> when given
-    /// a function from (C) -> (A, B).
-    /// - Parameter fb: data of type Kind<F, B>
-    /// - Parameter f: a function to transform C into (A, B)
-    /// - Returns: Kind<F, C>
+    /// Takes a computation and provides a computation using a function that specifies how to divide the returned type into the two provided types.
+    /// - Parameter fb: 2nd computation.
+    /// - Parameter f: Dividing function.
+    /// - Returns: A computation that merges the divided parts.
     func divide<B, C>(_ fb: Kind<F, B>, _ f: (C) -> (A, B)) -> Kind<F, C> {
         F.divide(self, fb, f)
     }
