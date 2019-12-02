26f489f86065111e603dfa9edc460011c81bc328
Tomás Ruiz-López <truizlop@gmail.com>
2019-12-02 16:21:11 +0100

    Hide visibility of unstable data types (#483)
    * Remove public visibility in Co
    
    * Remove public visibility in Pairing
    
    * Comment out tests temporarily

--- Sources/Bow/Data/Pairing.swift
+++ Sources/Bow/Data/Pairing.swift
@@ -1,26 +1,26 @@
 // type Pairing f g = forall a b. f (a -> b) -> g a -> b
 
-public final class ForPairing {}
-public final class PairingPartial<F: Functor>: Kind<ForPairing, F> {}
-public typealias PairingOf<F: Functor, G: Functor> =  Kind<PairingPartial<F>, G>
+final class ForPairing {}
+final class PairingPartial<F: Functor>: Kind<ForPairing, F> {}
+typealias PairingOf<F: Functor, G: Functor> =  Kind<PairingPartial<F>, G>
 
-public class Pairing<F: Functor, G: Functor> : PairingOf<F, G> {
+class Pairing<F: Functor, G: Functor> : PairingOf<F, G> {
     internal let pair : (Kind<F, (/*A*/Any) -> /*B*/Any>) -> (Kind<G, /*A*/Any>) -> /*B*/Any
     
-    public static func fix(_ value : PairingOf<F, G>) -> Pairing<F, G> {
+    static func fix(_ value : PairingOf<F, G>) -> Pairing<F, G> {
         value as! Pairing<F, G>
     }
     
-    public init(_ pair: @escaping (Kind<F, (/*A*/Any) -> /*B*/Any>) -> (Kind<G, /*A*/Any>) -> /*B*/Any) {
+    init(_ pair: @escaping (Kind<F, (/*A*/Any) -> /*B*/Any>) -> (Kind<G, /*A*/Any>) -> /*B*/Any) {
         self.pair = pair
     }
     
-    /// Annihalate the `F` and `G` effects effectively calling the wrapped function in `F` with the wrapped value
+    /// Annihilate the `F` and `G` effects effectively calling the wrapped function in `F` with the wrapped value
     ///
     /// - Parameter fab: An `F`-effectful `A -> B`
     /// - Parameter ga: A `G`-effectful `A`
     /// - Returns: A pure `B`
-    public func zap<A, B>(_ fab: Kind<F, (A) -> B>, ga: Kind<G, A>) -> B {
+    func zap<A, B>(_ fab: Kind<F, (A) -> B>, ga: Kind<G, A>) -> B {
         let gany = ga.map{ $0 as Any }
         let fany =
             fab.map{ aArrb in { (any: Any) in
@@ -33,6 +33,6 @@ public class Pairing<F: Functor, G: Functor> : PairingOf<F, G> {
 ///
 /// - Parameter value: Value in higher-kind form.
 /// - Returns: Value cast to Pairing.
-public postfix func ^<F, G>(_ value: PairingOf<F, G>) -> Pairing<F, G> {
+postfix func ^<F, G>(_ value: PairingOf<F, G>) -> Pairing<F, G> {
     return Pairing.fix(value)
 }
