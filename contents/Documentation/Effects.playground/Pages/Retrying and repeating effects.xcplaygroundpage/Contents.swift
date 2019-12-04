efa19bf98138ae1f6eeac9a2e1b185ab03aeadd2
Tomás <truizlop@gmail.com>
2019-12-04 17:09:45 +0100

    Page for retrying and repeating effects

---
+++ contents/Documentation/Effects.playground/Pages/Retrying and repeating effects.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+// nef:begin:header
+/*
+ layout: docs
+ title: Retrying and repeating effects
+ */
+// nef:end
+// nef:begin:hidden
+import Foundation
+import Bow
+import BowEffects
+// nef:end
+/*:
+ # Retrying and repeating effects
+ 
+ */
