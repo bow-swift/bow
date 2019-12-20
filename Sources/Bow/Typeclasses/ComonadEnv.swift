9ca49412d3912191cef50905ff2a4597353296b2
Tomás <truizlop@gmail.com>
2019-12-20 12:56:19 +0100

    Provide default implementation for methods in ComonadEnv

--- Sources/Bow/Typeclasses/ComonadEnv.swift
+++ Sources/Bow/Typeclasses/ComonadEnv.swift
@@ -2,7 +2,12 @@ public protocol ComonadEnv: Comonad {
     associatedtype E
     
     static func ask<A>(_ wa: Kind<Self, A>) -> E
-    static func asks<A, EE>(_ wa: Kind<Self, A>, _ f: @escaping (E) -> EE) -> EE
+}
+
+public extension ComonadEnv {
+    static func asks<A, EE>(_ wa: Kind<Self, A>, _ f: @escaping (E) -> EE) -> EE {
+        f(ask(wa))
+    }
 }
 
 // MARK: Syntax for ComonadEnv
