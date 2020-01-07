263d75156f39952889c069db362135c73de0ecf8
Tomás <truizlop@gmail.com>
2020-01-07 17:08:45 +0100

    Add generator for EnvT

---
+++ Tests/BowGenerators/Transformers/EnvT+Gen.swift
@@ -0,0 +1,18 @@
+import Bow
+import SwiftCheck
+
+// MARK: Generator for Property-based Testing
+
+extension EnvT: Arbitrary where E: Arbitrary, W: ArbitraryK, A: Arbitrary {
+    public static var arbitrary: Gen<EnvT<E, W, A>> {
+        Gen.from(EnvTPartial.generate >>> EnvT.fix)
+    }
+}
+
+// MARK: Instance of `ArbitraryK` for `Store`
+
+extension EnvTPartial: ArbitraryK where E: Arbitrary, W: ArbitraryK {
+    public static func generate<A: Arbitrary>() -> EnvTOf<E, W, A> {
+        EnvT(E.arbitrary.generate, W.generate())
+    }
+}
