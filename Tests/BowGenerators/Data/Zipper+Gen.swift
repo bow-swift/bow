5a3b4e4fd31004bd15fb6a0b61e88f97b58c6818
Tomás <truizlop@gmail.com>
2020-01-08 17:16:14 +0100

    Add Generator for Zipper

---
+++ Tests/BowGenerators/Data/Zipper+Gen.swift
@@ -0,0 +1,20 @@
+import Bow
+import SwiftCheck
+
+// MARK: Generator for Property-based Testing
+
+extension Zipper: Arbitrary where A: Arbitrary {
+    public static var arbitrary: Gen<Zipper<A>> {
+        Gen.zip([A].arbitrary, A.arbitrary, [A].arbitrary)
+            .map(Zipper.init(left:focus:right:))
+    }
+}
+
+// MARK: Instance of `ArbitraryK` for `ArrayK`
+
+extension ForZipper: ArbitraryK {
+    public static func generate<A: Arbitrary>() -> ZipperOf<A> {
+        Zipper<A>.arbitrary.generate
+    }
+}
+
