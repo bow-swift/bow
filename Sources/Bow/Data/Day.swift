042d22ccc2e5301beeebaea3b2c9b64b85153a63
Tomás <truizlop@gmail.com>
2020-01-08 14:57:30 +0100

    Add missing functions to Day

--- Sources/Bow/Data/Day.swift
+++ Sources/Bow/Data/Day.swift
@@ -28,6 +28,70 @@ public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
     internal func step<R>(_ ff: @escaping (Kind<F, Any>, Kind<G, Any>, @escaping (Any, Any) -> A) -> R) -> R {
         ff(left, right, f)
     }
+    
+    public func assoc<FF: Comonad, GG: Comonad>() -> Day<DayPartial<F, FF>, GG, A> where G == DayPartial<FF, GG> {
+        let newLeft = Day<F, FF, Any>(
+            left: self.left,
+            right: self.right^.left) { a, b in (a, b) }
+        
+        return Day<DayPartial<F, FF>, GG, A>(
+            left: newLeft,
+            right: self.right^.right) { x, c in
+                let xx = x as! (Any, Any)
+                return self.f(xx.0, self.right^.f(xx.1, c))
+        }
+    }
+    
+    public func disassoc<FF: Comonad, GG: Comonad>() -> Day<FF, DayPartial<GG, G>, A> where F == DayPartial<FF, GG> {
+        let newRight = Day<GG, G, Any>(
+            left: self.left^.right,
+            right: self.right) { a, b in (a, b) }
+                
+        return Day<FF, DayPartial<GG, G>, A>(
+            left: self.left^.left,
+            right: newRight) { a, x in
+                let xx = x as! (Any, Any)
+                return self.f(self.left^.f(a, xx.0), xx.1)
+        }
+    }
+    
+    public func swapped() -> Day<G, F, A> {
+        Day<G, F, A>(left: right, right: left) { c, b in self.f(b, c) }
+    }
+    
+    public func trans1<H: Comonad>(_ nat: FunctionK<F, H>) -> Day<H, G, A> {
+        Day<H, G, A>(left: nat.invoke(left), right: right, f)
+    }
+    
+    public func trans2<H: Comonad>(_ nat: FunctionK<G, H>) -> Day<F, H, A> {
+        Day<F, H, A>(left: left, right: nat.invoke(right), f)
+    }
+}
+
+public extension Day where F == G, F: Applicative {
+    func dap() -> Kind<F, A> {
+        F.map(left, right, f)
+    }
+}
+
+public extension Day where F == ForId {
+    static func intro1(_ right: Kind<G, A>) -> Day<ForId, G, A> {
+        Day(left: Id(()), right: right.map { a in a as Any }) { _, a in a as! A }
+    }
+    
+    func elim1() -> Kind<G, A> {
+        right.map { x in self.f(self.left^.value, x) }
+    }
+}
+
+public extension Day where G == ForId {
+    static func intro2(_ left: Kind<F, A>) -> Day<F, ForId, A> {
+        Day(left: left.map { a in a as Any }, right: Id(())) { a, _ in a as! A }
+    }
+    
+    func elim2() -> Kind<F, A> {
+        left.map { x in self.f(x, self.right^.value) }
+    }
 }
 
 /// Safe downcast.
