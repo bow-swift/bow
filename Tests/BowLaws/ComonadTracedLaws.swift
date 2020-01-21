c4d6c5f6e996ed73ca2e4274e5524b4bf8cd7eb4
Tomás <truizlop@gmail.com>
2020-01-21 11:36:43 +0100

    Add ComonadTraced laws

---
+++ Tests/BowLaws/ComonadTracedLaws.swift
@@ -0,0 +1,24 @@
+import SwiftCheck
+import Bow
+import BowGenerators
+
+public class ComonadTracedLaws<F: ComonadTraced & EquatableK & ArbitraryK, M: Equatable & Arbitrary & Monoid> where F.M == M {
+    
+    public static func check() {
+        traceEmpty()
+        sequentialTracing()
+    }
+    
+    static func traceEmpty() {
+        property("trace empty is equal to extract") <~ forAll { (fa: KindOf<F, Int>) in
+            fa.value.trace(.empty()) == fa.value.extract()
+        }
+    }
+    
+    static func sequentialTracing() {
+        property("tracing twice is equal to tracing combination of values") <~ forAll { (fa: KindOf<F, Int>, s: M, t: M) in
+            fa.value.coflatMap { wa in wa.trace(t) }.trace(s) ==
+                fa.value.trace(s.combine(t))
+        }
+    }
+}
