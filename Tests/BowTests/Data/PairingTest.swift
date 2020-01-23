181a0fdc5ca3aa996314ff9cc3bde12dce1c33f7
Tomás <truizlop@gmail.com>
2020-01-23 16:07:54 +0100

    Add test for Pairing of Writer and Traced

--- Tests/BowTests/Data/PairingTest.swift
+++ Tests/BowTests/Data/PairingTest.swift
@@ -17,4 +17,20 @@ class PairingTest: XCTestCase {
         
         XCTAssertEqual(w2.extract(), 31)
     }
+    
+    func testPairingWriterTraced() {
+        let w = Traced<Int, String> { x in Array(repeating: "*", count: x).joined() }
+        
+        let m = Writer<Int, Int>.var()
+        let a = Writer<Int, Void>.var()
+        
+        let actions: Writer<Int, Void> = binding(
+            (m, a) <- Writer.tell(3).censor { x in 2 * x }.listens(id),
+            |<-.tell(m.get + 1),
+            yield: ())^
+        
+        let w2 = Pairing.pairWriterTraced().select(actions, w.duplicate())
+        
+        XCTAssertEqual(w2.extract(), "*************")
+    }
 }
