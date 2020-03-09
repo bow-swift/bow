44ea5ae1d19d3403347dc9ff2f2b65318d4f6d0a
Tomás <truizlop@gmail.com>
2020-03-09 16:55:28 +0100

    Document Trampoline API

--- Sources/Bow/Data/Trampoline.swift
+++ Sources/Bow/Data/Trampoline.swift
@@ -1,16 +1,32 @@
+/// The Trampoline type helps us overcome stack safety issues of recursive calls by transforming them into loops.
 public class Trampoline<A> {
+    /// Creates a Trampoline that does not need to recurse and provides the final result.
+    ///
+    /// - Parameter value: Result of the computation.
+    /// - Returns: A Trampoline that provides a value and stops recursing.
     public static func done(_ value: A) -> Trampoline<A> {
         Done(value)
     }
     
+    /// Creates a Trampoline that performs a computation and needs to recurse.
+    ///
+    /// - Parameter f: Function describing the recursive step.
+    /// - Returns: A Trampoline that describes a recursive step.
     public static func `defer`(_ f: @escaping () -> Trampoline<A>) -> Trampoline<A> {
         Defer(f)
     }
     
+    /// Creates a Trampoline that performs a computation in a moment in the future.
+    ///
+    /// - Parameter f: Function to compute the value wrapped in this Trampoline.
+    /// - Returns: A Trampoline that delays the obtention of a value and stops recursing.
     public static func later(_ f: @escaping () -> A) -> Trampoline<A> {
         .defer { .done(f()) }
     }
     
+    /// Executes the computations described by this Trampoline by converting it into a loop.
+    ///
+    /// - Returns: Value resulting from the execution of the Trampoline.
     public final func run() -> A {
         var trampoline = self
         while true {
@@ -26,16 +42,23 @@ public class Trampoline<A> {
     internal func step() -> Either<() -> Trampoline<A>, A> {
         fatalError("Implement step in subclasses")
     }
-
+    
+    /// Composes this trampoline with another one that depends on the output of this one.
+    ///
+    /// - Parameter f: Function to compute a Trampoline based on the value of this one.
+    /// - Returns: A Trampoline describing the sequential application of both.
     public func flatMap<B>(_ f: @escaping (A) -> Trampoline<B>) -> Trampoline<B> {
         FlatMap(self, f)
     }
-
+    
+    /// Transforms the eventual value provided by this Trampoline.
+    ///
+    /// - Parameter f: Transforming function.
+    /// - Returns: A Trampoline that behaves as the original one but its result is transformed.
     public func map<B>(_ f: @escaping (A) -> B) -> Trampoline<B> {
         flatMap { a in .done(f(a)) }
     }
 }
-
 
 private final class Done<A>: Trampoline<A> {
     let result: A
@@ -49,7 +72,6 @@ private final class Done<A>: Trampoline<A> {
     }
 }
 
-
 private final class Defer<A>: Trampoline<A> {
     let deferred: () -> Trampoline<A>
 
@@ -61,7 +83,6 @@ private final class Defer<A>: Trampoline<A> {
         .left(deferred)
     }
 }
-
 
 private final class FlatMap<A, B>: Trampoline<B> {
     let trampoline: Trampoline<A>
