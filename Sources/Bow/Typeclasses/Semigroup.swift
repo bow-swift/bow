170abd33eae749ccda82e523be6187b6763f89ee
Tomás <truizlop@gmail.com>
2020-03-12 12:22:12 +0100

    Make combineAll function safe

--- Sources/Bow/Typeclasses/Semigroup.swift
+++ Sources/Bow/Typeclasses/Semigroup.swift
@@ -23,22 +23,26 @@ public extension Semigroup {
     ///   - b: Value of the implementing type.
     /// - Returns: Combination of `a` and `b`.
     static func combine(_ a: Self, _ b: Self) -> Self {
-        return a.combine(b)
+        a.combine(b)
     }
 
-    /// Combines a variable number of values of the implementing type, in the order provided in the call.
+    /// Combines a variable number of values (at least one) of the implementing type, in the order provided in the call.
     ///
-    /// - Parameter elems: Values of the implementing type.
+    /// - Parameters:
+    ///     - first: First value of the implementing type.
+    ///     - elems: Values of the implementing type.
     /// - Returns: A single value of the implementing type representing the combination of all the parameter values.
-    static func combineAll(_ elems: Self...) -> Self {
-        return combineAll(elems)
+    static func combineAll(_ first: Self, _ elems: Self...) -> Self {
+        combineAll(first, elems)
     }
 
     /// Combines an array of values of the implementing type, in the order provided in the call.
     ///
-    /// - Parameter elems: Array of values of the implementing type.
+    /// - Parameters:
+    ///     - first: First value of the implementing type.
+    ///     - elems: Values of the implementing type.
     /// - Returns: A single value of the implementing type representing the combination of all the parameter values.
-    static func combineAll(_ elems: [Self]) -> Self {
-        return elems[1 ..< elems.count].reduce(elems[0], { partial, next in partial.combine(next) })
+    static func combineAll(_ first: Self, _ elems: [Self]) -> Self {
+        elems.reduce(first, Self.combine)
     }
 }
