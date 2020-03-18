7beb69d0fc0c6fe9f3a498bc9f74af6e69f3f645
Tomás Ruiz-López <truizlop@gmail.com>
2020-03-18 13:09:16 +0100

    Document Day API (#553)

--- Sources/Bow/Data/Day.swift
+++ Sources/Bow/Data/Day.swift
@@ -1,14 +1,26 @@
 import Foundation
 
+/// Witness for the `Day<F, G, A>` data type. To be used in simulated Higher Kinded Types.
 public final class ForDay {}
+
+/// Partial application of the Day type constructor, omitting the last parameter.
 public final class DayPartial<F: Comonad, G: Comonad>: Kind2<ForDay, F, G> {}
+
+/// Higher Kinded Type alias to improve readability of `Kind<DayPartial<F, G>, A>`.
 public typealias DayOf<F: Comonad, G: Comonad, A> = Kind<DayPartial<F, G>, A>
 
-public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
+/// Day convolution of two Functors. It can be seen as the suspended version of a Pairing between Functors F and G, applied to specific values.
+public final class Day<F: Comonad, G: Comonad, A>: DayOf<F, G, A> {
     internal let left: Kind<F, Any /*B*/>
     internal let right: Kind<G, Any /*C*/>
     internal let f: (Any /*B*/, Any /*C*/) -> A
-
+    
+    /// Initializes the Day convolution.
+    ///
+    /// - Parameters:
+    ///   - left: An F-effectful value.
+    ///   - right: A G-effectful value.
+    ///   - f: A function to combine the values provided by both effectful computations.
     public init<B, C>(left: Kind<F, B>,
                       right: Kind<G, C>,
                       _ f: @escaping (B, C) -> A) {
@@ -17,10 +29,17 @@ public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
         self.f = { b, c in f(b as! B, c as! C) }
     }
     
+    /// Safe downcast.
+    ///
+    /// - Parameter value: Value in the higher-kind form.
+    /// - Returns: Value cast to Day.
     public static func fix(_ value: DayOf<F, G, A>) -> Day<F, G, A> {
         value as! Day<F, G, A>
     }
-
+    
+    /// Runs the Day convolution.
+    ///
+    /// - Returns: Result of the convolution.
     public func run() -> A {
         extract()
     }
@@ -29,6 +48,11 @@ public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
         ff(left, right, f)
     }
     
+    /// Associates the convolution of three Functors to the left.
+    ///
+    /// It goes from `Day f (Day g h) a` to `Day (Day f g) h a`.
+    ///
+    /// - Returns: Left associated Day convolution.
     public func assoc<FF: Comonad, GG: Comonad>() -> Day<DayPartial<F, FF>, GG, A> where G == DayPartial<FF, GG> {
         let newLeft = Day<F, FF, Any>(
             left: self.left,
@@ -42,6 +66,11 @@ public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
         }
     }
     
+    /// Associates the convolution of three Functors to the right.
+    ///
+    /// It goes from `Day (Day f g) h a` to `Day f (Day g h) a`.
+    ///
+    /// - Returns: Right associated Day convolution.
     public func disassoc<FF: Comonad, GG: Comonad>() -> Day<FF, DayPartial<GG, G>, A> where F == DayPartial<FF, GG> {
         let newRight = Day<GG, G, Any>(
             left: self.left^.right,
@@ -55,40 +84,68 @@ public final class Day<F: Comonad, G: Comonad, A> : DayOf<F, G, A> {
         }
     }
     
+    /// Swaps the convoluted Functors.
+    ///
+    /// - Returns: A Day convolution with Functors swapped.
     public func swapped() -> Day<G, F, A> {
         Day<G, F, A>(left: right, right: left) { c, b in self.f(b, c) }
     }
     
+    /// Applies a natural transformation on the first Functor.
+    ///
+    /// - Parameter nat: Natural transformation.
+    /// - Returns: Day convolution with the first Functor transformed.
     public func trans1<H: Comonad>(_ nat: FunctionK<F, H>) -> Day<H, G, A> {
         Day<H, G, A>(left: nat.invoke(left), right: right, f)
     }
     
+    /// Applies a natural transformation on the second Functor.
+    ///
+    /// - Parameter nat: Natural transformation.
+    /// - Returns: Day convolution with the second Functor transformed.
     public func trans2<H: Comonad>(_ nat: FunctionK<G, H>) -> Day<F, H, A> {
         Day<F, H, A>(left: left, right: nat.invoke(right), f)
     }
 }
 
 public extension Day where F == G, F: Applicative {
+    /// Collapses the convolution using Applicative.map.
+    ///
+    /// - Returns: Result of the convolution.
     func dap() -> Kind<F, A> {
         F.map(left, right, f)
     }
 }
 
 public extension Day where F == ForId {
+    /// Introduces Id at the left to make a Day convolution.
+    ///
+    /// - Parameter right: Value for the right side of the convolution.
+    /// - Returns: A convolution with the provided value on the right.
     static func intro1(_ right: Kind<G, A>) -> Day<ForId, G, A> {
         Day(left: Id(()), right: right.map { a in a as Any }) { _, a in a as! A }
     }
     
+    /// Eliminates Id from the left of this convolution.
+    ///
+    /// - Returns: Result of eliminating Id from the left of the convolution.
     func elim1() -> Kind<G, A> {
         right.map { x in self.f(self.left^.value, x) }
     }
 }
 
 public extension Day where G == ForId {
+    /// Introduces Id at the right to make a Day convolution.
+    ///
+    /// - Parameter right: Value for the left side of the convolution.
+    /// - Returns: A convolution with the provided value on the left.
     static func intro2(_ left: Kind<F, A>) -> Day<F, ForId, A> {
         Day(left: left.map { a in a as Any }, right: Id(())) { a, _ in a as! A }
     }
     
+    /// Eliminates Id from the right of this convolution.
+    ///
+    /// - Returns: Result of eliminating Id from the right of the convolution.
     func elim2() -> Kind<F, A> {
         left.map { x in self.f(x, self.right^.value) }
     }
@@ -102,6 +159,8 @@ public postfix func ^<F, G, A>(_ value: DayOf<F, G, A>) -> Day<F, G, A> {
     Day.fix(value)
 }
 
+// MARK: Instance of Functor for Day
+
 extension DayPartial: Functor {
     public static func map<A, B>(_ fa: DayOf<F, G, A>, _ f: @escaping (A) -> B) -> DayOf<F, G, B> {
         fa^.step { left, right, get in
@@ -109,6 +168,8 @@ extension DayPartial: Functor {
         }
     }
 }
+
+// MARK: Instance of Applicative for Day
 
 extension DayPartial: Applicative where F: Applicative, G: Applicative {
     public static func pure<A>(_ a: A) -> DayOf<F, G, A> {
@@ -129,6 +190,8 @@ extension DayPartial: Applicative where F: Applicative, G: Applicative {
         }
     }
 }
+
+// MARK: Instance of Comonad for Day
 
 extension DayPartial: Comonad {
     public static func coflatMap<A, B>(_ fa: DayOf<F, G, A>, _ f: @escaping (DayOf<F, G, A>) -> B) -> DayOf<F, G, B> {
