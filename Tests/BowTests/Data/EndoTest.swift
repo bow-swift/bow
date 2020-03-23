acaa1380dc75760737798a0ddcf3f19b0cbbb3ac
Tomás <truizlop@gmail.com>
2020-03-23 12:27:04 +0100

    Update Endo Tests to new convention

--- Tests/BowTests/Data/EndoTest.swift
+++ Tests/BowTests/Data/EndoTest.swift
@@ -3,14 +3,13 @@ import SwiftCheck
 import BowLaws
 import Bow
 
-extension ForEndo: EquatableK {
-    public static func eq<A>(_ a: EndoOf<A>, _ b: EndoOf<A>) -> Bool where A : Equatable {
-        return a^.run(1 as! A) == b^.run(1 as! A)
+extension EndoPartial: EquatableK {
+    public static func eq<A: Equatable>(_ a: EndoOf<A>, _ b: EndoOf<A>) -> Bool {
+        a^.run(1 as! A) == b^.run(1 as! A)
     }
 }
 
 class EndoTest: XCTestCase {
-    
     func testSemigroupLaws() {
         SemigroupLaws<Endo<Int>>.check()
     }
