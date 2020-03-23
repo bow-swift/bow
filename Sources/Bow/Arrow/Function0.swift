1f29558648fe5b187e70f65c7229e611c993bb50
Tomás <truizlop@gmail.com>
2020-03-23 12:21:21 +0100

    Update Function0 to new convention

--- Sources/Bow/Arrow/Function0.swift
+++ Sources/Bow/Arrow/Function0.swift
@@ -3,6 +3,9 @@ import Foundation
 /// Witness for the `Function0<A>` data type. To be used in simulated Higher Kinded Types.
 public final class ForFunction0 {}
 
+/// Partial application of the Function0 type constructor, omitting the last type parameter.
+public typealias Function0Partial = ForFunction0
+
 /// Higher Kinded Type alias to improve readability of `Kind<ForFunction0, A>`.
 public typealias Function0Of<A> = Kind<ForFunction0, A>
 
@@ -15,7 +18,7 @@ public final class Function0<A>: Function0Of<A> {
     /// - Parameter fa: Value in the higher-kind form.
     /// - Returns: Value cast to `Function0`.
     public static func fix(_ fa: Function0Of<A>) -> Function0<A> {
-        return fa as! Function0
+        fa as! Function0
     }
     
     /// Constructs a value of `Function0`.
@@ -29,7 +32,7 @@ public final class Function0<A>: Function0Of<A> {
     ///
     /// - Returns: Value produced by this function.
     public func invoke() -> A {
-        return f()
+        f()
     }
 }
 
@@ -38,39 +41,39 @@ public final class Function0<A>: Function0Of<A> {
 /// - Parameter fa: Value in the higher-kind form.
 /// - Returns: Value cast to `Function0`.
 public postfix func ^<A>(_ fa: Function0Of<A>) -> Function0<A> {
-    return Function0.fix(fa)
+    Function0.fix(fa)
 }
 
 // MARK: Protocol conformances
 
 // MARK: Instance of `EquatableK` for `Function0`
-extension ForFunction0: EquatableK {
-    public static func eq<A>(_ lhs: Kind<ForFunction0, A>, _ rhs: Kind<ForFunction0, A>) -> Bool where A : Equatable {
-        return Function0.fix(lhs).extract() == Function0.fix(rhs).extract()
+extension Function0Partial: EquatableK {
+    public static func eq<A: Equatable>(_ lhs: Function0Of<A>, _ rhs: Function0Of<A>) -> Bool {
+        Function0.fix(lhs).extract() == Function0.fix(rhs).extract()
     }
 }
 
 // MARK: Instance of `Functor` for `Function0`
-extension ForFunction0: Functor {
-    public static func map<A, B>(_ fa: Kind<ForFunction0, A>, _ f: @escaping (A) -> B) -> Kind<ForFunction0, B> {
-        return Function0(Function0.fix(fa).f >>> f)
+extension Function0Partial: Functor {
+    public static func map<A, B>(_ fa: Function0Of<A>, _ f: @escaping (A) -> B) -> Function0Of<B> {
+        Function0(Function0.fix(fa).f >>> f)
     }
 }
 
 // MARK: Instance of `Applicative` for `Function0`
-extension ForFunction0: Applicative {
-    public static func pure<A>(_ a: A) -> Kind<ForFunction0, A> {
-        return Function0(constant(a))
+extension Function0Partial: Applicative {
+    public static func pure<A>(_ a: A) -> Function0Of<A> {
+        Function0(constant(a))
     }
 }
 
 // MARK: Instance of `Selective` for `Function0`
-extension ForFunction0: Selective {}
+extension Function0Partial: Selective {}
 
 // MARK: Instance of `Monad` for `Function0`
-extension ForFunction0: Monad {
-    public static func flatMap<A, B>(_ fa: Kind<ForFunction0, A>, _ f: @escaping (A) -> Kind<ForFunction0, B>) -> Kind<ForFunction0, B> {
-        return f(Function0.fix(fa).f())
+extension Function0Partial: Monad {
+    public static func flatMap<A, B>(_ fa: Function0Of<A>, _ f: @escaping (A) -> Function0Of<B>) -> Function0Of<B> {
+        f(fa^.f())
     }
 
     private static func loop<A, B>(_ a: A, _ f: @escaping (A) -> Function0Of<Either<A, B>>) -> Trampoline<B> {
@@ -80,35 +83,35 @@ extension ForFunction0: Monad {
         }
     }
 
-    public static func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> Kind<ForFunction0, Either<A, B>>) -> Kind<ForFunction0, B> {
-        return Function0<B>({ loop(a, f).run() })
+    public static func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> Function0Of<Either<A, B>>) -> Function0Of<B> {
+        Function0<B>({ loop(a, f).run() })
     }
 }
 
 // MARK: Instance of `Comonad` for `Function0`
-extension ForFunction0: Comonad {
-    public static func coflatMap<A, B>(_ fa: Kind<ForFunction0, A>, _ f: @escaping (Kind<ForFunction0, A>) -> B) -> Kind<ForFunction0, B> {
-        return Function0<B>({ f(Function0.fix(fa)) })
+extension Function0Partial: Comonad {
+    public static func coflatMap<A, B>(_ fa: Function0Of<A>, _ f: @escaping (Function0Of<A>) -> B) -> Function0Of<B> {
+        Function0<B> { f(fa^) }
     }
 
-    public static func extract<A>(_ fa: Kind<ForFunction0, A>) -> A {
-        return Function0.fix(fa).f()
+    public static func extract<A>(_ fa: Function0Of<A>) -> A {
+        fa^.f()
     }
 }
 
 // MARK: Instance of `Bimonad` for `Function0`
-extension ForFunction0: Bimonad {}
+extension Function0Partial: Bimonad {}
 
 // MARK: Instance of `Semigroup` for `Function0`
 extension Function0: Semigroup where A: Semigroup {
     public func combine(_ other: Function0<A>) -> Function0<A> {
-        return Function0 { self.invoke().combine(other.invoke()) }
+        Function0 { self.invoke().combine(other.invoke()) }
     }
 }
 
 // MARK: Instance of `Monoid` for `Function0`
 extension Function0: Monoid where A: Monoid {
     public static func empty() -> Function0<A> {
-        return Function0(constant(A.empty()))
+        Function0(constant(A.empty()))
     }
 }
