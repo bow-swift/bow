b9a3071784efca9eb06f95831c66b78633966627
Tomás <truizlop@gmail.com>
2020-03-23 12:24:40 +0100

    Update ObservableK to new convention

--- Sources/BowRx/ObservableK.swift
+++ Sources/BowRx/ObservableK.swift
@@ -6,6 +6,9 @@ import BowEffects
 /// Witness for the `ObservableK<A>` data type. To be used in simulated Higher Kinded Types.
 public final class ForObservableK {}
 
+/// Partial application of the ObservableK type constructor, omitting the last type parameter.
+public typealias ObservableKPartial = ForObservableK
+
 /// Higher Kinded Type alias to improve readability over `Kind<ForObservableK, A>`.
 public typealias ObservableKOf<A> = Kind<ForObservableK, A>
 
@@ -14,7 +17,7 @@ public extension Observable {
     ///
     /// - Returns: An `ObservableK` wrapping this object.
     func k() -> ObservableK<Element> {
-        return ObservableK<Element>(self)
+        ObservableK<Element>(self)
     }
 }
 
@@ -49,14 +52,14 @@ public final class ObservableK<A>: ObservableKOf<A> {
     /// - Parameter value: Value in the higher-kind form.
     /// - Returns: Value cast to ObservableK.
     public static func fix(_ value: ObservableKOf<A>) -> ObservableK<A> {
-        return value as! ObservableK<A>
+        value as! ObservableK<A>
     }
     
     /// Provides an empty `ObservableK`.
     ///
     /// - Returns: An `ObservableK` that does not provide any value.
     public static func empty() -> ObservableK<A> {
-        return Observable.empty().k()
+        Observable.empty().k()
     }
 
     /// Creates an `ObservableK` from the result of evaluating a function, suspending its execution.
@@ -64,7 +67,7 @@ public final class ObservableK<A>: ObservableKOf<A> {
     /// - Parameter f: Function providing the value to be provided in the underlying `Observable`.
     /// - Returns: An `ObservableK` that provides the value obtained from the closure.
     public static func from(_ f: @escaping () throws -> A) -> ObservableK<A> {
-        return ForObservableK.defer {
+        ForObservableK.defer {
             do {
                 return pure(try f())
             } catch {
@@ -78,7 +81,7 @@ public final class ObservableK<A>: ObservableKOf<A> {
     /// - Parameter f: Function providing the value to be provided in the underlying `Observable`.
     /// - Returns: An `ObservableK` that provides the value obtained from the closure.
     public static func invoke(_ f: @escaping () throws -> ObservableKOf<A>) -> ObservableK<A> {
-        return ForObservableK.defer {
+        ForObservableK.defer {
             do {
                 return try f()
             } catch {
@@ -99,7 +102,7 @@ public final class ObservableK<A>: ObservableKOf<A> {
     /// - Parameter f: Function to be mapped.
     /// - Returns: An ObservableK resulting from the application of the function.
     public func concatMap<B>(_ f: @escaping (A) -> ObservableKOf<B>) -> ObservableK<B> {
-        return value.concatMap { a in f(a)^.value }.k()
+        value.concatMap { a in f(a)^.value }.k()
     }
 }
 
@@ -108,37 +111,45 @@ public final class ObservableK<A>: ObservableKOf<A> {
 /// - Parameter value: Value in higher-kind form.
 /// - Returns: Value cast to ObservableK.
 public postfix func ^<A>(_ value: ObservableKOf<A>) -> ObservableK<A> {
-    return ObservableK.fix(value)
+    ObservableK.fix(value)
 }
 
 // MARK: Instance of `Functor` for `ObservableK`
-extension ForObservableK: Functor {
-    public static func map<A, B>(_ fa: Kind<ForObservableK, A>, _ f: @escaping (A) -> B) -> Kind<ForObservableK, B> {
-        return ObservableK.fix(fa).value.map(f).k()
+extension ObservableKPartial: Functor {
+    public static func map<A, B>(
+        _ fa: ObservableKOf<A>,
+        _ f: @escaping (A) -> B) -> ObservableKOf<B> {
+        fa^.value.map(f).k()
     }
 }
 
 // MARK: Instance of `Applicative` for `ObservableK`
-extension ForObservableK: Applicative {
-    public static func pure<A>(_ a: A) -> Kind<ForObservableK, A> {
-        return Observable.just(a).k()
+extension ObservableKPartial: Applicative {
+    public static func pure<A>(_ a: A) -> ObservableKOf<A> {
+        Observable.just(a).k()
     }
 }
 
 // MARK: Instance of `Selective` for `ObservableK`
-extension ForObservableK: Selective {}
+extension ObservableKPartial: Selective {}
 
 // MARK: Instance of `Monad` for `ObservableK`
-extension ForObservableK: Monad {
-    public static func flatMap<A, B>(_ fa: Kind<ForObservableK, A>, _ f: @escaping (A) -> Kind<ForObservableK, B>) -> Kind<ForObservableK, B> {
-        return ObservableK.fix(fa).value.flatMap { a in ObservableK<B>.fix(f(a)).value }.k()
+extension ObservableKPartial: Monad {
+    public static func flatMap<A, B>(
+        _ fa: ObservableKOf<A>,
+        _ f: @escaping (A) -> ObservableKOf<B>) -> ObservableKOf<B> {
+        fa^.value.flatMap { a in f(a)^.value }.k()
     }
 
-    public static func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> Kind<ForObservableK, Either<A, B>>) -> Kind<ForObservableK, B> {
+    public static func tailRecM<A, B>(
+        _ a: A,
+        _ f: @escaping (A) -> ObservableKOf<Either<A, B>>) -> ObservableKOf<B> {
         _tailRecM(a, f).run()
     }
     
-    private static func _tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> ObservableKOf<Either<A, B>>) -> Trampoline<ObservableKOf<B>> {
+    private static func _tailRecM<A, B>(
+        _ a: A,
+        _ f: @escaping (A) -> ObservableKOf<Either<A, B>>) -> Trampoline<ObservableKOf<B>> {
         .defer {
             let either = f(a)^.value.blockingGet()!
             return either.fold({ a in _tailRecM(a, f)},
@@ -148,28 +159,36 @@ extension ForObservableK: Monad {
 }
 
 // MARK: Instance of `ApplicativeError` for `ObservableK`
-extension ForObservableK: ApplicativeError {
+extension ObservableKPartial: ApplicativeError {
     public typealias E = Error
 
-    public static func raiseError<A>(_ e: Error) -> Kind<ForObservableK, A> {
-        return Observable.error(e).k()
+    public static func raiseError<A>(_ e: Error) -> ObservableKOf<A> {
+        Observable.error(e).k()
     }
 
-    public static func handleErrorWith<A>(_ fa: Kind<ForObservableK, A>, _ f: @escaping (Error) -> Kind<ForObservableK, A>) -> Kind<ForObservableK, A> {
-        return ObservableK.fix(fa).value.catchError { e in ObservableK.fix(f(e)).value }.k()
+    public static func handleErrorWith<A>(
+        _ fa: ObservableKOf<A>,
+        _ f: @escaping (Error) -> ObservableKOf<A>) -> ObservableKOf<A> {
+        fa^.value.catchError { e in f(e)^.value }.k()
     }
 }
 
 // MARK: Instance of `MonadError` for `ObservableK`
-extension ForObservableK: MonadError {}
+extension ObservableKPartial: MonadError {}
 
 // MARK: Instance of `Foldable` for `ObservableK`
-extension ForObservableK: Foldable {
-    public static func foldLeft<A, B>(_ fa: Kind<ForObservableK, A>, _ b: B, _ f: @escaping (B, A) -> B) -> B {
-        return ObservableK.fix(fa).value.reduce(b, accumulator: f).blockingGet()!
+extension ObservableKPartial: Foldable {
+    public static func foldLeft<A, B>(
+        _ fa: ObservableKOf<A>,
+        _ b: B,
+        _ f: @escaping (B, A) -> B) -> B {
+        fa^.value.reduce(b, accumulator: f).blockingGet()!
     }
 
-    public static func foldRight<A, B>(_ fa: Kind<ForObservableK, A>, _ b: Eval<B>, _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
+    public static func foldRight<A, B>(
+        _ fa: ObservableKOf<A>,
+        _ b: Eval<B>,
+        _ f: @escaping (A, Eval<B>) -> Eval<B>) -> Eval<B> {
         func loop(_ fa: ObservableK<A>) -> Eval<B> {
             if let get = fa.value.blockingGet() {
                 return f(get, Eval.defer { loop(fa.value.skip(1).k()) } )
@@ -182,27 +201,32 @@ extension ForObservableK: Foldable {
 }
 
 // MARK: Instance of `Traverse` for `ObservableK`
-extension ForObservableK: Traverse {
-    public static func traverse<G: Applicative, A, B>(_ fa: Kind<ForObservableK, A>, _ f: @escaping (A) -> Kind<G, B>) -> Kind<G, Kind<ForObservableK, B>> {
-        return fa.foldRight(Eval.always { G.pure(Observable<B>.empty().k() as ObservableKOf<B>) }, { a, eval in
+extension ObservableKPartial: Traverse {
+    public static func traverse<G: Applicative, A, B>(
+        _ fa: ObservableKOf<A>,
+        _ f: @escaping (A) -> Kind<G, B>)
+        -> Kind<G, ObservableKOf<B>> {
+        fa.foldRight(Eval.always { G.pure(Observable<B>.empty().k() as ObservableKOf<B>) }, { a, eval in
             G.map2Eval(f(a), eval, { x, y in
-                Observable.concat(Observable.just(x), ObservableK.fix(y).value).k() as ObservableKOf<B>
+                Observable.concat(Observable.just(x), y^.value).k() as ObservableKOf<B>
             })
         }).value()
     }
 }
 
 // MARK: Instance of `MonadDefer` for `ObservableK`
-extension ForObservableK: MonadDefer {
-    public static func `defer`<A>(_ fa: @escaping () -> Kind<ForObservableK, A>) -> Kind<ForObservableK, A> {
-        return Observable.deferred { ObservableK<A>.fix(fa()).value }.k()
+extension ObservableKPartial: MonadDefer {
+    public static func `defer`<A>(_ fa: @escaping () -> ObservableKOf<A>) -> ObservableKOf<A> {
+        Observable.deferred { fa()^.value }.k()
     }
 }
 
 // MARK: Instance of `Async` for `ObservableK`
-extension ForObservableK: Async {
-    public static func asyncF<A>(_ procf: @escaping (@escaping (Either<Error, A>) -> ()) -> Kind<ForObservableK, ()>) -> Kind<ForObservableK, A> {
-        return Observable.create { emitter in
+extension ObservableKPartial: Async {
+    public static func asyncF<A>(
+        _ procf: @escaping (@escaping (Either<Error, A>) -> Void)
+        -> ObservableKOf<Void>) -> ObservableKOf<A> {
+        Observable.create { emitter in
             procf { either in
                 either.fold(
                     { error in emitter.on(.error(error)) },
@@ -211,12 +235,12 @@ extension ForObservableK: Async {
         }.k()
     }
 
-    public static func continueOn<A>(_ fa: Kind<ForObservableK, A>, _ queue: DispatchQueue) -> Kind<ForObservableK, A> {
-        return fa^.value.observeOn(SerialDispatchQueueScheduler(queue: queue, internalSerialQueueName: queue.label)).k()
+    public static func continueOn<A>(_ fa: ObservableKOf<A>, _ queue: DispatchQueue) -> ObservableKOf<A> {
+        fa^.value.observeOn(SerialDispatchQueueScheduler(queue: queue, internalSerialQueueName: queue.label)).k()
     }
 
-    public static func runAsync<A>(_ fa: @escaping ((Either<ForObservableK.E, A>) -> ()) throws -> ()) -> Kind<ForObservableK, A> {
-        return Observable.create { emitter in
+    public static func runAsync<A>(_ fa: @escaping ((Either<ForObservableK.E, A>) -> Void) throws -> Void) -> ObservableKOf<A> {
+        Observable.create { emitter in
             do {
                 try fa { either in
                     either.fold({ e in emitter.onError(e) },
@@ -229,34 +253,49 @@ extension ForObservableK: Async {
 }
 
 // MARK: Instance of `Effect` for `ObservableK`
-extension ForObservableK: Effect {
-    public static func runAsync<A>(_ fa: Kind<ForObservableK, A>, _ callback: @escaping (Either<Error, A>) -> Kind<ForObservableK, ()>) -> Kind<ForObservableK, ()> {
-        return ObservableK.fix(fa).value.flatMap { a in ObservableK<()>.fix(callback(Either.right(a))).value }
-            .catchError { e in ObservableK<()>.fix(callback(Either.left(e))).value }.k()
+extension ObservableKPartial: Effect {
+    public static func runAsync<A>(
+        _ fa: ObservableKOf<A>,
+        _ callback: @escaping (Either<Error, A>) -> ObservableKOf<Void>)
+        -> ObservableKOf<Void> {
+        fa^.value.flatMap { a in callback(Either.right(a))^.value }
+            .catchError { e in callback(Either.left(e))^.value }.k()
     }
 }
 
 // MARK: Instance of `Concurrent` for `ObservableK`
-extension ForObservableK: Concurrent {
-    public static func race<A, B>(_ fa: Kind<ForObservableK, A>, _ fb: Kind<ForObservableK, B>) -> Kind<ForObservableK, Either<A, B>> {
+extension ObservableKPartial: Concurrent {
+    public static func race<A, B>(
+        _ fa: ObservableKOf<A>,
+        _ fb: ObservableKOf<B>) -> ObservableKOf<Either<A, B>> {
         let left = fa.map(Either<A, B>.left)^.value
         let right = fb.map(Either<A, B>.right)^.value
         return left.amb(right).k()
     }
     
-    public static func parMap<A, B, Z>(_ fa: Kind<ForObservableK, A>, _ fb: Kind<ForObservableK, B>, _ f: @escaping (A, B) -> Z) -> Kind<ForObservableK, Z> {
-        return Observable.zip(fa^.value, fb^.value, resultSelector: f).k()
+    public static func parMap<A, B, Z>(
+        _ fa: ObservableKOf<A>,
+        _ fb: ObservableKOf<B>,
+        _ f: @escaping (A, B) -> Z) -> ObservableKOf<Z> {
+        Observable.zip(fa^.value, fb^.value, resultSelector: f).k()
     }
     
-    public static func parMap<A, B, C, Z>(_ fa: Kind<ForObservableK, A>, _ fb: Kind<ForObservableK, B>, _ fc: Kind<ForObservableK, C>, _ f: @escaping (A, B, C) -> Z) -> Kind<ForObservableK, Z> {
-        return Observable.zip(fa^.value, fb^.value, fc^.value, resultSelector: f).k()
+    public static func parMap<A, B, C, Z>(
+        _ fa: ObservableKOf<A>,
+        _ fb: ObservableKOf<B>,
+        _ fc: ObservableKOf<C>,
+        _ f: @escaping (A, B, C) -> Z) -> ObservableKOf<Z> {
+        Observable.zip(fa^.value, fb^.value, fc^.value, resultSelector: f).k()
     }
 }
 
 // MARK: Instance of `ConcurrentEffect` for `ObservableK`
-extension ForObservableK: ConcurrentEffect {
-    public static func runAsyncCancellable<A>(_ fa: Kind<ForObservableK, A>, _ callback: @escaping (Either<ForObservableK.E, A>) -> Kind<ForObservableK, ()>) -> Kind<ForObservableK, BowEffects.Disposable> {
-        return Observable.create { _ in
+extension ObservableKPartial: ConcurrentEffect {
+    public static func runAsyncCancellable<A>(
+        _ fa: ObservableKOf<A>,
+        _ callback: @escaping (Either<ForObservableK.E, A>)
+        -> ObservableKOf<Void>) -> ObservableKOf<BowEffects.Disposable> {
+        Observable.create { _ in
             let disposable = ObservableK.fix(ObservableK.fix(fa).runAsync(callback)).value.subscribe()
             return Disposables.create {
                 disposable.dispose()
@@ -266,12 +305,12 @@ extension ForObservableK: ConcurrentEffect {
 }
 
 // MARK: Instance of `Bracket` for `ObservableK`
-extension ForObservableK: Bracket {
+extension ObservableKPartial: Bracket {
     public static func bracketCase<A, B>(
-        acquire fa: Kind<ForObservableK, A>,
-        release: @escaping (A, ExitCase<Error>) -> Kind<ForObservableK, ()>,
-        use: @escaping (A) throws -> Kind<ForObservableK, B>) -> Kind<ForObservableK, B> {
-        return Observable.create { emitter in
+        acquire fa: ObservableKOf<A>,
+        release: @escaping (A, ExitCase<Error>) -> ObservableKOf<Void>,
+        use: @escaping (A) throws -> ObservableKOf<B>) -> ObservableKOf<B> {
+        Observable.create { emitter in
             fa.handleErrorWith { e in ObservableK.from { emitter.on(.error(e)) }.value.flatMap { _ in Observable.error(e) }.k() }^
                 .concatMap { a in
                     ObservableK.invoke { try use(a)^ }
