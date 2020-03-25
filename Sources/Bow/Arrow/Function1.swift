69b2f292bf88a79672183f102052e8e15ef28c68
Tomás <truizlop@gmail.com>
2020-03-25 13:52:56 +0100

    Clean Function1

--- Sources/Bow/Arrow/Function1.swift
+++ Sources/Bow/Arrow/Function1.swift
@@ -18,7 +18,7 @@ public final class Function1<I, O>: Function1Of<I, O> {
     /// - Parameter fa: Value in the higher-kind form.
     /// - Returns: Value cast to `Function1`.
     public static func fix(_ fa: Function1Of<I, O>) -> Function1<I, O> {
-        return fa as! Function1<I, O>
+        fa as! Function1<I, O>
     }
     
     /// Constructs a value of `Function1`.
@@ -33,7 +33,7 @@ public final class Function1<I, O>: Function1Of<I, O> {
     /// - Parameter value: Input to the function.
     /// - Returns: Result of invoking the function.
     public func invoke(_ value: I) -> O {
-        return f(value)
+        f(value)
     }
 
     /// Composes with another function.
@@ -41,7 +41,7 @@ public final class Function1<I, O>: Function1Of<I, O> {
     /// - Parameter f: Function to compose.
     /// - Returns: Composition of the two functions.
     public func compose<A>(_ f: Function1<A, I>) -> Function1<A, O> {
-        return Function1<A, O>(self.f <<< f.f)
+        Function1<A, O>(self.f <<< f.f)
     }
 
     /// Concatenates another function.
@@ -49,7 +49,7 @@ public final class Function1<I, O>: Function1Of<I, O> {
     /// - Parameter f: Function to concatenate.
     /// - Returns: Concatenation of the two functions.
     public func andThen<A>(_ f: Function1<O, A>) -> Function1<I, A> {
-        return f.compose(self)
+        f.compose(self)
     }
 
     /// Composes with another function.
@@ -57,7 +57,7 @@ public final class Function1<I, O>: Function1Of<I, O> {
     /// - Parameter f: Function to compose.
     /// - Returns: Composition of the two functions.
     public func contramap<A>(_ f: @escaping (A) -> I) -> Function1<A, O> {
-        return Function1<A, O>(self.f <<< f)
+        Function1<A, O>(self.f <<< f)
     }
 }
 
@@ -66,62 +66,71 @@ public final class Function1<I, O>: Function1Of<I, O> {
 /// - Parameter fa: Value in the higher-kind form.
 /// - Returns: Value cast to `Function1`.
 public postfix func ^<I, O>(_ fa: Function1Of<I, O>) -> Function1<I, O> {
-    return Function1.fix(fa)
+    Function1.fix(fa)
 }
 
-// MARK: Protocol conformances
-
-// MARK: Instance of `Functor` for `Function1`
+// MARK: Instance of Functor for Function1
 extension Function1Partial: Functor {
-    public static func map<A, B>(_ fa: Kind<Function1Partial<I>, A>, _ f: @escaping (A) -> B) -> Kind<Function1Partial<I>, B> {
-        return Function1(Function1.fix(fa).f >>> f)
+    public static func map<A, B>(
+        _ fa: Function1Of<I, A>,
+        _ f: @escaping (A) -> B) -> Function1Of<I, B> {
+        Function1(fa^.f >>> f)
     }
 }
 
-// MARK: Instance of `Applicative` for `Function1`
+// MARK: Instance of Applicative for Function1
 extension Function1Partial: Applicative {
-    public static func pure<A>(_ a: A) -> Kind<Function1Partial<I>, A> {
-        return Function1(constant(a))
+    public static func pure<A>(_ a: A) -> Function1Of<I, A> {
+        Function1(constant(a))
     }
 }
 
-// MARK: Instance of `Selective` for `Function1`
+// MARK: Instance of Selective for Function1
 extension Function1Partial: Selective {}
 
-// MARK: Instance of `Monad` for `Function1`
+// MARK: Instance of Monad for Function1
 extension Function1Partial: Monad {
-    public static func flatMap<A, B>(_ fa: Kind<Function1Partial<I>, A>, _ f: @escaping (A) -> Kind<Function1Partial<I>, B>) -> Kind<Function1Partial<I>, B> {
-        return Function1<I, B>({ i in Function1.fix(f(Function1.fix(fa).f(i))).f(i) })
+    public static func flatMap<A, B>(
+        _ fa: Function1Of<I, A>,
+        _ f: @escaping (A) -> Function1Of<I, B>) -> Function1Of<I, B> {
+        Function1<I, B> { i in f(fa^.f(i))^.f(i) }
     }
 
-    private static func step<A, B>(_ a: A, _ t: I, _ f: @escaping (A) -> Function1Of<I, Either<A, B>>) -> Trampoline<B> {
+    private static func step<A, B>(
+        _ a: A,
+        _ t: I,
+        _ f: @escaping (A) -> Function1Of<I, Either<A, B>>) -> Trampoline<B> {
         .defer {
             f(a)^.f(t).fold({ a in step(a, t, f) },
                            { b in .done(b) })
         }
     }
 
-    public static func tailRecM<A, B>(_ a: A, _ f: @escaping (A) -> Kind<Function1Partial<I>, Either<A, B>>) -> Kind<Function1Partial<I>, B> {
-        return Function1<I, B>({ t in step(a, t, f).run() })
+    public static func tailRecM<A, B>(
+        _ a: A,
+        _ f: @escaping (A) -> Function1Of<I, Either<A, B>>) -> Function1Of<I, B> {
+        Function1<I, B>({ t in step(a, t, f).run() })
     }
 }
 
-// MARK: Instance of `MonadReader` for `Function1`
+// MARK: Instance of MonadReader for Function1
 extension Function1Partial: MonadReader {
     public typealias D = I
 
-    public static func ask() -> Kind<Function1Partial<I>, I> {
-        return Function1(id)
+    public static func ask() -> Function1Of<I, I> {
+        Function1(id)
     }
 
-    public static func local<A>(_ fa: Kind<Function1Partial<I>, A>, _ f: @escaping (I) -> I) -> Kind<Function1Partial<I>, A> {
-        return Function1(f >>> Function1.fix(fa).f)
+    public static func local<A>(
+        _ fa: Function1Of<I, A>,
+        _ f: @escaping (I) -> I) -> Function1Of<I, A> {
+        Function1(f >>> fa^.f)
     }
 }
 
-// MARK: Instance of `Semigroup` for Function1
+// MARK: Instance of Semigroup for Function1
 extension Function1: Semigroup where O: Semigroup {
     public func combine(_ other: Function1<I, O>) -> Function1<I, O> {
-        return Function1 { i in self.invoke(i).combine(other.invoke(i)) }
+        Function1 { i in self.invoke(i).combine(other.invoke(i)) }
     }
 }
