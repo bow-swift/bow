6275102deb6246fbdc7131e155f2c1327b5ef551
Tomás <truizlop@gmail.com>
2020-03-25 17:05:13 +0100

    Clean Boolean Functions

--- Sources/Bow/Syntax/BooleanFunctions.swift
+++ Sources/Bow/Syntax/BooleanFunctions.swift
@@ -4,8 +4,8 @@ import Foundation
 ///
 /// - Parameter a: Value to be negated.
 /// - Returns: `true` if the input was `false`; `false`, otherwise.
-public func not(_ a : Bool) -> Bool {
-    return !a
+public func not(_ a: Bool) -> Bool {
+    !a
 }
 
 /// Conjunction of two boolean values.
@@ -14,8 +14,8 @@ public func not(_ a : Bool) -> Bool {
 ///   - a: Left-hand side value.
 ///   - b: Right-hand side value.
 /// - Returns: `true` if both inputs are `true`, or `false` otherwise.
-public func and(_ a : Bool, _ b : Bool) -> Bool {
-    return a && b
+public func and(_ a: Bool, _ b: Bool) -> Bool {
+    a && b
 }
 
 /// Disjunction of two boolean values.
@@ -24,8 +24,8 @@ public func and(_ a : Bool, _ b : Bool) -> Bool {
 ///   - a: Left-hand side value.
 ///   - b: Right-hand side value.
 /// - Returns: `false` if both inputs are `false`, or `true` otherwise.
-public func or(_ a : Bool, _ b : Bool) -> Bool {
-    return a || b
+public func or(_ a: Bool, _ b: Bool) -> Bool {
+    a || b
 }
 
 /// Exclusive or of two boolean values.
@@ -34,16 +34,16 @@ public func or(_ a : Bool, _ b : Bool) -> Bool {
 ///   - a: Left-hand side value.
 ///   - b: Right-hand side value.
 /// - Returns: `true` if both inputs have different truth value, or `false` if they are equal.
-public func xor(_ a : Bool, _ b : Bool) -> Bool {
-    return a != b
+public func xor(_ a: Bool, _ b: Bool) -> Bool {
+    a != b
 }
 
 /// Given a 0-ary function, provides a 0-ary function that returns the complement boolean value.
 ///
 /// - Parameter ff: Function to be complemented.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement(_ ff : @escaping () -> Bool) -> () -> Bool {
-    return ff >>> not
+public func complement(_ ff: @escaping () -> Bool) -> () -> Bool {
+    ff >>> not
 }
 
 /// Given a 1-ary function, provides a 1-ary function that returns the complement boolean value.
@@ -52,8 +52,8 @@ public func complement(_ ff : @escaping () -> Bool) -> () -> Bool {
 ///   - ff: Function to be complemented.
 ///   - a: 1st argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A>(_ ff : @escaping (_ a: A) -> Bool) -> (A) -> Bool {
-    return ff >>> not
+public func complement<A>(_ ff: @escaping (_ a: A) -> Bool) -> (A) -> Bool {
+    ff >>> not
 }
 
 /// Given a 2-ary function, provides a 2-ary function that returns the complement boolean value.
@@ -63,8 +63,8 @@ public func complement<A>(_ ff : @escaping (_ a: A) -> Bool) -> (A) -> Bool {
 ///   - a: 1st argument of `ff`.
 ///   - b: 2nd argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B>(_ ff : @escaping (_ a: A, _ b: B) -> Bool) -> (A, B) -> Bool {
-    return { a, b in !ff(a, b) }
+public func complement<A, B>(_ ff: @escaping (_ a: A, _ b: B) -> Bool) -> (A, B) -> Bool {
+    { a, b in !ff(a, b) }
 }
 
 /// Given a 3-ary function, provides a 3-ary function that returns the complement boolean value.
@@ -75,8 +75,8 @@ public func complement<A, B>(_ ff : @escaping (_ a: A, _ b: B) -> Bool) -> (A, B
 ///   - b: 2nd argument of `ff`.
 ///   - c: 3rd argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C>(_ ff : @escaping (_ a: A, _ b: B, _ c: C) -> Bool) -> (A, B, C) -> Bool {
-    return { a, b, c in !ff(a, b, c) }
+public func complement<A, B, C>(_ ff: @escaping (_ a: A, _ b: B, _ c: C) -> Bool) -> (A, B, C) -> Bool {
+    { a, b, c in !ff(a, b, c) }
 }
 
 /// Given a 4-ary function, provides a 4-ary function that returns the complement boolean value.
@@ -88,8 +88,8 @@ public func complement<A, B, C>(_ ff : @escaping (_ a: A, _ b: B, _ c: C) -> Boo
 ///   - c: 3rd argument of `ff`.
 ///   - d: 4th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D) -> Bool) -> (A, B, C, D) -> Bool {
-    return { a, b, c, d in !ff(a, b, c, d) }
+public func complement<A, B, C, D>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D) -> Bool) -> (A, B, C, D) -> Bool {
+    { a, b, c, d in !ff(a, b, c, d) }
 }
 
 /// Given a 5-ary function, provides a 5-ary function that returns the complement boolean value.
@@ -102,8 +102,8 @@ public func complement<A, B, C, D>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d
 ///   - d: 4th argument of `ff`.
 ///   - e: 5th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E) -> Bool) -> (A, B, C, D, E) -> Bool {
-    return { a, b, c, d, e in !ff(a, b, c, d, e) }
+public func complement<A, B, C, D, E>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E) -> Bool) -> (A, B, C, D, E) -> Bool {
+    { a, b, c, d, e in !ff(a, b, c, d, e) }
 }
 
 /// Given a 6-ary function, provides a 6-ary function that returns the complement boolean value.
@@ -117,8 +117,8 @@ public func complement<A, B, C, D, E>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, 
 ///   - e: 5th argument of `ff`.
 ///   - f: 6th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E, F>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F) -> Bool) -> (A, B, C, D, E, F) -> Bool {
-    return { a, b, c, d, e, f in !ff(a, b, c, d, e, f) }
+public func complement<A, B, C, D, E, F>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F) -> Bool) -> (A, B, C, D, E, F) -> Bool {
+    { a, b, c, d, e, f in !ff(a, b, c, d, e, f) }
 }
 
 /// Given a 7-ary function, provides a 7-ary function that returns the complement boolean value.
@@ -133,8 +133,8 @@ public func complement<A, B, C, D, E, F>(_ ff : @escaping (_ a: A, _ b: B, _ c: 
 ///   - f: 6th argument of `ff`.
 ///   - g: 7th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E, F, G>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G) -> Bool) -> (A, B, C, D, E, F, G) -> Bool {
-    return { a, b, c, d, e, f, g in !ff(a, b, c, d, e, f, g) }
+public func complement<A, B, C, D, E, F, G>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G) -> Bool) -> (A, B, C, D, E, F, G) -> Bool {
+    { a, b, c, d, e, f, g in !ff(a, b, c, d, e, f, g) }
 }
 
 /// Given a 8-ary function, provides a 8-ary function that returns the complement boolean value.
@@ -150,8 +150,8 @@ public func complement<A, B, C, D, E, F, G>(_ ff : @escaping (_ a: A, _ b: B, _ 
 ///   - g: 7th argument of `ff`.
 ///   - h: 8th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E, F, G, H>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H) -> Bool) -> (A, B, C, D, E, F, G, H) -> Bool {
-    return { a, b, c, d, e, f, g, h in !ff(a, b, c, d, e, f, g, h) }
+public func complement<A, B, C, D, E, F, G, H>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H) -> Bool) -> (A, B, C, D, E, F, G, H) -> Bool {
+    { a, b, c, d, e, f, g, h in !ff(a, b, c, d, e, f, g, h) }
 }
 
 /// Given a 9-ary function, provides a 9-ary function that returns the complement boolean value.
@@ -168,8 +168,8 @@ public func complement<A, B, C, D, E, F, G, H>(_ ff : @escaping (_ a: A, _ b: B,
 ///   - h: 8th argument of `ff`.
 ///   - i: 9th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E, F, G, H, I>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I) -> Bool) -> (A, B, C, D, E, F, G, H, I) -> Bool {
-    return { a, b, c, d, e, f, g, h, i in !ff(a, b, c, d, e, f, g, h, i) }
+public func complement<A, B, C, D, E, F, G, H, I>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I) -> Bool) -> (A, B, C, D, E, F, G, H, I) -> Bool {
+    { a, b, c, d, e, f, g, h, i in !ff(a, b, c, d, e, f, g, h, i) }
 }
 
 /// Given a 10-ary function, provides a 10-ary function that returns the complement boolean value.
@@ -187,6 +187,6 @@ public func complement<A, B, C, D, E, F, G, H, I>(_ ff : @escaping (_ a: A, _ b:
 ///   - i: 9th argument of `ff`.
 ///   - j: 10th argument of `ff`.
 /// - Returns: Function that returns the negated result of `ff`.
-public func complement<A, B, C, D, E, F, G, H, I, J>(_ ff : @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I, _ j: J) -> Bool) -> (A, B, C, D, E, F, G, H, I, J) -> Bool {
-    return { a, b, c, d, e, f, g, h, i, j in !ff(a, b, c, d, e, f, g, h, i, j) }
+public func complement<A, B, C, D, E, F, G, H, I, J>(_ ff: @escaping (_ a: A, _ b: B, _ c: C, _ d: D, _ e: E, _ f: F, _ g: G, _ h: H, _ i: I, _ j: J) -> Bool) -> (A, B, C, D, E, F, G, H, I, J) -> Bool {
+    { a, b, c, d, e, f, g, h, i, j in !ff(a, b, c, d, e, f, g, h, i, j) }
 }
