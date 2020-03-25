1d80175f6c29706c46ab9f8ff8898c9be167cfc4
Tomás <truizlop@gmail.com>
2020-03-25 16:11:44 +0100

    Clean Result

--- Sources/Bow/Data/Result.swift
+++ Sources/Bow/Data/Result.swift
@@ -4,35 +4,35 @@ public extension Result {
     ///
     /// - Returns: An Either.left if this is a Result.failure, or an Either.right if this is a Result.success
     func toEither() -> Either<Failure, Success> {
-        return fold(Either.left, Either.right)
+        fold(Either.left, Either.right)
     }
 
     /// Converts this Result into a Try value.
     ///
     /// - Returns: A Try.failure if this is a Result.failure, or a Try.success if this is a Result.success
     func toTry() -> Try<Success> {
-        return fold(Try.failure, Try.success)
+        fold(Try.failure, Try.success)
     }
     
     /// Converts this Result into a Validated value.
     ///
     /// - Returns: A Validated.invalid if this is a Result.failure, or a Validated.valid if this is a Result.success.
     func toValidated() -> Validated<Failure, Success> {
-        return fold(Validated.invalid, Validated.valid)
+        fold(Validated.invalid, Validated.valid)
     }
     
     /// Converts this Result into a ValidatedNEA value.
     ///
     /// - Returns: A Validated.invalid with a NonEmptyArray of the Failure type if this is a Result.failure, or a Validated.valid if this is a Result.success.
     func toValidatedNEA() -> ValidatedNEA<Failure, Success> {
-        return toValidated().toValidatedNEA()
+        toValidated().toValidatedNEA()
     }
 
     /// Converts this Result into an Option value.
     ///
     /// - Returns: Option.none if this is a Result.failure, or Option.some if this is a Result.success.
     func toOption() -> Option<Success> {
-        return fold(constant(Option.none()), Option.some)
+        fold(constant(Option.none()), Option.some)
     }
 
     /// Applies the corresponding closure based on the value contained in this result.
@@ -57,7 +57,7 @@ public extension Either where A: Error {
     ///
     /// - Returns: A Result.success if this is an Either.right, or a Result.failure if this is an Either.left.
     func toResult() -> Result<B, A> {
-        return self.fold(Result.failure, Result.success)
+        self.fold(Result.failure, Result.success)
     }
 }
 
@@ -68,6 +68,17 @@ public extension Validated where E: Error {
     ///
     /// - Returns: A Result.success if this is a Validated.valid, or a Result.failure if this is a Validated.invalid.
     func toResult() -> Result<A, E> {
-        return self.fold(Result.failure, Result.success)
+        self.fold(Result.failure, Result.success)
+    }
+}
+
+// MARK: Conversion to Result
+
+public extension Try {
+    /// Converts this Try into a Result value.
+    ///
+    /// - Returns: A Result.success if this is a Try.success, or a Result.failure if this is a Try.failure.
+    func toResult() -> Result<A, Error> {
+        self.fold(Result.failure, Result.success)
     }
 }
