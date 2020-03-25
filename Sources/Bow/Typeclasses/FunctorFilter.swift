efcb5d6f1d33d36a69e0677f0488ac96a3a76f1a
Tomás <truizlop@gmail.com>
2020-03-25 18:08:11 +0100

    Clean FunctorFilter

--- Sources/Bow/Typeclasses/FunctorFilter.swift
+++ Sources/Bow/Typeclasses/FunctorFilter.swift
@@ -8,7 +8,9 @@ public protocol FunctorFilter: Functor {
     ///   - fa: A value in the context implementing this instance.
     ///   - f: A function to map objects and filter them out.
     /// - Returns: Transformed and filtered values, in the context implementing this instance.
-    static func mapFilter<A, B>(_ fa: Kind<Self, A>, _ f: @escaping (A) -> OptionOf<B>) -> Kind<Self, B>
+    static func mapFilter<A, B>(
+        _ fa: Kind<Self, A>,
+        _ f: @escaping (A) -> OptionOf<B>) -> Kind<Self, B>
 }
 
 // MARK: Related functions
@@ -19,7 +21,7 @@ public extension FunctorFilter {
     /// - Parameter fa: Optional values in the context implementing this instance.
     /// - Returns: Plain values in the context implementing this instance.
     static func flattenOption<A>(_ fa: Kind<Self, OptionOf<A>>) -> Kind<Self, A> {
-        return mapFilter(fa, id)
+        mapFilter(fa, id)
     }
 
     /// Filters out the value/s in the context implementing this instance that do not match the given predicate.
@@ -28,8 +30,10 @@ public extension FunctorFilter {
     ///   - fa: Value in the context implementing this instance.
     ///   - f: Filtering predicate.
     /// - Returns: Filtered value in the context implementing this instance.
-    static func filter<A>(_ fa: Kind<Self, A>, _ f: @escaping (A) -> Bool) -> Kind<Self, A> {
-        return mapFilter(fa, { a in f(a) ? Option.some(a) : Option.none() })
+    static func filter<A>(
+        _ fa: Kind<Self, A>,
+        _ f: @escaping (A) -> Bool) -> Kind<Self, A> {
+        mapFilter(fa, { a in f(a) ? Option.some(a) : Option.none() })
     }
 }
 
@@ -44,7 +48,7 @@ public extension Kind where F: FunctorFilter {
     ///   - f: A function to map objects and filter them out.
     /// - Returns: Transformed and filtered values, in this context.
     func mapFilter<B>(_ f: @escaping (A) -> OptionOf<B>) -> Kind<F, B> {
-        return F.mapFilter(self, f)
+        F.mapFilter(self, f)
     }
 
     /// Removes the `Option.none` value/s in this context and extracts the `Option.some` ones.
@@ -54,7 +58,7 @@ public extension Kind where F: FunctorFilter {
     /// - Parameter fa: Optional values in this context.
     /// - Returns: Plain values in this context.
     static func flattenOption(_ fa: Kind<F, OptionOf<A>>) -> Kind<F, A> {
-        return F.flattenOption(fa)
+        F.flattenOption(fa)
     }
 
     /// Filters out the value/s in this context that do not match the given predicate.
@@ -65,6 +69,6 @@ public extension Kind where F: FunctorFilter {
     ///   - f: Filtering predicate.
     /// - Returns: Filtered value in this context.
     func filter(_ f: @escaping (A) -> Bool) -> Kind<F, A> {
-        return F.filter(self, f)
+        F.filter(self, f)
     }
 }
