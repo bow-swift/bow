9e9a746154639fc9c7ca09339d7299607ad11f18
Tomás <truizlop@gmail.com>
2020-03-26 13:57:20 +0100

    Clean MonadWriter laws

--- Tests/BowLaws/MonadWriterLaws.swift
+++ Tests/BowLaws/MonadWriterLaws.swift
@@ -12,34 +12,47 @@ public class MonadWriterLaws<F: MonadWriter & EquatableK> where F.W == Int {
     
     private static func writerPure() {
         property("Writer pure") <~ forAll { (a: Int) in
-            return F.writer((Int.empty(), a)) == F.pure(a)
+            
+            F.writer((.empty(), a))
+                ==
+            F.pure(a)
         }
     }
     
     private static func tellFusion() {
         property("Tell fusion") <~ forAll { (a: Int, b: Int) in
-            return isEqual(F.flatMap(F.tell(a), { _ in F.tell(b) }),
-                           F.tell(a.combine(b)))
+            
+            isEqual(
+                F.tell(a).flatMap { _ in F.tell(b) },
+                F.tell(a.combine(b)))
         }
     }
     
     private static func listenPure() {
         property("Listen pure") <~ forAll { (a: Int) in
-            return isEqual(F.listen(F.pure(a)), F.pure((Int.empty(), a)))
+            
+            isEqual(
+                F.pure(a).listen(),
+                F.pure((.empty(), a)))
         }
     }
     
     private static func listenWriterProperty() {
         property("Listen writer") <~ forAll { (a: Int, w: Int) in
             let tuple = (w, a)
-            return isEqual(F.listen(F.writer(tuple)), F.map(F.tell(tuple.0), { _ in tuple }))
+            
+            return isEqual(
+                F.writer(tuple).listen(),
+                F.tell(tuple.0).map { _ in tuple })
         }
     }
     
     private static func censorTell() {
         property("Censor tell") <~ forAll { (a: Int, w: Int, f: ArrowOf<Int, Int>) in
-            isEqual(F.writer((f.getArrow(w), a)).listen(),
-                    F.writer((w, a)).censor(f.getArrow).listen())
+            
+            isEqual(
+                F.writer((f.getArrow(w), a)).listen(),
+                F.writer((w, a)).censor(f.getArrow).listen())
         }
     }
     
