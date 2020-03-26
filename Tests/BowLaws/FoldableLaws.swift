8fbe5e9b54fa5dd7cf1bce25387daf58fa9f5de6
Tomás <truizlop@gmail.com>
2020-03-26 13:20:07 +0100

    Clean Foldable laws

--- Tests/BowLaws/FoldableLaws.swift
+++ Tests/BowLaws/FoldableLaws.swift
@@ -16,20 +16,31 @@ public class FoldableLaws<F: Foldable & EquatableK & ArbitraryK> {
     
     private static func leftFoldConsistentWithFoldMap() {
         property("Left fold consistent with foldMap") <~ forAll { (input: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
-            return F.foldMap(input.value, f.getArrow) == F.foldLeft(input.value, Int.empty(), { b, a in b.combine(f.getArrow(a)) })
+            
+            input.value.foldMap(f.getArrow)
+                ==
+            input.value.foldLeft(.empty()) { b, a in b.combine(f.getArrow(a))
+            }
         }
     }
     
     private static func rightFoldConsistentWithFoldMap() {
         property("Right fold consistent with folMap") <~ forAll { (f: ArrowOf<Int, Int>, fa: KindOf<F, Int>) in
-            return fa.value.foldMap(f.getArrow) ==
-                fa.value.foldRight(Eval.later { Int.empty() }, { a, lb in lb.map { b in f.getArrow(a).combine(b) }^ }).value()
+            
+            fa.value.foldMap(f.getArrow)
+                ==
+            fa.value.foldRight(Eval.later { .empty() }) { a, lb in lb.map { b in f.getArrow(a).combine(b) }^
+            }.value()
         }
     }
     
     private static func existsConsistentWithFind() {
         property("Exists consistent with find") <~ forAll { (input: KindOf<F, Int>, predicate: ArrowOf<Int, Bool>) in
-            return F.exists(input.value, predicate.getArrow) == F.find(input.value, predicate.getArrow).fold(constant(false), constant(true))
+            
+            input.value.exists(predicate.getArrow)
+                ==
+            input.value.find(predicate.getArrow)
+                .fold(constant(false), constant(true))
         }
     }
     
@@ -53,9 +64,9 @@ public class FoldableLaws<F: Foldable & EquatableK & ArbitraryK> {
     
     private static func forallConsistentWithExists() {
         property("Forall consistent with exists") <~ forAll { (input: KindOf<F, Int>, predicate: ArrowOf<Int, Bool>) in
-            if F.forall(input.value, predicate.getArrow) {
-                let negationExists = F.exists(input.value, predicate.getArrow >>> not)
-                return !negationExists && (F.isEmpty(input.value) || F.exists(input.value, predicate.getArrow))
+            if input.value.forall(predicate.getArrow) {
+                let negationExists = input.value.exists(predicate.getArrow >>> not)
+                return !negationExists && (F.isEmpty(input.value) || input.value.exists(predicate.getArrow))
             } else {
                 return true
             }
@@ -64,15 +75,18 @@ public class FoldableLaws<F: Foldable & EquatableK & ArbitraryK> {
     
     private static func forallReturnsTrueIfEmpty() {
         property("Forall returns true if empty") <~ forAll { (input: KindOf<F, Int>, predicate: ArrowOf<Int, Bool>) in
-            return !F.isEmpty(input.value) || F.forall(input.value, predicate.getArrow)
+            input.value.nonEmpty || input.value.forall(predicate.getArrow)
         }
     }
     
     private static func foldMIdIsFoldL()  {
         property("Exists consistent with find") <~ forAll { (input: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
-            let foldL = F.foldLeft(input.value, Int.empty(), { b, a in b.combine(f.getArrow(a)) })
-            let foldM = Id.fix(F.foldM(input.value, Int.empty(), { b, a in Id(b.combine(f.getArrow(a))) })).value
-            return foldL == foldM
+            
+            input.value.foldLeft(.empty()) { b, a in b.combine(f.getArrow(a))
+            }
+                ==
+            input.value.foldM(.empty()) { b, a in Id(b.combine(f.getArrow(a)))
+            }^.value
         }
     }
 }
