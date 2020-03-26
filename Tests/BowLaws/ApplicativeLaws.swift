608d30a818f9f978f8bb5d85934bf35913e99319
Tomás <truizlop@gmail.com>
2020-03-26 12:42:14 +0100

    Clean Applicative laws

--- Tests/BowLaws/ApplicativeLaws.swift
+++ Tests/BowLaws/ApplicativeLaws.swift
@@ -10,96 +10,160 @@ public class ApplicativeLaws<F: Applicative & EquatableK & ArbitraryK> {
         interchange()
         mapDerived()
         cartesianBuilderMap()
-        cartesianBuilderTupled()
-        mapWithMultipleInputs()
     }
     
     private static func apIdentity() {
         property("ap identity") <~ forAll() { (fa: KindOf<F, Int>) in
-            return F.ap(F.pure(id), fa.value) == fa.value
+            
+            F.pure(id).ap(fa.value)
+                ==
+            fa.value
         }
     }
     
     private static func homomorphism() {
         property("homomorphism") <~ forAll() { (a: Int, f: ArrowOf<Int, Int>) in
-            return F.ap(F.pure(f.getArrow), F.pure(a)) == F.pure(f.getArrow(a))
+            
+            F.pure(f.getArrow).ap(F.pure(a))
+                ==
+            F.pure(f.getArrow(a))
         }
     }
     
     private static func interchange() {
         property("interchange") <~ forAll() { (a: Int, b: Int) in
             let fa = F.pure(constant(a) as (Int) -> Int)
-            return F.ap(fa, F.pure(b)) == F.ap(F.pure({ (x: (Int) -> Int) in x(a) } ), fa)
+            
+            return fa.ap(F.pure(b))
+                ==
+            F.pure({ (x: (Int) -> Int) in x(a) } ).ap(fa)
         }
     }
     
     private static func mapDerived() {
-        property("mad derived") <~ forAll() { (fa: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
-            return F.map(fa.value, f.getArrow) == F.ap(F.pure(f.getArrow), fa.value)
+        property("map derived") <~ forAll() { (fa: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
+            
+            fa.value.map(f.getArrow)
+                ==
+            F.pure(f.getArrow).ap(fa.value)
         }
     }
     
     private static func cartesianBuilderMap() {
-        property("cartesian builder map") <~ forAll() { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f), { x, y, z, u, v, w in x + y + z - u - v - w })
-            let right: Kind<F, Int> = F.pure(a + b + c - d - e - f)
-            return left == right
-        }
-    }
-    
-    private static func cartesianBuilderTupled() {
-        property("cartesian builder map") <~ forAll() { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) in
-            let left: Kind<F, Int> = F.map(F.zip(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f)), { x, y, z, u, v, w in x + y + z - u - v - w })
-            let right: Kind<F, Int> = F.pure(a + b + c - d - e - f)
-            return left == right
-        }
-    }
-    
-    private static func mapWithMultipleInputs() {
         property("Map with 2 inputs") <~ forAll { (a: Int, b: Int) in
-            return F.map(F.pure(a), F.pure(b), { a1, b1 in a1 + b1 }) == F.pure(a + b)
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b)) { a1, b1 in
+                    a1 + b1
+            }
+                ==
+            Kind<F, Int>.pure(a + b)
         }
         
         property("Map with 3 inputs") <~ forAll { (a: Int, b: Int, c: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), { a1, b1, c1 in a1 + b1 + c1 })
-            let right: Kind<F, Int> = F.pure(a + b + c)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c)) { a1, b1, c1 in
+                    a1 + b1 + c1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c)
         }
         
         property("Map with 4 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), { a1, b1, c1, d1 in a1 + b1 + c1 + d1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d)) { a1, b1, c1, d1 in
+                    a1 + b1 + c1 + d1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d)
         }
         
         property("Map with 5 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), { a1, b1, c1, d1, e1 in a1 + b1 + c1 + d1 + e1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d + e)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e)) { a1, b1, c1, d1, e1 in
+                    a1 + b1 + c1 + d1 + e1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d + e)
         }
 
         property("Map with 6 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f), { a1, b1, c1, d1, e1, f1 in a1 + b1 + c1 + d1 + e1 + f1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d + e + f)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f)) { a1, b1, c1, d1, e1, f1 in
+                    a1 + b1 + c1 + d1 + e1 + f1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d + e + f)
         }
         
         property("Map with 7 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f), F.pure(g), { a1, b1, c1, d1, e1, f1, g1 in a1 + b1 + c1 + d1 + e1 + f1 + g1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d + e + f + g)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g)) { a1, b1, c1, d1, e1, f1, g1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d + e + f + g)
         }
         
         property("Map with 8 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f), F.pure(g), F.pure(h), { a1, b1, c1, d1, e1, f1, g1, h1 in a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d + e + f + g + h)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g),
+                .pure(h)) { a1, b1, c1, d1, e1, f1, g1, h1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d + e + f + g + h)
         }
         
         property("Map with 9 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int) in
-            let left: Kind<F, Int> = F.map(F.pure(a), F.pure(b), F.pure(c), F.pure(d), F.pure(e), F.pure(f), F.pure(g), F.pure(h), F.pure(a), { a1, b1, c1, d1, e1, f1, g1, h1, i1 in a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1 + i1 })
-            let right: Kind<F, Int> = F.pure(a + b + c + d + e + f + g + h + a)
-            return left == right
+            
+            Kind<F, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g),
+                .pure(h),
+                .pure(a)) { a1, b1, c1, d1, e1, f1, g1, h1, i1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1 + i1
+            }
+                ==
+            Kind<F, Int>.pure(a + b + c + d + e + f + g + h + a)
         }
     }
 }
