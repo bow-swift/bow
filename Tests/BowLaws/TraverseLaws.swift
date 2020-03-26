985e06bddf0df9f53a5928cfcc8b936d298994b0
Tomás <truizlop@gmail.com>
2020-03-26 14:19:44 +0100

    Clean Traverse laws

--- Tests/BowLaws/TraverseLaws.swift
+++ Tests/BowLaws/TraverseLaws.swift
@@ -13,42 +13,56 @@ public class TraverseLaws<F: Traverse & EquatableK & ArbitraryK> {
     private static func identityTraverse() {
         property("Identity traverse") <~ forAll { (fa: KindOf<F, Int>, y: Int) in
             let f: (Int) -> Kind<ForId, Int> = { _ in Id<Int>(y) }
-            return Id.fix(F.traverse(fa.value, f)).value == F.map(F.map(fa.value, f), { a in Id.fix(a).value })
+            
+            return fa.value.traverse(f)^.value
+                ==
+            fa.value.map(f).map { a in a^.value }
         }
     }
     
     private static func sequentialComposition() {
         property("Sequential composition") <~ forAll { (f: ArrowOf<Int, Id<Int>>, g: ArrowOf<Int, Id<Int>>, x: KindOf<F, Int>) in
             let fa = x.value.traverse(f.getArrow)
-            let composed = fa.map { a in a.traverse(g.getArrow) }^.value^.value
-            let expected = x.value.traverse { a in f.getArrow(a).map(g.getArrow) }^.value.map { a in a.value }
-            return composed == expected
+            
+            return fa.map { a in a.traverse(g.getArrow) }^.value^.value
+                ==
+            x.value.traverse { a in
+                f.getArrow(a).map(g.getArrow)
+            }^.value.map { a in a.value }
         }
     }
     
     private static func parallelComposition() {
         property("Parallel composition") <~ forAll { (f: ArrowOf<Int, Id<Int>>, g: ArrowOf<Int, Id<Int>>, x: KindOf<F, Int>) in
-            let actual = TupleK.fix(x.value.traverse { a in TupleK((f.getArrow(a), g.getArrow(a))) })
-            let expected = TupleK((x.value.traverse(f.getArrow)^, x.value.traverse(g.getArrow)^))
-            return actual == expected
+            
+            x.value.traverse { a in
+                TupleK((f.getArrow(a), g.getArrow(a)))
+            }^
+                ==
+            TupleK((x.value.traverse(f.getArrow)^,
+                    x.value.traverse(g.getArrow)^))
         }
     }
     
     private static func foldMapDerived() {
         property("foldMap derived") <~ forAll { (f: ArrowOf<Int, Int>, fa: KindOf<F, Int>) in
-            let traversed = fa.value.traverse { a in Const<Int, Int>(f.getArrow(a)) }^.value
-            let mapped = fa.value.foldMap(f.getArrow)
-            return traversed == mapped
+            
+            fa.value.traverse { a in
+                Const<Int, Int>(f.getArrow(a))
+            }^.value
+                ==
+            fa.value.foldMap(f.getArrow)
         }
     }
 }
 
 private final class ForTupleK {}
-private class TupleK<A>: Kind<ForTupleK, A> {
+private typealias TupleKOf<A> = Kind<ForTupleK, A>
+private class TupleK<A>: TupleKOf<A> {
     let value: (Id<A>, Id<A>)
     
-    static func fix(_ value: Kind<ForTupleK, A>) -> TupleK<A> {
-        return value as! TupleK<A>
+    static func fix(_ value: TupleKOf<A>) -> TupleK<A> {
+        value as! TupleK<A>
     }
     
     init(_ value: (Id<A>, Id<A>)) {
@@ -56,27 +70,36 @@ private class TupleK<A>: Kind<ForTupleK, A> {
     }
 }
 
+private postfix func ^<A>(_ value: TupleKOf<A>) -> TupleK<A> {
+    TupleK.fix(value)
+}
+
 extension ForTupleK: Applicative {
-    static func pure<A>(_ a: A) -> Kind<ForTupleK, A> {
-        return TupleK((Id(a), Id(a)))
+    static func pure<A>(_ a: A) -> TupleKOf<A> {
+        TupleK((Id(a), Id(a)))
     }
     
-    static func ap<A, B>(_ ff: Kind<ForTupleK, (A) -> B>, _ fa: Kind<ForTupleK, A>) -> Kind<ForTupleK, B> {
-        let tuplef = TupleK.fix(ff)
-        let tuplea = TupleK.fix(fa)
-        return TupleK((tuplea.value.0.map(tuplef.value.0.value)^, tuplea.value.1.map(tuplef.value.1.value)^))
+    static func ap<A, B>(
+        _ ff: TupleKOf<(A) -> B>,
+        _ fa: TupleKOf<A>) -> Kind<ForTupleK, B> {
+        
+        TupleK((fa^.value.0.map(ff^.value.0.value)^,
+                fa^.value.1.map(ff^.value.1.value)^))
     }
     
-    static func map<A, B>(_ fa: Kind<ForTupleK, A>, _ f: @escaping (A) -> B) -> Kind<ForTupleK, B> {
-        let tuple = TupleK<A>.fix(fa)
-        return TupleK((tuple.value.0.map(f)^, tuple.value.1.map(f)^))
+    static func map<A, B>(
+        _ fa: TupleKOf<A>,
+        _ f: @escaping (A) -> B) -> TupleKOf<B> {
+        TupleK((fa^.value.0.map(f)^,
+                fa^.value.1.map(f)^))
     }
 }
 
 extension ForTupleK: EquatableK {
-    static func eq<A: Equatable>(_ lhs: Kind<ForTupleK, A>, _ rhs: Kind<ForTupleK, A>) -> Bool {
-        let x = TupleK.fix(lhs)
-        let y = TupleK.fix(rhs)
-        return x.value.0 == y.value.0 && x.value.1 == y.value.1
+    static func eq<A: Equatable>(
+        _ lhs: TupleKOf<A>,
+        _ rhs: TupleKOf<A>) -> Bool {
+        lhs^.value.0 == rhs^.value.0 &&
+        lhs^.value.1 == rhs^.value.1
     }
 }
