ad714b2ede016d62247357abbe8e5e8574c2f907
Tomás <truizlop@gmail.com>
2020-03-26 12:54:59 +0100

    Clean ComonadTraced laws

--- Tests/BowLaws/ComonadTracedLaws.swift
+++ Tests/BowLaws/ComonadTracedLaws.swift
@@ -11,14 +11,19 @@ public class ComonadTracedLaws<F: ComonadTraced & EquatableK & ArbitraryK, M: Eq
     
     static func traceEmpty() {
         property("trace empty is equal to extract") <~ forAll { (fa: KindOf<F, Int>) in
-            fa.value.trace(.empty()) == fa.value.extract()
+            
+            fa.value.trace(.empty())
+                ==
+            fa.value.extract()
         }
     }
     
     static func sequentialTracing() {
         property("tracing twice is equal to tracing combination of values") <~ forAll { (fa: KindOf<F, Int>, s: M, t: M) in
-            fa.value.coflatMap { wa in wa.trace(t) }.trace(s) ==
-                fa.value.trace(s.combine(t))
+            
+            fa.value.coflatMap { wa in wa.trace(t) }.trace(s)
+                ==
+            fa.value.trace(s.combine(t))
         }
     }
 }
