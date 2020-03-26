37495013db2f6812b4eeb9d7f04ad36b562cf6a4
Tomás <truizlop@gmail.com>
2020-03-26 11:30:16 +0100

    Clean Const

--- Tests/BowGenerators/Data/Const+Gen.swift
+++ Tests/BowGenerators/Data/Const+Gen.swift
@@ -5,14 +5,14 @@ import SwiftCheck
 
 extension Const: Arbitrary where A: Arbitrary {
     public static var arbitrary: Gen<Const<A, T>> {
-        return A.arbitrary.map(Const.init)
+        A.arbitrary.map(Const.init)
     }
 }
 
-// MARK: Instance of `ArbitraryK` for `Const`
+// MARK: Instance of ArbitraryK for Const
 
 extension ConstPartial: ArbitraryK where A: Arbitrary {
-    public static func generate<T: Arbitrary>() -> Kind<ConstPartial<A>, T> {
-        return Const.arbitrary.generate
+    public static func generate<T: Arbitrary>() -> ConstOf<A, T> {
+        Const.arbitrary.generate
     }
 }
