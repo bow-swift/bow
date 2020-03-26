45186bef0c0422739569cbd0ff5fc673e7a38e1e
Tomás <truizlop@gmail.com>
2020-03-26 12:23:12 +0100

    Clean ApplicativeError laws

--- Tests/BowLaws/ApplicativeErrorLaws.swift
+++ Tests/BowLaws/ApplicativeErrorLaws.swift
@@ -3,7 +3,7 @@ import Bow
 import BowGenerators
 
 public class ApplicativeErrorLaws<F: ApplicativeError & EquatableK> where F.E: Equatable & Arbitrary {
-    public static func check()  {
+    public static func check() {
         handle()
         handleWith()
         handleWithPure()
@@ -17,39 +17,57 @@ public class ApplicativeErrorLaws<F: ApplicativeError & EquatableK> where F.E: E
     private static func handle() {
         property("Applicative error handle") <~ forAll { (a: Int, error: F.E) in
             let f = { (_: F.E) in a }
-            return F.handleError(F.raiseError(error), f) == F.pure(f(error))
+            
+            return F.raiseError(error).handleError(f)
+                ==
+            F.pure(f(error))
         }
     }
     
     private static func handleWith() {
         property("Applicative error handle with") <~ forAll { (a: Int, error: F.E) in
             let f = { (_: F.E) in F.pure(a) }
-            return F.handleErrorWith(F.raiseError(error), f) == f(error)
+            
+            return Kind<F, Int>.raiseError(error).handleErrorWith(f)
+                ==
+            f(error)
         }
     }
     
     private static func handleWithPure() {
         property("Applicative error handle with pure") <~ forAll { (a: Int) in
             let f = { (_: F.E) in F.pure(a) }
-            return F.handleErrorWith(F.pure(a), f) == F.pure(a)
+            
+            return F.pure(a).handleErrorWith(f)
+                ==
+            F.pure(a)
         }
     }
     
     private static func attemptError() {
         property("Attempt error") <~ forAll { (error: F.E) in
-            return F.attempt(F.raiseError(error)) == F.pure(Either<F.E, Int>.left(error))
+            
+            F.raiseError(error).attempt()
+                ==
+            F.pure(Either<F.E, Int>.left(error))
         }
     }
     
     private static func attemptSuccess() {
         property("Attempt success") <~ forAll { (a: Int) in
-            return F.attempt(F.pure(a)) == F.pure(Either<F.E, Int>.right(a))
+            
+            F.pure(a).attempt()
+                ==
+            F.pure(Either<F.E, Int>.right(a))
         }
     }
     
     private static func attemptFromEitherConsistentWithPure() {
         property("Attempt from either consistent with pure") <~ forAll { (either: Either<F.E, Int>) in
-            return F.attempt(F.fromEither(either)) == F.pure(either)
+            
+            F.fromEither(either).attempt()
+                ==
+            F.pure(either)
         }
     }
     
@@ -57,7 +75,10 @@ public class ApplicativeErrorLaws<F: ApplicativeError & EquatableK> where F.E: E
         property("Catch") <~ forAll { (_: Int, error: F.E) in
             if error is Error {
                 let f : () throws -> Int = { throw error as! Error }
-                return F.catchError(f, { e in e as! F.E }) == F.raiseError(error)
+                
+                return Kind<F, Int>.catchError(f, { e in e as! F.E })
+                    ==
+                Kind<F, Int>.raiseError(error)
             } else {
                 return true
             }
@@ -67,7 +88,10 @@ public class ApplicativeErrorLaws<F: ApplicativeError & EquatableK> where F.E: E
     private static func catchesSuccess() {
         property("Catch") <~ forAll { (a: Int) in
             let f : () throws -> Int = { return a }
-            return F.catchError(f, { e in e as! F.E }) == F.pure(a)
+            
+            return Kind<F, Int>.catchError(f, { e in e as! F.E })
+                ==
+            F.pure(a)
         }
     }
 }
@@ -92,6 +116,9 @@ extension CategoryError: Equatable {}
 
 extension CategoryError: Arbitrary {
     static var arbitrary: Gen<CategoryError> {
-        return Gen<CategoryError>.fromElements(of: [CategoryError.common, CategoryError.fatal, CategoryError.unknown])
+        Gen<CategoryError>.fromElements(of: [
+            CategoryError.common,
+            CategoryError.fatal,
+            CategoryError.unknown])
     }
 }
