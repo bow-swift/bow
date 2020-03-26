d2f6d09362b4ccf9800cb6550f582d444bf37114
Tomás <truizlop@gmail.com>
2020-03-26 13:38:22 +0100

    Clean MonadFilter laws

--- Tests/BowLaws/MonadFilterLaws.swift
+++ Tests/BowLaws/MonadFilterLaws.swift
@@ -13,32 +13,50 @@ public class MonadFilterLaws<F: MonadFilter & EquatableK & ArbitraryK> {
     
     private static func leftEmpty() {
         property("Left empty") <~ forAll { (f: ArrowOf<Int, Int>) in
-            return F.flatMap(F.empty(), f.getArrow >>> F.pure) == F.empty()
+            
+            Kind<F, Int>.empty.flatMap(f.getArrow >>> F.pure)
+                ==
+            Kind<F, Int>.empty
         }
     }
     
     private static func rightEmpty() {
         property("Right empty") <~ forAll { (fa: KindOf<F, Int>) in
-            return F.flatMap(fa.value, constant(F.empty())) == F.empty() as Kind<F, Int>
+            
+            fa.value.followedBy(Kind<F, Int>.empty)
+                ==
+            Kind<F, Int>.empty
         }
     }
     
     private static func consistency()  {
         property("Consistency") <~ forAll { (fa: KindOf<F, Int>, f: ArrowOf<Int, Bool>) in
-            return F.filter(fa.value, f.getArrow) == F.flatMap(fa.value, { a in f.getArrow(a) ? F.pure(a) : F.empty() })
+            
+            fa.value.filter(f.getArrow)
+                ==
+            fa.value.flatMap { a in
+                f.getArrow(a) ? F.pure(a) : F.empty()
+            }
         }
     }
     
     private static func mapFilter() {
         property("mapFilter") <~ forAll { (flag: Bool, n: Int) in
-            return F.pure(()).mapFilter { _ in flag ? Option.some(n) : Option.none() } ==
-                (flag ? F.pure(n) : F.empty())
+            
+            F.pure(()).mapFilter { _ in
+                flag ? Option.some(n) : Option.none()
+            }
+                ==
+            (flag ? F.pure(n) : F.empty())
         }
     }
     
     private static func filter() {
         property("filter") <~ forAll { (flag: Bool, n: Int) in
-            return F.pure(n).filter(constant(flag)) == (flag ? F.pure(n) : F.empty())
+            
+            F.pure(n).filter(constant(flag))
+                ==
+            (flag ? F.pure(n) : F.empty())
         }
     }
 }
