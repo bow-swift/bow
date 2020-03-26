efb1402de010539ec24163da0d7210bc2864602a
Tomás <truizlop@gmail.com>
2020-03-26 13:59:36 +0100

    Clean Monoid laws

--- Tests/BowLaws/MonoidLaws.swift
+++ Tests/BowLaws/MonoidLaws.swift
@@ -9,13 +9,13 @@ public class MonoidLaws<A: Monoid & Equatable & Arbitrary> {
     
     private static func leftIdentity() {
         property("Left identity") <~ forAll { (a: A) in
-            return A.empty().combine(a) == a
+            A.empty().combine(a) == a
         }
     }
     
     private static func rightIdentity() {
         property("Right identity") <~ forAll { (a: A) in
-            return a.combine(A.empty()) == a
+            a.combine(A.empty()) == a
         }
     }
 }
