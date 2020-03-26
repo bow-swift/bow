0d6dd72b1d23921d8f63adf18a96618b6ad7ec5c
Tomás <truizlop@gmail.com>
2020-03-26 11:29:36 +0100

    Clean Kleisli

--- Tests/BowGenerators/Arrow/Kleisli+Gen.swift
+++ Tests/BowGenerators/Arrow/Kleisli+Gen.swift
@@ -5,14 +5,14 @@ import SwiftCheck
 
 extension Kleisli: Arbitrary where F: ArbitraryK, A: Arbitrary {
     public static var arbitrary: Gen<Kleisli<F, D, A>> {
-        return Gen.from(KleisliPartial.generate >>> Kleisli.fix)
+        Gen.from(KleisliPartial.generate >>> Kleisli.fix)
     }
 }
 
-// MARK: Instance of `ArbitraryK` for `Kleisli`
+// MARK: Instance of ArbitraryK for Kleisli
 
 extension KleisliPartial: ArbitraryK where F: ArbitraryK {
-    public static func generate<A: Arbitrary>() -> Kind<KleisliPartial<F, D>, A> {
+    public static func generate<A: Arbitrary>() -> KleisliOf<F, D, A> {
         let x: Kind<F, A> = F.generate()
         return Kleisli { _ in x }
     }
