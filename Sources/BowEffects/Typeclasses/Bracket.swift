89a36437d9cc4a4e88ab5e04f322028a0c9719c0
Tomás <truizlop@gmail.com>
2020-03-26 10:22:18 +0100

    Clean Bracket

--- Sources/BowEffects/Typeclasses/Bracket.swift
+++ Sources/BowEffects/Typeclasses/Bracket.swift
@@ -17,7 +17,10 @@ public enum ExitCase<E> {
     ///   - ifCanceled: Function to be applied if this value is canceled.
     ///   - ifError: Function to be applied if this value is errored.
     /// - Returns: Result of applying the corresponding function based on the content of this value.
-    public func fold<A>(_ ifCompleted: () -> A, _ ifCanceled: () -> A, _ ifError: (E) -> A) -> A {
+    public func fold<A>(
+        _ ifCompleted: () -> A,
+        _ ifCanceled: () -> A,
+        _ ifError: (E) -> A) -> A {
         switch self {
         case .completed: return ifCompleted()
         case .canceled: return ifCanceled()
@@ -54,9 +57,10 @@ public protocol Bracket: MonadError {
     ///   - release: Function to release the acquired resource.
     ///   - use: Function to use the acquired resource.
     /// - Returns: Computation describing the result of using the resource.
-    static func bracketCase<A, B>(acquire fa: Kind<Self, A>,
-                                  release: @escaping (A, ExitCase<Self.E>) -> Kind<Self, ()>,
-                                  use: @escaping (A) throws -> Kind<Self, B>) -> Kind<Self, B>
+    static func bracketCase<A, B>(
+        acquire fa: Kind<Self, A>,
+        release: @escaping (A, ExitCase<Self.E>) -> Kind<Self, Void>,
+        use: @escaping (A) throws -> Kind<Self, B>) -> Kind<Self, B>
 }
 
 // MARK: Related functions
@@ -69,10 +73,14 @@ public extension Bracket {
     ///   - release: Function to release the acquired resource, ignoring the outcome of the release of the resource.
     ///   - use: Function to use the acquired resource.
     /// - Returns: Computation describing the result of using the resource.
-    static func bracket<A, B>(acquire fa: Kind<Self, A>,
-                              release: @escaping (A) -> Kind<Self, ()>,
-                              use: @escaping (A) throws -> Kind<Self, B>) -> Kind<Self, B> {
-        return bracketCase(acquire: fa, release: { a, _ in release(a) }, use: use)
+    static func bracket<A, B>(
+        acquire fa: Kind<Self, A>,
+        release: @escaping (A) -> Kind<Self, Void>,
+        use: @escaping (A) throws -> Kind<Self, B>) -> Kind<Self, B> {
+        bracketCase(
+            acquire: fa,
+            release: { a, _ in release(a) },
+            use: use)
     }
     
     /// Forces a resource to be uncancelable even when an interruption happens.
@@ -80,7 +88,10 @@ public extension Bracket {
     /// - Parameter fa: Computation describing the acquisition of the resource.
     /// - Returns: An uncancelable computation.
     static func uncancelable<A>(_ fa: Kind<Self, A>) -> Kind<Self, A> {
-        return bracket(acquire: fa, release: constant(pure(())), use: { x in pure(x) })
+        bracket(
+            acquire: fa,
+            release: constant(pure(())),
+            use: { x in pure(x) })
     }
     
     /// Executes the given finalizer when the source is finished, either in success, error or cancelation.
@@ -89,9 +100,12 @@ public extension Bracket {
     ///   - fa: Computation describing the acquisition of the resource.
     ///   - finalizer: Finalizer function to be invoked when the resource is released.
     /// - Returns: A computation describing the resouce that will invoke the finalizer when it is released.
-    static func guarantee<A>(_ fa: Kind<Self, A>,
-                             finalizer: Kind<Self, ()>) -> Kind<Self, A> {
-        return guaranteeCase(fa, finalizer: constant(finalizer))
+    static func guarantee<A>(
+        _ fa: Kind<Self, A>,
+        finalizer: Kind<Self, Void>) -> Kind<Self, A> {
+        guaranteeCase(
+            fa,
+            finalizer: constant(finalizer))
     }
     
     /// Executes the given finalizer when the source is finished, either in success, error or cancelation, alowing to differentiate between exit conditions.
@@ -100,9 +114,13 @@ public extension Bracket {
     ///   - fa: Computation describing the acquisition of the resource.
     ///   - finalizer: Finalizer function to be invoked when the resource is released, distinguishing the exit case.
     /// - Returns: A computation describing the resource that will invoke the finalizer when it is released.
-    static func guaranteeCase<A>(_ fa: Kind<Self, A>,
-                                 finalizer: @escaping (ExitCase<Self.E>) -> Kind<Self, ()>) -> Kind<Self, A> {
-        return bracketCase(acquire: pure(()), release: { _, e in finalizer(e) }, use: { fa })
+    static func guaranteeCase<A>(
+        _ fa: Kind<Self, A>,
+        finalizer: @escaping (ExitCase<Self.E>) -> Kind<Self, Void>) -> Kind<Self, A> {
+        bracketCase(
+            acquire: pure(()),
+            release: { _, e in finalizer(e) },
+            use: { fa })
     }
 }
 
@@ -115,9 +133,13 @@ public extension Kind where F: Bracket {
     ///   - release: Function to release the acquired resource.
     ///   - use: Function to use the acquired resource.
     /// - Returns: Computation describing the result of using the resource.
-    func bracketCase<B>(release: @escaping (A, ExitCase<F.E>) -> Kind<F, ()>,
-                        use: @escaping (A) throws -> Kind<F, B>) -> Kind<F, B> {
-        return F.bracketCase(acquire: self, release: release, use: use)
+    func bracketCase<B>(
+        release: @escaping (A, ExitCase<F.E>) -> Kind<F, Void>,
+        use: @escaping (A) throws -> Kind<F, B>) -> Kind<F, B> {
+        F.bracketCase(
+            acquire: self,
+            release: release,
+            use: use)
     }
     
     /// A way to safely acquire a resource and release in the face of errors and cancellations. It uses `ExitCase` to distinguish between different exit cases when releasing the acquired resource.
@@ -126,31 +148,35 @@ public extension Kind where F: Bracket {
     ///   - release: Function to release the acquired resource, ignoring the outcome of the release of the resource.
     ///   - use: Function to use the acquired resource.
     /// - Returns: Computation describing the result of using the resource.
-    func bracket<B>(release: @escaping (A) -> Kind<F, ()>,
-                    use: @escaping (A) throws -> Kind<F, B>) -> Kind<F, B> {
-        return F.bracket(acquire: self, release: release, use: use)
+    func bracket<B>(
+        release: @escaping (A) -> Kind<F, Void>,
+        use: @escaping (A) throws -> Kind<F, B>) -> Kind<F, B> {
+        F.bracket(
+            acquire: self,
+            release: release,
+            use: use)
     }
     
     /// Forces this resource to be uncancelable even when an interruption happens.
     ///
     /// - Returns: An uncancelable computation.
     func uncancelable() -> Kind<F, A> {
-        return F.uncancelable(self)
+        F.uncancelable(self)
     }
     
     /// Executes the given finalizer when the source is finished, either in success, error or cancelation.
     ///
     /// - Parameter finalizer: Finalizer function to be invoked when the resource is released.
     /// - Returns: A computation describing the resouce that will invoke the finalizer when it is released.
-    func guarantee(_ finalizer: Kind<F, ()>) -> Kind<F, A> {
-        return F.guarantee(self, finalizer: finalizer)
+    func guarantee(_ finalizer: Kind<F, Void>) -> Kind<F, A> {
+        F.guarantee(self, finalizer: finalizer)
     }
     
     /// Executes the given finalizer when the source is finished, either in success, error or cancelation, alowing to differentiate between exit conditions.
     ///
     /// - Parameter finalizer: Finalizer function to be invoked when the resource is released, distinguishing the exit case.
     /// - Returns: A computation describing the resource that will invoke the finalizer when it is released.
-    func guaranteeCase(_ finalizer: @escaping (ExitCase<F.E>) -> Kind<F, ()>) -> Kind<F, A> {
-        return F.guaranteeCase(self, finalizer: finalizer)
+    func guaranteeCase(_ finalizer: @escaping (ExitCase<F.E>) -> Kind<F, Void>) -> Kind<F, A> {
+        F.guaranteeCase(self, finalizer: finalizer)
     }
 }
