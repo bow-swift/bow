4b5d98d8bf811db08bbe593f5c5fa1c21e2d3e99
Tomás <truizlop@gmail.com>
2020-03-26 12:53:52 +0100

    Clean Comonad laws

--- Tests/BowLaws/ComonadLaws.swift
+++ Tests/BowLaws/ComonadLaws.swift
@@ -15,46 +15,67 @@ public class ComonadLaws<F: Comonad & EquatableK & ArbitraryK> {
     
     private static func duplicateThenExtractIsId() {
         property("Duplicate then extract is equivalent to id") <~ forAll { (fa: KindOf<F, Int>) in
-            return F.extract(F.duplicate(fa.value)) == fa.value
+            
+            fa.value.duplicate().extract()
+                ==
+            fa.value
         }
     }
     
     private static func duplicateThenMapExtractIsId() {
         property("Duplicate then map extract is equivalent to id") <~ forAll { (fa: KindOf<F, Int>) in
-            return F.map(F.duplicate(fa.value), F.extract) == fa.value
+            
+            fa.value.duplicate().map(F.extract)
+                ==
+            fa.value
         }
     }
     
     private static func mapAndCoflatMapCoherence() {
         property("map and coflatMap coherence") <~ forAll { (fa: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
-            return F.map(fa.value, f.getArrow) == F.coflatMap(fa.value, { a in f.getArrow(F.extract(a)) })
+            
+            fa.value.map(f.getArrow)
+                ==
+            fa.value.coflatMap { a in f.getArrow(a.extract()) }
         }
     }
     
     private static func leftIdentity() {
         property("Left identity") <~ forAll { (fa: KindOf<F, Int>) in
-            return F.coflatMap(fa.value, F.extract) == fa.value
+            
+            fa.value.coflatMap(F.extract)
+                ==
+            fa.value
         }
     }
     
     private static func rightIdentity() {
         property("Right identity") <~ forAll { (fa: KindOf<F, Int>, fb: KindOf<F, Int>) in
             let f = { (_ : Kind<F, Int>) in fb.value }
-            return F.extract(F.coflatMap(fa.value, f)) == f(fa.value)
+            
+            return fa.value.coflatMap(f).extract()
+                ==
+            f(fa.value)
         }
     }
     
     private static func cokleisliLeftIdentity() {
         property("Cokleisli left identity") <~ forAll { (fa: KindOf<F, Int>, fb: KindOf<F, Int>) in
             let f = { (_: Kind<F, Int>) in fb.value }
-            return Cokleisli(F.extract).andThen(Cokleisli(f)).run(fa.value) == f(fa.value)
+            
+            return Cokleisli(F.extract).andThen(Cokleisli(f)).run(fa.value)
+                ==
+            f(fa.value)
         }
     }
     
     private static func cokleisliRightIdentity() {
         property("Cokleisli right identity") <~ forAll { (fa: KindOf<F, Int>, fb: KindOf<F, Int>) in
             let f = { (_: Kind<F, Int>) in fb.value }
-            return Cokleisli(f).andThen(Cokleisli(F.extract)).run(fa.value) == f(fa.value)
+            
+            return Cokleisli(f).andThen(Cokleisli(F.extract)).run(fa.value)
+                ==
+            f(fa.value)
         }
     }
 }
