8f002b46764974544671cb9469d53a99d19319f0
Tomás <truizlop@gmail.com>
2020-03-26 12:47:56 +0100

    Clean ComonadEnv laws

--- Tests/BowLaws/ComonadEnvLaws.swift
+++ Tests/BowLaws/ComonadEnvLaws.swift
@@ -3,6 +3,7 @@ import Bow
 import BowGenerators
 
 public class ComonadEnvLaws<F: ComonadEnv & EquatableK & ArbitraryK, A: Arbitrary & CoArbitrary & Hashable> where F.E == A {
+    
     public static func check() {
         askLocal()
         extractLocal()
@@ -11,15 +12,19 @@ public class ComonadEnvLaws<F: ComonadEnv & EquatableK & ArbitraryK, A: Arbitrar
     
     static func askLocal() {
         property("Ask followed by local is equivalent to applying a function to ask") <~ forAll { (fa: KindOf<F, Int>, f: ArrowOf<A, A>) in
-            fa.value.local(f.getArrow).ask() ==
-                f.getArrow(fa.value.ask())
+            
+            fa.value.local(f.getArrow).ask()
+                ==
+            f.getArrow(fa.value.ask())
         }
     }
     
     static func extractLocal() {
         property("Local does not affect extract") <~ forAll { (fa: KindOf<F, Int>, f: ArrowOf<A, A>) in
-            fa.value.local(f.getArrow).extract() ==
-                fa.value.extract()
+            
+            fa.value.local(f.getArrow).extract()
+                ==
+            fa.value.extract()
         }
     }
     
@@ -27,8 +32,9 @@ public class ComonadEnvLaws<F: ComonadEnv & EquatableK & ArbitraryK, A: Arbitrar
         property("CoflatMap Local") <~ forAll { (fa: KindOf<F, Int>, f: ArrowOf<A, A>, g: ArrowOf<Int, Int>) in
             let h: (Kind<F, Int>) -> Int = { wa in g.getArrow(wa.extract()) }
 
-            return fa.value.local(f.getArrow).coflatMap(h) ==
-                fa.value.coflatMap(h).local(f.getArrow)
+            return fa.value.local(f.getArrow).coflatMap(h)
+                ==
+            fa.value.coflatMap(h).local(f.getArrow)
         }
     }
 }
