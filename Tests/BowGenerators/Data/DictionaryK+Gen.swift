2a4b9525606ac3a70e69beb39baf3424ed624bf7
Tomás <truizlop@gmail.com>
2020-03-26 11:31:07 +0100

    Clean DictionaryK

--- Tests/BowGenerators/Data/DictionaryK+Gen.swift
+++ Tests/BowGenerators/Data/DictionaryK+Gen.swift
@@ -5,14 +5,14 @@ import SwiftCheck
 
 extension DictionaryK: Arbitrary where A: Arbitrary, K: Arbitrary {
     public static var arbitrary: Gen<DictionaryK<K, A>> {
-        return Dictionary.arbitrary.map(DictionaryK.init)
+        Dictionary.arbitrary.map(DictionaryK.init)
     }
 }
 
 // MARK: Instance of `ArbitraryK` for `DictionaryK`
 
 extension DictionaryKPartial: ArbitraryK where K: Hashable & Arbitrary {
-    public static func generate<A: Arbitrary>() -> Kind<DictionaryKPartial<K>, A> {
-        return DictionaryK.arbitrary.generate
+    public static func generate<A: Arbitrary>() -> DictionaryKOf<K, A> {
+        DictionaryK.arbitrary.generate
     }
 }
