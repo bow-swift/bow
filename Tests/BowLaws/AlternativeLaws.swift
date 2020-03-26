0887ab48866eac5eccd8f53a5cfeae6c1ba4ba4a
Tomás <truizlop@gmail.com>
2020-03-26 12:14:15 +0100

    Clean Alternative laws

--- Tests/BowLaws/AlternativeLaws.swift
+++ Tests/BowLaws/AlternativeLaws.swift
@@ -11,24 +11,30 @@ public class AlternativeLaws<F: Alternative & EquatableK & ArbitraryK> {
     
     private static func rightAbsorption() {
         property("Right absorption") <~ forAll { (ff: KindOf<F, ArrowOf<Int, Int>>) in
-            return F.ap(F.emptyK(), ff.value) ==
-                F.emptyK() as Kind<F, Int>
+            
+            F.emptyK().ap(ff.value)
+                ==
+            Kind<F, Int>.emptyK()
         }
     }
     
     private static func leftDistributivity() {
         property("Left distributivity") <~ forAll { (fx: KindOf<F, Int>, fy: KindOf<F, Int>, f: ArrowOf<Int, Int>) in
-            return F.map(F.combineK(fx.value, fy.value), f.getArrow) ==
-                F.combineK(F.map(fx.value, f.getArrow), F.map(fy.value, f.getArrow))
+            
+            fx.value.combineK(fy.value).map(f.getArrow)
+                ==
+            fx.value.map(f.getArrow).combineK( fy.value.map(f.getArrow))
         }
     }
     
     private static func rightDistributivity() {
-        property("Left distributivity") <~ forAll { (fx: KindOf<F, Int>, f: ArrowOf<Int, Int>, g: ArrowOf<Int, Int>) in
-            return F.ap(F.combineK(F.pure(f.getArrow), F.pure(g.getArrow)),
-                        fx.value) ==
-                F.combineK(F.ap(F.pure(f.getArrow), fx.value),
-                           F.ap(F.pure(g.getArrow), fx.value))
+        property("Right distributivity") <~ forAll { (fx: KindOf<F, Int>, f: ArrowOf<Int, Int>, g: ArrowOf<Int, Int>) in
+            
+            F.pure(f.getArrow).combineK(F.pure(g.getArrow))
+                .ap(fx.value)
+                ==
+            F.pure(f.getArrow).ap(fx.value)
+                .combineK(F.pure(g.getArrow).ap(fx.value))
         }
     }
 }
