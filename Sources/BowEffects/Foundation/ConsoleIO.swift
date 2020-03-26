a55d3ffd721b58d8d9d472db0dbfc5f38404d251
Tomás <truizlop@gmail.com>
2020-03-26 10:07:31 +0100

    Clean ConsoleIO

--- Sources/BowEffects/Foundation/ConsoleIO.swift
+++ Sources/BowEffects/Foundation/ConsoleIO.swift
@@ -3,12 +3,22 @@ import Foundation
 /// Utilities to read and write to the standard input/output in a functional manner.
 public enum ConsoleIO {
     /// IO suspended version of `Swift.print(_:separator:terminator:)`. Refer to that function for further documentation.
-    public static func print<E: Error>(_ line: @escaping @autoclosure () -> Any, separator: @escaping @autoclosure () -> CustomStringConvertible = " ", terminator: @escaping @autoclosure () -> CustomStringConvertible = "\n") -> IO<E, ()> {
-        return IO.invoke { Swift.print(line(), separator: "\(separator())", terminator: "\(terminator())") }
+    public static func print<E: Error>(
+        _ line: @escaping @autoclosure () -> Any,
+        separator: @escaping @autoclosure () -> CustomStringConvertible = " ",
+        terminator: @escaping @autoclosure () -> CustomStringConvertible = "\n") -> IO<E, ()> {
+        IO.invoke {
+            Swift.print(
+                line(),
+                separator: "\(separator())",
+                terminator: "\(terminator())")
+        }
     }
     
     /// IO suspended version of `Swift.readLine(strippingNewline:)`. Refer to that function for further documentation.
     public static func readLine<E: Error>(strippingNewline: Bool = true) -> IO<E, String?> {
-        return IO.invoke { Swift.readLine(strippingNewline: strippingNewline) }
+        IO.invoke {
+            Swift.readLine(strippingNewline: strippingNewline)
+        }
     }
 }
