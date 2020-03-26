8082c8175f942b4c4194db28ec6d568767beef25
Tomás <truizlop@gmail.com>
2020-03-26 14:07:55 +0100

    Clean Semiring laws

--- Tests/BowLaws/SemiringLaws.swift
+++ Tests/BowLaws/SemiringLaws.swift
@@ -16,49 +16,57 @@ public class SemiringLaws<A: Semiring & Equatable & Arbitrary> {
     
     private static func commutativityForCombining() {
         property("Commutativity for combining") <~ forAll { (a: A) in
-            return A.zero().combine(a) == a.combine(.zero())
+            A.zero().combine(a)
+                ==
+            a.combine(.zero())
         }
     }
     
     private static func associativityForMultiplying() {
         property("Ascociativity for multiplying") <~ forAll { (a: A, b: A, c: A) in
-            return (a.multiply(b)).multiply(c) == a.multiply(b.multiply(c))
+            a.multiply(b).multiply(c)
+                ==
+            a.multiply(b.multiply(c))
         }
     }
     
     private static func leftIdentityForMultiplying() {
         property("Left identity for multiplying") <~ forAll { (a: A) in
-            return A.one().multiply(a) == a
+            A.one().multiply(a) == a
         }
     }
     
     private static func rightIdentityForMultiplying() {
         property("Right identity for multiplying") <~ forAll { (a: A) in
-            return a.multiply(.one()) == a
+            a.multiply(.one()) == a
         }
     }
     
     private static func leftDistribution() {
         property("Left distribution") <~ forAll { (a: A, b: A, c: A) in
-            return a.multiply(b.combine(c)) == (a.multiply(b)).combine(a.multiply(c))
+            a.multiply(b.combine(c))
+                ==
+            (a.multiply(b)).combine(a.multiply(c))
        }
     }
     
     private static func rightDistribution() {
         property("Right distribution") <~ forAll { (a: A, b: A, c: A) in
-            return (a.combine(b)).multiply(c) == (a.multiply(c)).combine(b.multiply(c))
+            a.combine(b).multiply(c)
+                ==
+            a.multiply(c).combine(b.multiply(c))
        }
     }
 
     private static func leftAnnihilation() {
         property("Left Annihilation") <~ forAll { (a: A) in
-            return A.zero().multiply(a) == .zero()
+            A.zero().multiply(a) == .zero()
        }
     }
     
     private static func rightAnnihilation() {
         property("Right Annihilation") <~ forAll { (a: A) in
-            return a.multiply(.zero()) == .zero()
+            a.multiply(.zero()) == .zero()
        }
     }
 }
