6189fbd83fbc4d9f6569b66248133b01169d7714
Tomás <truizlop@gmail.com>
2020-03-26 11:40:06 +0100

    Clean EitherT

--- Tests/BowGenerators/Transformers/EitherT+Gen.swift
+++ Tests/BowGenerators/Transformers/EitherT+Gen.swift
@@ -5,14 +5,14 @@ import SwiftCheck
 
 extension EitherT: Arbitrary where F: ArbitraryK, A: Arbitrary, B: Arbitrary {
     public static var arbitrary: Gen<EitherT<F, A, B>> {
-        return Gen.from(EitherTPartial.generate >>> EitherT.fix)
+        Gen.from(EitherTPartial.generate >>> EitherT.fix)
     }
 }
 
-// MARK: Instance of `ArbitraryK` for `EitherT`
+// MARK: Instance of ArbitraryK for EitherT
 
 extension EitherTPartial: ArbitraryK where F: ArbitraryK, L: Arbitrary {
-    public static func generate<A: Arbitrary>() -> Kind<EitherTPartial<F, L>, A> {
-        return EitherT(F.generate())
+    public static func generate<A: Arbitrary>() -> EitherTOf<F, L, A> {
+        EitherT(F.generate())
     }
 }
