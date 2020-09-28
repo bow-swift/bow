a48ace555be187b11eefd1e6e7e9d5669739d6aa
Tomás Ruiz López <truizlop@gmail.com>
2020-09-28 12:27:44 +0200

    Test Cofree interpretation

--- Tests/BowFreeTests/CofreeTest.swift
+++ Tests/BowFreeTests/CofreeTest.swift
@@ -15,6 +15,27 @@ extension CofreePartial: EquatableK where F: EquatableK {
 }
 
 class CofreeTest: XCTestCase {
+    func testCofreeOptionIsNEA() {
+        func toNEA(cofree: Cofree<OptionPartial, Int>) -> NEA<Int> {
+            let a = cofree.head
+            if let next = cofree.tailForced()^.toOptional() {
+                return NEA.of(a) + toNEA(cofree: next)
+            } else {
+                return NEA.of(a)
+            }
+        }
+        
+        let nea = Cofree<OptionPartial, Int>.create(1) { x in
+            x < 5
+                ? Option.some(x + 1)
+                : Option.none()
+        }
+        let mapped = nea.map { x in 2 * x }^
+        let result = toNEA(cofree: mapped)
+        let expected = NEA.of(2, 4, 6, 8, 10)
+        XCTAssertEqual(result, expected)
+    }
+    
     func testFunctorLaws() {
         FunctorLaws<CofreePartial<OptionPartial>>.check()
     }
