b5a2d7f6502afa8e82bbcbfaa3ca4666348594de
Tomás Ruiz-López <truizlop@gmail.com>
2020-10-02 10:49:07 +0200

    Revamp Yoneda (#618)
    * Revamp Yoneda
    
    * Add generator for Yoneda
    
    * Test type class instances for Yoneda
    
    * Add documentation for Yoneda
    
    * Fix conflicts

---
+++ Tests/BowFreeTests/YonedaTest.swift
@@ -0,0 +1,31 @@
+import XCTest
+import Bow
+import BowFree
+import BowFreeGenerators
+import BowLaws
+
+class YonedaTest: XCTestCase {
+    func testFunctorLaws() {
+        FunctorLaws<YonedaPartial<IdPartial>>.check()
+    }
+    
+    func testApplicativeLaws() {
+        ApplicativeLaws<YonedaPartial<IdPartial>>.check()
+    }
+    
+    func testMonadLaws() {
+        MonadLaws<YonedaPartial<IdPartial>>.check(withStackSafety: false)
+    }
+    
+    func testComonadLaws() {
+        ComonadLaws<YonedaPartial<IdPartial>>.check()
+    }
+    
+    func testFoldableLaws() {
+        FoldableLaws<YonedaPartial<ArrayKPartial>>.check()
+    }
+    
+    func testTraverseLaws() {
+        TraverseLaws<YonedaPartial<ArrayKPartial>>.check()
+    }
+}
