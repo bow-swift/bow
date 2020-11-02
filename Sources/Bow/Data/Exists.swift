a9f04362831c9c76499a24aeb75d425383a6d308
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2020-11-02 15:24:20 +0100

    Make Exists conform to CustomStringConvertible

--- Sources/Bow/Data/Exists.swift
+++ Sources/Bow/Data/Exists.swift
@@ -44,3 +44,15 @@ fileprivate final class ExistsPrivate<F, A>: AnyExistsPrivate<F> {
         f.invoke(fa)
     }
 }
+
+extension Exists: CustomStringConvertible {
+    public var description: String {
+        (fa as! CustomStringConvertible).description
+    }
+}
+
+extension ExistsPrivate: CustomStringConvertible {
+    var description: String {
+        (fa as? CustomStringConvertible)?.description ?? "\(fa)"
+    }
+}
